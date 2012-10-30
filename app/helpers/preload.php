6a0d334e1c407bae4ddbbc39a24b234c27162d5e
Seth <seth@whirl-i-gig.com>
2012-10-30 11:48:27 -0400

    Don't preload requestHelpers after all

--- app/helpers/preload.php
+++ app/helpers/preload.php
@@ -35,7 +35,6 @@
    */
    
 	require(__CA_APP_DIR__."/helpers/utilityHelpers.php");
-	require(__CA_APP_DIR__."/helpers/requestHelpers.php");
 	require(__CA_APP_DIR__."/helpers/navigationHelpers.php");
 	require(__CA_APP_DIR__."/helpers/mailHelpers.php");
 	require(__CA_APP_DIR__."/helpers/clientServicesHelpers.php");
