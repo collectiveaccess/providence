444d2e4db5618610b97ac6caa9baed4114542fe1
stefan <stefan@whirl-i-gig.com>
2012-11-26 16:14:34 +0100

    Data structures for export

---
+++ app/models/ca_data_exporter_items.php
@@ -0,0 +1,217 @@
+<?php
+/** ---------------------------------------------------------------------
+ * app/models/ca_data_exporter_items.php
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2012 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ * 
+ * @package CollectiveAccess
+ * @subpackage models
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+ * 
+ * ----------------------------------------------------------------------
+ */
+ 
+ /**
+   *
+   */
+
+require_once(__CA_LIB_DIR__.'/core/ModelSettings.php');
+require_once(__CA_MODELS_DIR__."/ca_data_exporters.php");
+
+BaseModel::$s_ca_models_definitions['ca_data_exporter_items'] = array(
+ 	'NAME_SINGULAR' 	=> _t('data exporter item'),
+ 	'NAME_PLURAL' 		=> _t('data exporter items'),
+	'FIELDS' 			=> array(
+		'item_id' => array(
+				'FIELD_TYPE' => FT_NUMBER, 'DISPLAY_TYPE' => DT_HIDDEN, 
+				'IDENTITY' => true, 'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => _t('CollectiveAccess id'), 'DESCRIPTION' => _t('Unique numeric identifier used by CollectiveAccess internally to identify this exporter item')
+		),
+		'parent_id' => array(
+				'FIELD_TYPE' => FT_NUMBER, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => true, 
+				'DEFAULT' => '',
+				'LABEL' => 'Parent id', 'DESCRIPTION' => 'Identifier of parent object; is null if object is root of hierarchy.'
+		),
+		'exporter_id' => array(
+				'FIELD_TYPE' => FT_NUMBER, 'DISPLAY_TYPE' => DT_OMIT,
+				'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false,
+				'DEFAULT' => '',
+				'LABEL' => 'exporter id', 'DESCRIPTION' => 'Identifier for exporter'
+		),
+		'element' => array(
+				'FIELD_TYPE' => FT_TEXT, 'DISPLAY_TYPE' => DT_FIELD, 
+				'DISPLAY_WIDTH' => 70, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false,
+				'DEFAULT' => '',
+				'LABEL' => _t('External element'), 'DESCRIPTION' => _t('Name of the target element.'),
+				'BOUNDS_LENGTH' => array(0,1024)
+		),
+		'source' => array(
+				'FIELD_TYPE' => FT_TEXT, 'DISPLAY_TYPE' => DT_FIELD, 
+				'DISPLAY_WIDTH' => 70, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => _t('Data source'), 'DESCRIPTION' => _t('CollectiveAccess data source to export. This will typically be a bundle name.'),
+				'BOUNDS_LENGTH' => array(0,1024)
+		),
+		'settings' => array(
+				'FIELD_TYPE' => FT_VARS, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 88, 'DISPLAY_HEIGHT' => 15,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => _t('Settings'), 'DESCRIPTION' => _t('exporter item settings')
+		),
+		'hier_item_id' => array(
+				'FIELD_TYPE' => FT_NUMBER, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => 'Exporter item hierarchy', 'DESCRIPTION' => 'Identifier of exporter item that is root of the item hierarchy.'
+		),
+		'hier_left' => array(
+				'FIELD_TYPE' => FT_NUMBER, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => 'Hierarchical index - left bound', 'DESCRIPTION' => 'Left-side boundary for nested set-style hierarchical indexing; used to accelerate search and retrieval of hierarchical record sets.'
+		),
+		'hier_right' => array(
+				'FIELD_TYPE' => FT_NUMBER, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => 'Hierarchical index - right bound', 'DESCRIPTION' => 'Right-side boundary for nested set-style hierarchical indexing; used to accelerate search and retrieval of hierarchical record sets.'
+		),
+		'vars' => array(
+				'FIELD_TYPE' => FT_VARS, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 88, 'DISPLAY_HEIGHT' => 15,
+				'IS_NULL' => false, 
+				'DEFAULT' => '',
+				'LABEL' => 'Variable storage', 'DESCRIPTION' => 'Storage area for exporter variables'
+		),
+	)
+);
+
+global $_ca_data_exporter_items_settings;
+$_ca_data_exporter_items_settings = array(		// global
+);
+	
+class ca_data_exporter_items extends BaseModel {
+	# ---------------------------------
+	# --- Object attribute properties
+	# ---------------------------------
+	# Describe structure of content object's properties - eg. database fields and their
+	# associated types, what modes are supported, et al.
+	#
+
+	# ------------------------------------------------------
+	# --- Basic object parameters
+	# ------------------------------------------------------
+	# what table does this class represent?
+	protected $TABLE = 'ca_data_exporter_items';
+	      
+	# what is the primary key of the table?
+	protected $PRIMARY_KEY = 'item_id';
+
+	# ------------------------------------------------------
+	# --- Properties used by standard editing scripts
+	# 
+	# These class properties allow generic scripts to properly display
+	# records from the table represented by this class
+	#
+	# ------------------------------------------------------
+
+	# Array of fields to display in a listing of records from this table
+	protected $LIST_FIELDS = array('item_id');
+
+	# When the list of "list fields" above contains more than one field,
+	# the LIST_DELIMITER text is displayed between fields as a delimiter.
+	# This is typically a comma or space, but can be any string you like
+	protected $LIST_DELIMITER = ' ';
+
+	# What you'd call a single record from this table (eg. a "person")
+	protected $NAME_SINGULAR;
+
+	# What you'd call more than one record from this table (eg. "people")
+	protected $NAME_PLURAL;
+
+	# List of fields to sort listing of records by; you can use 
+	# SQL 'ASC' and 'DESC' here if you like.
+	protected $ORDER_BY = array('item_id');
+
+	# If you want to order records arbitrarily, add a numeric field to the table and place
+	# its name here. The generic list scripts can then use it to order table records.
+	protected $RANK = '';
+	
+	# ------------------------------------------------------
+	# Hierarchical table properties
+	# ------------------------------------------------------
+	protected $HIERARCHY_TYPE				=	__CA_HIER_TYPE_ADHOC_MONO__;
+	protected $HIERARCHY_LEFT_INDEX_FLD 	= 	'hier_left';
+	protected $HIERARCHY_RIGHT_INDEX_FLD 	= 	'hier_right';
+	protected $HIERARCHY_PARENT_ID_FLD		=	'parent_id';
+	protected $HIERARCHY_DEFINITION_TABLE	=	'ca_data_exporter_items';
+	protected $HIERARCHY_ID_FLD				=	'hier_item_id';
+	protected $HIERARCHY_POLY_TABLE			=	null;
+	
+	# ------------------------------------------------------
+	# Change logging
+	# ------------------------------------------------------
+	protected $UNIT_ID_FIELD = null;
+	protected $LOG_CHANGES_TO_SELF = false;
+	protected $LOG_CHANGES_USING_AS_SUBJECT = array(
+		"FOREIGN_KEYS" => array(
+		
+		),
+		"RELATED_TABLES" => array(
+		
+		)
+	);
+	
+	# ------------------------------------------------------
+	# $FIELDS contains information about each field in the table. The order in which the fields
+	# are listed here is the order in which they will be returned using getFields()
+
+	protected $FIELDS;
+	
+	/**
+	 * Settings delegate - implements methods for setting, getting and using 'settings' var field
+	 */
+	public $SETTINGS;
+	
+	# ------------------------------------------------------
+	public function __construct($pn_id=null) {		
+		global $_ca_data_exporter_items_settings;
+		parent::__construct($pn_id);
+		
+		//
+		$this->SETTINGS = new ModelSettings($this, 'settings', $_ca_data_exporter_items_settings);
+		
+	}
+	# ------------------------------------------------------
+}
+?>
--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -2216,6 +2216,76 @@ create table ca_data_importer_items (
 create index i_importer_id on ca_data_importer_items(importer_id);
 create index i_group_id on ca_data_importer_items(group_id);
 
+/*==========================================================================*/
+create table ca_data_exporters (
+   exporter_id          int unsigned         not null AUTO_INCREMENT,
+   exporter_code        varchar(100)         not null,
+   table_num            tinyint unsigned     not null,
+   settings          longtext          not null,
+   primary key (exporter_id)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+create unique index u_exporter_code on ca_data_exporters(exporter_code);
+create index i_table_num on ca_data_exporters(table_num);
+
+/*==========================================================================*/
+create table ca_data_exporter_labels (
+   label_id          int unsigned         not null AUTO_INCREMENT,
+   exporter_id          int unsigned         not null,
+   locale_id            smallint unsigned    not null,
+   name              varchar(255)         not null,
+   name_sort            varchar(255)         not null,
+   description          text              not null,
+   source_info          longtext             not null,
+   is_preferred         tinyint unsigned     not null,
+
+   primary key (label_id),
+
+   constraint fk_ca_data_exporter_labels_exporter_id foreign key (exporter_id)
+      references ca_data_exporters (exporter_id) on delete restrict on update restrict,
+
+   constraint fk_ca_data_exporter_labels_locale_id foreign key (locale_id)
+      references ca_locales (locale_id) on delete restrict on update restrict
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+create index i_exporter_id on ca_data_exporter_labels(exporter_id);
+create index i_locale_id on ca_data_exporter_labels(locale_id);
+create index i_name_sort on ca_data_exporter_labels(name_sort);
+create unique index u_all on ca_data_exporter_labels
+(
+   exporter_id,
+   locale_id,
+   name,
+   is_preferred
+);
+
+/*==========================================================================*/
+create table ca_data_exporter_items (
+   item_id           int unsigned         not null AUTO_INCREMENT,
+   parent_id            int unsigned         not null,
+   exporter_id          int unsigned         not null,
+   element              varchar(1024)        not null,
+   source               varchar(1024)        not null,
+   settings          longtext          not null,
+   hier_item_id         int unsigned         not null,
+   hier_left            decimal(30,20) unsigned not null,
+   hier_right           decimal(30,20) unsigned not null,
+   vars              longtext          not null,
+
+   primary key (item_id),
+
+   constraint fk_ca_data_exporter_items_exporter_id foreign key (exporter_id)
+      references ca_data_exporters (exporter_id) on delete restrict on update restrict,
+   constraint fk_ca_data_exporter_items_parent_id foreign key (parent_id)
+      references ca_data_exporter_items (item_id) on delete restrict on update restrict
+
+)  engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+create index i_parent_id on ca_data_exporter_items(parent_id);
+create index i_exporter_id on ca_data_exporter_items(exporter_id);
+create index i_hier_left on ca_data_exporter_items(hier_left);
+create index i_hier_right on ca_data_exporter_items(hier_right);
+create index i_hier_item_id on ca_data_exporter_items(hier_item_id);
 
 /*==========================================================================*/
 create table ca_object_lots_x_collections
@@ -6668,5 +6738,5 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-/* CURRENT MIGRATION: 72 */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (72, unix_timestamp());
+/* CURRENT MIGRATION: 73 */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (73, unix_timestamp());
