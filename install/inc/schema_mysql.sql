4ecd056b16d01d7130c6afe95ad2fb0c480fcb3e
Seth <seth@whirl-i-gig.com>
2013-01-09 16:16:46 -0500

    Fix for sometimes missing ca_list_items.deleted field

--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -6704,5 +6704,5 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-/* CURRENT MIGRATION: 76 */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (76, unix_timestamp());
+/* CURRENT MIGRATION: 77 */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (77, unix_timestamp());
