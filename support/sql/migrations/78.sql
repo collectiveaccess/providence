a5e5987a14598038c2a2d4437136be41721a68af
Seth <seth@whirl-i-gig.com>
2013-01-14 01:41:19 -0500

    Missing migration

--- support/sql/migrations/78.sql
+++ support/sql/migrations/78.sql
@@ -1,13 +1,16 @@
 /* 
-	Date: 9 January 2013
-	Migration: 77
+	Date: 13 January 2013
+	Migration: 78
 	Description:
 */
 
 
 /* -------------------------------------------------------------------------------- */
 
+create index i_hier_left on ca_places(hier_left);
+create index i_hier_right on ca_places(hier_right);
+
 /* -------------------------------------------------------------------------------- */
 
 /* Always add the update to ca_schema_updates at the end of the file */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (77, unix_timestamp());
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (78, unix_timestamp());
