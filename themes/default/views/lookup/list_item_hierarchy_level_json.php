7c60a7e9589481806144d34d7b3134716f5f7191
Seth <seth@collectiveaccess.org>
2013-01-14 00:36:57 -0500

    Restore display of list attribute icons

--- themes/default/views/lookup/list_item_hierarchy_level_json.php
+++ themes/default/views/lookup/list_item_hierarchy_level_json.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2009-2011 Whirl-i-Gig
+ * Copyright 2009-2013 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -27,23 +27,26 @@
  */
  
  	$va_list = $this->getVar('list_item_list');
- 	foreach($va_list as $vs_key => $va_info) {
- 		if (!is_array($va_info)) { continue; }
- 		
- 		if (!$this->getVar('dontShowSymbols')) {
-			if ($va_info['use_as_vocabulary']) {
-				$va_list[$vs_key]['name'] .= ' ⧩';
-			}
-			if ($va_info['is_system_list']) {
-				$va_list[$vs_key]['name'] .= ' ⟗';
-			}
-			if ($va_info['is_default']) {
-				$va_list[$vs_key]['name'] .= ' ◉';
-			}
-			if (isset($va_info['is_enabled']) && !$va_info['is_enabled']) {
-				$va_list[$vs_key]['name'] .= ' ⨂';
+ 	foreach($va_list as $vs_level => $va_level_content) {
+ 		foreach($va_level_content as $vs_key => $va_info) {
+			if (!is_array($va_info)) { continue; }
+		
+			if (!$this->getVar('dontShowSymbols')) {
+				if ($va_info['use_as_vocabulary']) {
+					$va_level_content[$vs_key]['name'] .= ' ⧩';
+				}
+				if ($va_info['is_system_list']) {
+					$va_level_content[$vs_key]['name'] .= ' ⟗';
+				}
+				if ($va_info['is_default']) {
+					$va_level_content[$vs_key]['name'] .= ' ◉';
+				}
+				if (isset($va_info['is_enabled']) && !$va_info['is_enabled']) {
+					$va_level_content[$vs_key]['name'] .= ' ⨂';
+				}
 			}
 		}
+		$va_list[$vs_level] = $va_level_content;
  	}
 	print json_encode($va_list);
 ?>
