e39eabbfe64a0143d12c5c2184daddb6eb270cc8
Seth <seth@whirl-i-gig.com>
2013-02-08 21:21:11 -0500

    Added importer log tables

--- app/models/ca_data_importers.php
+++ app/models/ca_data_importers.php
@@ -95,6 +95,14 @@ BaseModel::$s_ca_models_definitions['ca_data_importers'] = array(
 				'IS_NULL' => false, 
 				'DEFAULT' => '',
 				'LABEL' => _t('Settings'), 'DESCRIPTION' => _t('Importer settings')
+		),
+		'deleted' => array(
+				'FIELD_TYPE' => FT_BIT, 'DISPLAY_TYPE' => DT_OMIT, 
+				'DISPLAY_WIDTH' => 10, 'DISPLAY_HEIGHT' => 1,
+				'IS_NULL' => false, 
+				'DEFAULT' => 0,
+				'LABEL' => _t('Is deleted?'), 'DESCRIPTION' => _t('Indicates if the importer is deleted or not.'),
+				'BOUNDS_VALUE' => array(0,1)
 		)
 	)
 );
