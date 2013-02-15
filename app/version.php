cce6a356263ef6c43088956c524f52b98699f395
Seth <seth@whirl-i-gig.com>
2013-02-15 07:42:14 -0500

    Reset version

--- app/version.php
+++ app/version.php
@@ -1,6 +1,6 @@
 <?php
  	# CollectiveAccess version number; don't change
-	define('__CollectiveAccess__', '1.4');
+	define('__CollectiveAccess__', '1.3');
 	
 	# Schema revision
 	define('__CollectiveAccess_Schema_Rev__', 79);
