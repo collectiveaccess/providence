b4f67a6c20149ed852d0680af301f0300bc1bd94
Seth <seth@whirl-i-gig.com>
2013-02-15 22:37:17 -0500

    Added entityJoiner refinery

---
+++ app/refineries/entityJoiner/entitySplitterRefinery.php
@@ -0,0 +1,212 @@
+<?php
+/* ----------------------------------------------------------------------
+ * entitySplitterRefinery.php : 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2013 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+ 	require_once(__CA_LIB_DIR__.'/ca/Import/BaseRefinery.php');
+ 	require_once(__CA_LIB_DIR__.'/ca/Utils/DataMigrationUtils.php');
+ 
+	class entitySplitterRefinery extends BaseRefinery {
+		# -------------------------------------------------------
+		
+		# -------------------------------------------------------
+		public function __construct() {
+			$this->ops_name = 'entitySplitter';
+			$this->ops_title = _t('Entity splitter');
+			$this->ops_description = _t('Splits entities');
+			
+			parent::__construct();
+		}
+		# -------------------------------------------------------
+		/**
+		 * Override checkStatus() to return true
+		 */
+		public function checkStatus() {
+			return array(
+				'description' => $this->getDescription(),
+				'errors' => array(),
+				'warnings' => array(),
+				'available' => true,
+			);
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public function refine(&$pa_destination_data, $pa_group, $pa_item, $pa_source_data, $pa_options=null) {
+			$va_group_dest = explode(".", $pa_group['destination']);
+			$vs_terminal = array_pop($va_group_dest);
+			$pm_value = $pa_source_data[$pa_item['source']];
+			
+			if ($vs_delimiter = $pa_item['settings']['entitySplitter_delimiter']) {
+				$va_entities = explode($vs_delimiter, $pm_value);
+			} else {
+				$va_entities = array($pm_value);
+			}
+			
+			//print_R($pa_item);
+			
+			$va_vals = array();
+			$vn_c = 0;
+			foreach($va_entities as $vn_i => $vs_entity) {
+				if (!($vs_entity = trim($vs_entity))) { continue; }				
+			
+				if (is_array($va_skip_values = $pa_item['settings']['entitySplitter_skipIfValue']) && in_array($vs_entity, $va_skip_values)) {
+					continue;
+				}
+			
+				$va_split_name = DataMigrationUtils::splitEntityName($vs_entity);
+		
+				if(isset($va_split_name[$vs_terminal])) {
+					return $va_split_name[$vs_terminal];
+				}
+			
+				if (in_array($vs_terminal, array('preferred_labels', 'nonpreferred_labels'))) {
+					return $va_split_name;	
+				}
+			
+				// Set label
+				$va_val = array('preferred_labels' => $va_split_name);
+			
+				// Set relationship type
+				if (
+					($vs_rel_type_opt = $pa_item['settings']['entitySplitter_relationshipType'])
+				) {
+					if (!($va_val['_relationship_type'] = BaseRefinery::parsePlaceholder($vs_rel_type_opt, $pa_source_data, $pa_item, $vs_delimiter, $vn_c))) {
+						if ($vs_rel_type_opt = $pa_item['settings']['entitySplitter_relationshipTypeDefault']) {
+							$va_val['_relationship_type'] = BaseRefinery::parsePlaceholder($vs_rel_type_opt, $pa_source_data, $pa_item, $vs_delimiter, $vn_c);
+						}
+					}
+				}
+			
+				// Set entity_type
+				if (
+					($vs_type_opt = $pa_item['settings']['entitySplitter_entityType'])
+				) {
+					
+					if (!($va_val['_type'] = BaseRefinery::parsePlaceholder($vs_type_opt, $pa_source_data, $pa_item, $vs_delimiter, $vn_c))) {
+						if($vs_type_opt = $pa_item['settings']['entitySplitter_entityTypeDefault']) {
+							$va_val['_type'] = BaseRefinery::parsePlaceholder($vs_type_opt, $pa_source_data, $pa_item, $vs_delimiter, $vn_c);
+						}
+					}
+				}
+			
+				// Set attributes
+				if (is_array($pa_item['settings']['entitySplitter_attributes'])) {
+					$va_attr_vals = array();
+					foreach($pa_item['settings']['entitySplitter_attributes'] as $vs_element_code => $va_attrs) {
+						if(is_array($va_attrs)) {
+							foreach($va_attrs as $vs_k => $vs_v) {
+								$va_attr_vals[$vs_element_code][$vs_k] = BaseRefinery::parsePlaceholder($vs_v, $pa_source_data, $pa_item, $vs_delimiter, $vn_c);
+							}
+						}
+					}
+					$va_val = array_merge($va_val, $va_attr_vals);
+				}
+				
+				$va_vals[] = $va_val;
+				$vn_c++;
+			}
+			
+			return $va_vals;
+		}
+		# -------------------------------------------------------	
+		/**
+		 * entitySplitter returns multiple values
+		 *
+		 * @return bool Always true
+		 */
+		public function returnsMultipleValues() {
+			return true;
+		}
+		# -------------------------------------------------------
+	}
+	
+	 BaseRefinery::$s_refinery_settings['entitySplitter'] = array(		
+			'entitySplitter_delimiter' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_SELECT,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Delimiter'),
+				'description' => _t('Sets the value of the delimiter to break on, separating data source values.')
+			),
+			'entitySplitter_relationshipType' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_SELECT,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Relationship type'),
+				'description' => _t('Accepts a constant type code for the relationship type or a reference to the location in the data source where the type can be found.')
+			),
+			'entitySplitter_entityType' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_SELECT,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Entity type'),
+				'description' => _t('Accepts a constant list item idno from the list entity_types or a reference to the location in the data source where the type can be found')
+			),
+			'entitySplitter_attributes' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_SELECT,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Attributes'),
+				'description' => _t('Sets or maps metadata for the entity record by referencing the metadataElement code and the location in the data source where the data values can be found.')
+			),
+			'entitySplitter_relationshipTypeDefault' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_FIELD,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Relationship type default'),
+				'description' => _t('Sets the default relationship type that will be used if none are defined or if the data source values do not match any values in the CollectiveAccess system.')
+			),
+			'entitySplitter_entityTypeDefault' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_FIELD,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Entity type default'),
+				'description' => _t('Sets the default entity type that will be used if none are defined or if the data source values do not match any values in the CollectiveAccess list entity_types.')
+			),
+			'entitySplitter_skipIfValue' => array(
+				'formatType' => FT_TEXT,
+				'displayType' => DT_FIELD,
+				'width' => 10, 'height' => 1,
+				'takesLocale' => false,
+				'default' => '',
+				'label' => _t('Skip if value'),
+				'description' => _t('Skip if imported value is in the specified list of values.')
+			),
+		);
+?>
