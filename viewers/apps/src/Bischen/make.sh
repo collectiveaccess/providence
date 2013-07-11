909c6779cd06175526a25c1869e4e675c778ce1e
Seth <seth@whirl-i-gig.com>
2013-07-11 06:20:39 -0400

    Remove old player code

--- viewers/apps/src/Bischen/make.sh
+++
@@ -1,1 +0,0 @@
-/Applications/mtasc/mtasc -cp ~bischen bischen/*  -cp whirllib -cp /Applications/mtasc/std -cp /Applications/mtasc/std8 -swf bischen.swf -main -trace whirl.Debug.logMessage -group -v
