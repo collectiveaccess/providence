099b30e3ade0c5981d560836ecf4f67bb8f672c7
Stefan <stefan@whirl-i-gig.com>
2013-07-17 14:09:29 +0200

    Implemented feature request PROV-484
    GeoNames, LCSH and Taxonomy lookups can now run through proxy servers
    (disabled by default).

--- app/controllers/lookup/GeoNamesController.php
+++ app/controllers/lookup/GeoNamesController.php
@@ -70,7 +70,30 @@ class GeoNamesController extends ActionController {
 			}
 
 			try {
-				$vo_xml = new SimpleXMLElement($x = @file_get_contents("{$vs_base}?$vs_query_string"));
+
+				if($vs_proxy = $vo_conf->get('web_services_proxy_url')){ /* proxy server is configured */
+
+					if(($vs_proxy_user = $vo_conf->get('web_services_proxy_auth_user')) && ($vs_proxy_pass = $vo_conf->get('web_services_proxy_auth_pw'))){
+						$vs_proxy_auth = base64_encode("{$vs_proxy_user}:{$vs_proxy_pass}");
+					}
+
+					$va_context_options = array( 'http' => array(
+						'proxy' => $vs_proxy,
+						'request_fulluri' => true
+					));
+
+					if($vs_proxy_auth){
+						$va_context_options['http']['header'] = "Proxy-Authorization: Basic {$vs_prosy_auth}";
+					}
+
+					$vo_context = stream_context_create($va_context_options);
+					$vs_xml = @file_get_contents("{$vs_base}?$vs_query_string", false, $vo_context);
+
+				} else {
+					$vs_xml = @file_get_contents("{$vs_base}?$vs_query_string");
+				}
+
+				$vo_xml = new SimpleXMLElement($vs_xml);
 				
 				$va_attr = $vo_xml->status ? $vo_xml->status->attributes() : null;
 				if ($va_attr && isset($va_attr['value']) && ((int)$va_attr['value'] > 0)) { 
