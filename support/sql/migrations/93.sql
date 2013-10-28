ed882c88bd6bd6bc239a00c311ce0554196f0d88
Seth <seth@whirl-i-gig.com>
2013-10-28 07:42:13 -0400

    PROV-651 Fix sql issue w/some version of MySQL

--- support/sql/migrations/93.sql
+++ support/sql/migrations/93.sql
@@ -4,7 +4,7 @@
 	Description: Add settings field to ca_list_items
 */
 
-alter table ca_list_items add column settings longtext not null default '';
+alter table ca_list_items add column settings longtext not null;
 
 /* Always add the update to ca_schema_updates at the end of the file */
 INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (93, unix_timestamp());
