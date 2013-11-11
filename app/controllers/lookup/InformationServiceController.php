ea3f097ff9ebbe63afa92ed4dde21b2d16abff7c
Seth <seth@whirl-i-gig.com>
2013-11-11 01:41:33 -0500

    PROV-672 Working lookup

--- app/controllers/lookup/InformationServiceController.php
+++ app/controllers/lookup/InformationServiceController.php
@@ -27,6 +27,8 @@
  */
  	require_once(__CA_APP_DIR__."/helpers/displayHelpers.php");
  	require_once(__CA_MODELS_DIR__."/ca_metadata_elements.php");
+ 	require_once(__CA_MODELS_DIR__."/ca_metadata_elements.php");
+ 	require_once(__CA_LIB_DIR__.'/core/InformationServiceManager.php');
  	
  
  	class InformationServiceController extends ActionController {
@@ -38,45 +40,56 @@
  		# AJAX handlers
  		# -------------------------------------------------------
 		public function Get($pa_additional_query_params=null, $pa_options=null) {
+			$o_config = Configuration::load();
+			
 			if (!($ps_query = $this->request->getParameter('q', pString))) {
 				$ps_query = $this->request->getParameter('term', pString);
 			}
 			$ps_type = $this->request->getParameter('type', pString);
+			$pn_element_id = $this->request->getParameter('element_id', pInteger);
+			$t_element = new ca_metadata_elements($pn_element_id);
 			
-			if ($vn_element_id = $this->request->getParameter('element_id', pInteger)) {
-				$t_element = new ca_metadata_elements($vn_element_id);
-				if ($vs_voc = $t_element->getSetting('vocabulary')) {
-					$vs_voc_query .= '&q='.urlencode($vs_voc);
-				}
-			}
-			$vo_conf = Configuration::load();
-			$va_items = array();
-			if (unicode_strlen($ps_query) >= 3) {
-				try {
-					// Load plugin and connect to information service
-
-					if ($vs_data) {
-						$o_xml = @simplexml_load_string($vs_data);
-	
-						if ($o_xml) {
-							$o_entries = $o_xml->{'entry'};
-							if ($o_entries && sizeof($o_entries)) {
-								foreach($o_entries as $o_entry) {
-									$o_links = $o_entry->{'link'};
-									$va_attr = $o_links[0]->attributes();
-									$vs_url = (string)$va_attr->{'href'};
-									$va_items[] = array('label' => (string)$o_entry->{'title'}, 'idno' => (string)$o_entry->{'id'}, 'url' => $vs_url);
+			if (!$t_element->getPrimaryKey()) { 
+				// error
+				$va_items['error'] = array('label' => _t('ERROR: Invalid element_id'), 'idno' => '');
+			} else {
+			
+				$vs_service = $t_element->getSetting('service');
+			
+				$va_items = array();
+				if (unicode_strlen($ps_query) >= 3) {
+					try {
+						// Load plugin and connect to information service
+						if (!($o_plugin = InformationServiceManager::getInformationServiceInstance($vs_service))) {
+							$va_items['error'] = array('label' => _t('ERROR: Invalid service'), 'idno' => '');
+						} else {
+							$va_data = $o_plugin->lookup($t_element->getSettings(), $ps_query, array('element_id' => $pn_element_id));
+					
+							if ($va_data && isset($va_data['results']) && is_array($va_data['results'])) {
+								foreach($va_data['results'] as $va_result) {
+									$va_items[] = array('label' => (string)$va_result['label'], 'idno' => (string)$va_result['idno'], 'url' => (string)$va_result['url']);
 								}
 							}
 						}
+					} catch (Exception $e) {
+						$va_items['error'] = array('label' => _t('ERROR').': '.$e->getMessage(), 'idno' => '');
 					}
-				} catch (Exception $e) {
-					$va_items['error'] = array('displayname' => _t('ERROR').':'.$e->getMessage(), 'idno' => '');
 				}
 			}
 			
-			$this->view->setVar('lcsh_list', $va_items);
- 			return $this->render('ajax_lcsh_list_html.php');
+			$this->view->setVar('information_service_list', $va_items);
+ 			return $this->render('ajax_information_service_list_html.php');
+		}
+ 		# -------------------------------------------------------
+ 		/**
+ 		 *
+ 		 */
+		public function GetDetail() {
+			$pn_attribute_value_id = $this->request->getParameter('id', pInteger);
+			$t_value = new ca_attribute_values($pn_attribute_value_id);
+			$t_value->dump();
+			//$vs_url = 
+			return $this->render('ajax_information_service_detail_html.php');
 		}
 		# -------------------------------------------------------
  	}
