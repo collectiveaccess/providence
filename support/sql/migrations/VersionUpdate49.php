5bc5454b48560013ad43c6df52ef58fa36356823
Seth <seth@whirl-i-gig.com>
2014-02-12 22:26:57 -0500

    PROV-805 Updater writes a change to the db that is logged to the change log, but the libs are expecting a "batch_id" field in the change log that isn't present until migration 74. We fix this by disabling logging for this particular migration

--- support/sql/migrations/VersionUpdate49.php
+++ support/sql/migrations/VersionUpdate49.php
@@ -117,6 +117,7 @@
 				// Create root ca_relationship_types row for table
 				$t_root = new ca_relationship_types();
 				if (!$t_root->load(array('type_code' => 'root_for_table_'.$vn_table_num))) {
+					$t_root->logChanges(false);
 					$t_root->setMode(ACCESS_WRITE);
 					$t_root->set('table_num', $vn_table_num);
 					$t_root->set('type_code', 'root_for_table_'.$vn_table_num);
