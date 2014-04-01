9a8b288161628ab8c16e145a0dfe00b227499a67
Stefan <stefan@whirl-i-gig.com>
2014-04-01 16:29:32 +0200

    PROV-875 Some display-related improvements in ConfigurationExporter
    1) Now export display settings as well
    2) Now properly insert labels as preferred. Previously is_preferred
    would be 0 for display labels created via Installer which meant they
    would never show up under Manage > My displays.
    3) Wrap display settings in CDATA instead of escaping. This helps with
    readability and editing of complex exported displays. A lot.

--- app/lib/ca/ConfigurationExporter.php
+++ app/lib/ca/ConfigurationExporter.php
@@ -882,10 +882,23 @@ final class ConfigurationExporter {
 				}
 			}
 			$vs_buf .= "\t\t</labels>\n";
-			
+
+			$va_settings = $t_display->getSettings();
+			if(sizeof($va_settings)>0){
+				$vs_buf .= "\t\t<settings>\n";
+				foreach ($va_settings as $vs_setting => $vm_val) {
+					if (is_array($vm_val)) {
+						foreach($vm_value as $vn_i => $vn_val) {
+							$vs_buf .= "\t\t\t<setting name='{$vs_setting}'><![CDATA[".$vn_val."]]></setting>\n";
+						}
+					} else {
+						$vs_buf .= "\t\t\t<setting name='{$vs_setting}'><![CDATA[".$vm_val."]]></setting>\n";
+					}
+				}
+				$vs_buf .= "\t\t</settings>\n";
+			}
 			
 			$va_placements = $t_display->getPlacements();
-			//print_R(($va_placements));
 			
 			$vs_buf .= "<bundlePlacements>\n";
 			foreach($va_placements as $vn_placement_id => $va_placement_info) {
@@ -907,10 +920,10 @@ final class ConfigurationExporter {
 							default:
 								if (is_array($vm_value)) {
 									foreach($vm_value as $vn_i => $vn_val) {
-										$vs_buf .= "\t\t\t\t<setting name='{$vs_setting}'>".caEscapeForXML($vn_val)."</setting>\n";
+										$vs_buf .= "\t\t\t\t<setting name='{$vs_setting}'><![CDATA[".$vn_val."]]></setting>\n";
 									}
 								} else {
-									$vs_buf .= "\t\t\t\t<setting name='{$vs_setting}'>".caEscapeForXML($vm_value)."</setting>\n";
+									$vs_buf .= "\t\t\t\t<setting name='{$vs_setting}'><![CDATA[".$vm_value."]]></setting>\n";
 								}
 								break;
 						}
