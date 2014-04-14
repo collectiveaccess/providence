d4504cc66fdf10f1be6384832f64ee9494bc94e6
Seth <seth@nsm264mmuscoll2.sept11mm.org>
2014-04-14 15:15:33 -0400

    NOJIRA Don't try to convert parent_id idnos when empty

--- app/lib/core/BaseModel.php
+++ app/lib/core/BaseModel.php
@@ -1158,7 +1158,7 @@ class BaseModel extends BaseObject {
 						}
 						
 						
-						if (($vs_field == $this->HIERARCHY_PARENT_ID_FLD) && (!is_numeric($vm_value))) {
+						if (($vs_field == $this->HIERARCHY_PARENT_ID_FLD) && strlen($vm_value) && (!is_numeric($vm_value))) {
 							if(is_array($va_ids = call_user_func_array($this->tableName()."::find", array(array('idno' => $vm_value, 'deleted' => 0), array('returnAs' => 'ids'))))) {
 								$vm_value = array_shift($va_ids);
 							}
