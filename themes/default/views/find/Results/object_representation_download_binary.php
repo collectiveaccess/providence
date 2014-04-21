54c6cb867c8ebcefe5c02eb3bf95f1f5976f37f0
Seth <seth@collectiveaccess.org>
2014-04-21 13:51:32 -0400

    NOJIRA fix memory issue with large downloads

--- themes/default/views/find/Results/object_representation_download_binary.php
+++ themes/default/views/find/Results/object_representation_download_binary.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2012 Whirl-i-Gig
+ * Copyright 2012-2014 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -37,6 +37,11 @@
 	header('Content-Length: ' . filesize($vs_file_path));
 	
 	header("Content-Disposition: attachment; filename=".$this->getVar('download_name'));
-	ob_end_flush();	// need to do this in order to not have read file use request memory due to buffering
-	readfile($vs_file_path);
+	set_time_limit(0);
+	$o_fp = @fopen($vs_file_path,"rb");
+	while(is_resource($o_fp) && !feof($o_fp)) {
+		print(@fread($o_fp, 1024*8));
+		ob_flush();
+		flush();
+	}
 ?>
