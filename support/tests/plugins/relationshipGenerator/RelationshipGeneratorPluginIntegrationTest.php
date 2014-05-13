7a1bfcfbeecaee5298ec7acc20d06ad0e303c316
leftclickben <ben@leftclick.com.au>
2014-05-13 18:25:07 +0800

    Fix issue with runs of multiple tests.
    + Share fixtures between tests and use static setUpBeforeClass() / tearDownAfterClass()
      methods instead of non-static setUp() / tearDown()
    + This commit only changes non-static to static and $this to self
    + Oh and added some phpdoc

--- support/tests/plugins/relationshipGenerator/RelationshipGeneratorPluginIntegrationTest.php
+++ support/tests/plugins/relationshipGenerator/RelationshipGeneratorPluginIntegrationTest.php
@@ -43,38 +43,38 @@ ApplicationPluginManager::initPlugins();
  */
 class RelationshipGeneratorPluginIntegrationTest extends PHPUnit_Framework_TestCase {
 
-	private $ops_timestamp;
-	private $ops_randomNumber;
-	private $opo_oldPluginInstance;
+	private static $ops_timestamp;
+	private static $ops_randomNumber;
+	private static $opo_oldPluginInstance;
 
 	/** @var ca_locales */
-	private $opo_locale;
+	private static $opo_locale;
 
 	/** @var ca_list_items[] */
-	private $opa_listItems;
+	private static $opa_listItems;
 
 	/** @var ca_collections[] */
-	private $opa_collections;
+	private static $opa_collections;
 
 	/** @var ca_objects[] */
-	private $opa_objects;
+	private static $opa_objects;
 
-	public function setUp() {
-		$this->ops_timestamp = date('YmdHis');
-		$this->ops_randomNumber = mt_rand(100000, 1000000);
-		$this->_generateConfFile(__DIR__ . '/conf/integration');
+	public static function setUpBeforeClass() {
+		self::$ops_timestamp = date('YmdHis');
+		self::$ops_randomNumber = mt_rand(100000, 1000000);
+		self::_generateConfFile(__DIR__ . '/conf/integration');
 
 		// HACK Store old instance of the plugin, and replace with one of our own with known configuration
-		$this->opo_oldPluginInstance = ApplicationPluginManager::$s_application_plugin_instances['relationshipGenerator'];
+		self::$opo_oldPluginInstance = ApplicationPluginManager::$s_application_plugin_instances['relationshipGenerator'];
 		ApplicationPluginManager::$s_application_plugin_instances['relationshipGenerator'] = new relationshipGeneratorPlugin(__DIR__ . '/conf/integration');
 		// END HACK
 
-		$this->opo_locale = new ca_locales();
-		$this->opo_locale->loadLocaleByCode(__CA_DEFAULT_LOCALE__);
+		self::$opo_locale = new ca_locales();
+		self::$opo_locale->loadLocaleByCode(__CA_DEFAULT_LOCALE__);
 
-		$this->opa_listItems = array();
-		$this->opa_collections = array();
-		$this->opa_objects = array();
+		self::$opa_listItems = array();
+		self::$opa_collections = array();
+		self::$opa_objects = array();
 
 		$va_listItems = array(
 			array(
@@ -88,45 +88,45 @@ class RelationshipGeneratorPluginIntegrationTest extends PHPUnit_Framework_TestC
 		);
 
 		foreach ($va_listItems as $va_listItemDetails) {
-			$this->_createListItem($va_listItemDetails['item'], $va_listItemDetails['list']);
+			self::_createListItem($va_listItemDetails['item'], $va_listItemDetails['list']);
 		}
 		foreach (array( 'single', 'multiple', 'ignored' ) as $va_listItemDetails) {
-			$this->_createCollection($va_listItemDetails);
+			self::_createCollection($va_listItemDetails);
 		}
 	}
 
-	public function tearDown() {
-		foreach ($this->opa_objects as $vo_object) {
+	public static function tearDownAfterClass() {
+		foreach (self::$opa_objects as $vo_object) {
 			$vo_object->setMode(ACCESS_WRITE);
 			// TODO This leaves data in the `ca_metadata_type_restrictions` table, it should be removed here
 			$vo_object->delete(true, array( 'hard' => true ));
 		}
-		foreach ($this->opa_collections as $vo_collection) {
+		foreach (self::$opa_collections as $vo_collection) {
 			$vo_collection->setMode(ACCESS_WRITE);
 			$vo_collection->delete(true, array( 'hard' => true ));
 		}
-		foreach ($this->opa_listItems as $vo_listItem) {
+		foreach (self::$opa_listItems as $vo_listItem) {
 			$vo_listItem->setMode(ACCESS_WRITE);
 			$vo_listItem->delete(true, array( 'hard' => true ));
 		}
 
 		// HACK Restore old instance of the plugin
-		ApplicationPluginManager::$s_application_plugin_instances['relationshipGenerator'] = $this->opo_oldPluginInstance;
+		ApplicationPluginManager::$s_application_plugin_instances['relationshipGenerator'] = self::$opo_oldPluginInstance;
 		// END HACK
 	}
 
 	public function testInsertedRecordNotMatchingAnyRuleDoesNotCreateRelationship() {
-		$vo_object = $this->_createObject('test1', 'non-numeric');
-		$this->assertEquals(0, $this->_getCollectionRelationshipCount($vo_object, 'single'));
-		$this->assertEquals(0, $this->_getCollectionRelationshipCount($vo_object, 'multiple'));
-		$this->assertEquals(0, $this->_getCollectionRelationshipCount($vo_object, 'ignored'));
+		$vo_object = self::_createObject('test1', 'non-numeric');
+		$this->assertEquals(0, self::_getCollectionRelationshipCount($vo_object, 'single'));
+		$this->assertEquals(0, self::_getCollectionRelationshipCount($vo_object, 'multiple'));
+		$this->assertEquals(0, self::_getCollectionRelationshipCount($vo_object, 'ignored'));
 	}
 
 	public function testInsertedRecordMatchingSingleRuleCreatesSingleRelationship() {
-		$vo_object = $this->_createObject('test2', '1');
-		$this->assertEquals(1, $this->_getCollectionRelationshipCount($vo_object, 'single'));
-		$this->assertEquals(0, $this->_getCollectionRelationshipCount($vo_object, 'multiple'));
-		$this->assertEquals(0, $this->_getCollectionRelationshipCount($vo_object, 'ignored'));
+		$vo_object = self::_createObject('test2', '1');
+		$this->assertEquals(1, self::_getCollectionRelationshipCount($vo_object, 'single'));
+		$this->assertEquals(0, self::_getCollectionRelationshipCount($vo_object, 'multiple'));
+		$this->assertEquals(0, self::_getCollectionRelationshipCount($vo_object, 'ignored'));
 	}
 
 //	public function testInsertedRecordMatchingMultipleRulesCreatesMultipleRelationships() {
@@ -161,23 +161,22 @@ class RelationshipGeneratorPluginIntegrationTest extends PHPUnit_Framework_TestC
 //
 //	}
 
-	private function _getIdno($ps_idnoBase) {
-		return sprintf('%s_%s_%s', $this->ops_timestamp, $this->ops_randomNumber, $ps_idnoBase);
+	private static function _getIdno($ps_idnoBase) {
+		return sprintf('%s_%s_%s', self::$ops_timestamp, self::$ops_randomNumber, $ps_idnoBase);
 	}
 
-	private function _generateConfFile($ps_path) {
+	private static function _generateConfFile($ps_path) {
 		$vs_outfile = $ps_path . '/conf/relationshipGenerator.conf';
 		if (file_exists($vs_outfile)) {
 			unlink($vs_outfile);
 		}
 		$va_parsed = array();
-		$vo_this = $this;
 		foreach (file($ps_path . '/conf/relationshipGenerator.conf.template') as $vs_line) {
 			while (strpos($vs_line, '%%') !== false) {
 				$vs_line = preg_replace_callback(
 					'/%%(.*?)%%/',
-					function ($pa_match) use ($vo_this) {
-						return sizeof($pa_match) > 1 ? $vo_this->_getIdno($pa_match[1]) : '::: ERROR PARSING CONFIGURATION TEMPLATE :::';
+					function ($pa_match) {
+						return sizeof($pa_match) > 1 ? self::_getIdno($pa_match[1]) : '::: ERROR PARSING CONFIGURATION TEMPLATE :::';
 					},
 					$vs_line
 				);
@@ -187,11 +186,11 @@ class RelationshipGeneratorPluginIntegrationTest extends PHPUnit_Framework_TestC
 		file_put_contents($vs_outfile, join('', $va_parsed));
 	}
 
-	private function _createListItem($ps_idnoBase, $pn_listId) {
+	private static function _createListItem($ps_idnoBase, $pn_listId) {
 		$vo_listItem = new ca_list_items();
 		$vo_listItem->setMode(ACCESS_WRITE);
 		$vo_listItem->set(array(
-			'idno' => $this->_getIdno($ps_idnoBase),
+			'idno' => self::_getIdno($ps_idnoBase),
 			'list_id' => $pn_listId,
 			'is_enabled' => true
 		));
@@ -201,56 +200,61 @@ class RelationshipGeneratorPluginIntegrationTest extends PHPUnit_Framework_TestC
 				'name_singular' => $ps_idnoBase,
 				'name_plural' => $ps_idnoBase
 			),
-			$this->opo_locale->getPrimaryKey(),
+			self::$opo_locale->getPrimaryKey(),
 			null,
 			true
 		);
-		$this->opa_listItems[$ps_idnoBase] = $vo_listItem;
+		self::$opa_listItems[$ps_idnoBase] = $vo_listItem;
 		return $vo_listItem;
 	}
 
-	private function _createCollection($ps_idnoBase) {
+	private static function _createCollection($ps_idnoBase) {
 		$vo_collection = new ca_collections();
 		$vo_collection->setMode(ACCESS_WRITE);
 		$vo_collection->set(array(
-			'idno' => $this->_getIdno($ps_idnoBase),
-			'type_id' => $this->opa_listItems['test_collection']->getPrimaryKey()
+			'idno' => self::_getIdno($ps_idnoBase),
+			'type_id' => self::$opa_listItems['test_collection']->getPrimaryKey()
 		));
 		$vo_collection->insert();
 		$vo_collection->addLabel(
 			array(
 				'name' => $ps_idnoBase
 			),
-			$this->opo_locale->getPrimaryKey(),
+			self::$opo_locale->getPrimaryKey(),
 			null,
 			true
 		);
-		$this->opa_collections[$ps_idnoBase] = $vo_collection;
+		self::$opa_collections[$ps_idnoBase] = $vo_collection;
 		return $vo_collection;
 	}
 
-	private function _createObject($ps_idnoBase, $ps_individualCount) {
+	private static function _createObject($ps_idnoBase, $ps_individualCount) {
 		$vo_object = new ca_objects();
 		$vo_object->setMode(ACCESS_WRITE);
-		$vn_testObjectListItemId = $this->opa_listItems['test_object']->getPrimaryKey();
+		$vn_testObjectListItemId = self::$opa_listItems['test_object']->getPrimaryKey();
 		$vo_object->set(array(
-			'idno' => $this->_getIdno($ps_idnoBase),
+			'idno' => self::_getIdno($ps_idnoBase),
 			'type_id' => $vn_testObjectListItemId
 		));
 		// TODO Hardcoded attribute name
 		$vo_object->addMetadataElementToType('individualCount', $vn_testObjectListItemId);
 		$vo_object->addAttribute(array( 'individualCount' => $ps_individualCount ), 'individualCount');
 		$vo_object->insert();
-		$this->opa_objects[$ps_idnoBase] = $vo_object;
+		self::$opa_objects[$ps_idnoBase] = $vo_object;
 		return $vo_object;
 	}
 
-	private function _getCollectionRelationshipCount($po_object, $ps_collectionIdno) {
+	/**
+	 * @param $po_object BundlableLabelableBaseModelWithAttributes
+	 * @param $ps_collectionIdno string
+	 * @return bool
+	 */
+	private static function _getCollectionRelationshipCount($po_object, $ps_collectionIdno) {
 		$va_collections = $po_object->getRelatedItems('ca_collections', array(
 			'restrict_to_types' => array( 'ca_collections' ),
-			'where' => array( 'idno' => $this->_getIdno($ps_collectionIdno) )
+			'where' => array( 'idno' => self::_getIdno($ps_collectionIdno) )
 		));
-		return is_array($va_collections) && sizeof($va_collections) > 0;
+		return is_array($va_collections) ? sizeof($va_collections) : 0;
 	}
 
 }
