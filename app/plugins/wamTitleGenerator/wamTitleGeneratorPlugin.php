cc39a1aacbe7823be8b18eb55c63a1f25b143b3b
leftclickben <ben@leftclick.com.au>
2014-05-19 17:24:37 +0800

    Change structure of title generator plugin.
    + Use bundle hooks instead of label hooks, this enables modification of labels
      even when the user isn't adding or editing a label.
    + Labels are being removed and then re-added as editLabel() does not seem to
      work as expected.

--- app/plugins/wamTitleGenerator/wamTitleGeneratorPlugin.php
+++ app/plugins/wamTitleGenerator/wamTitleGeneratorPlugin.php
@@ -25,60 +25,80 @@
  *
  * ----------------------------------------------------------------------
  */
-	require_once(__CA_APP_DIR__.'/helpers/displayHelpers.php');
+require_once(__CA_APP_DIR__.'/helpers/displayHelpers.php');
 
-	class wamTitleGeneratorPlugin extends BaseApplicationPlugin {
-		# -------------------------------------------------------
-		private $opo_config;
-		# -------------------------------------------------------
-		public function __construct($ps_plugin_path) {
-			$this->description = _t('Generates title based upon rules you specify in the configuration file associated with the plugin');
-			parent::__construct();
-			$this->opo_config = Configuration::load($ps_plugin_path.'/conf/wamTitleGenerator.conf');
-		}
-		# -------------------------------------------------------
-		/**
-		 * Override checkStatus() to return true - the wamTitleGeneratorPlugin plugin always initializes ok
-		 */
-		public function checkStatus() {
-			return array(
-				'description' => $this->getDescription(),
-				'errors' => array(),
-				'warnings' => array(),
-				'available' => ((bool)$this->opo_config->get('enabled'))
-			);
-		}
-		# -------------------------------------------------------
-		/**
-		 * Generate title on save
-		 */
-		public function hookBeforeLabelInsert(&$pa_params) {
-			$this->_rewriteLabel($pa_params);
+class wamTitleGeneratorPlugin extends BaseApplicationPlugin {
+	# -------------------------------------------------------
+	/** @var Configuration */
+	private $opo_config;
+
+	private static $opa_processed_records = array();
+	# -------------------------------------------------------
+	public function __construct($ps_plugin_path) {
+		parent::__construct();
+		$this->description = _t('Generates title based upon rules you specify in the configuration file associated with the plugin');
+		$this->opo_config = Configuration::load($ps_plugin_path.'/conf/wamTitleGenerator.conf');
+	}
+	# -------------------------------------------------------
+	/**
+	 * Override checkStatus() to return true - the wamTitleGeneratorPlugin plugin always initializes ok
+	 */
+	public function checkStatus() {
+		return array(
+			'description' => $this->getDescription(),
+			'errors' => array(),
+			'warnings' => array(),
+			'available' => ((bool)$this->opo_config->get('enabled'))
+		);
+	}
+	# -------------------------------------------------------
+	public static function getRoleActionList(){
+		return array();
+	}
+	# -------------------------------------------------------
+	public function hookAfterBundleInsert(&$pa_params) {
+		return $this->_rewriteLabel($pa_params);
+	}
+
+	public function hookAfterBundleUpdate(&$pa_params) {
+		return $this->_rewriteLabel($pa_params);
+	}
+	# -------------------------------------------------------
+	private function _rewriteLabel(&$pa_params) {
+
+		$vs_table_name = $pa_params['table_name'];
+		$vn_id = $pa_params['id'];
+
+		if (isset(self::$opa_processed_records[$vs_table_name]) && isset(self::$opa_processed_records[$vs_table_name][$vn_id]) && self::$opa_processed_records[$vs_table_name][$vn_id]) {
 			return $pa_params;
 		}
-		public function hookBeforeLabelUpdate(&$pa_params) {
-			$this->_rewriteLabel($pa_params);
-			
-			return $pa_params;
-		}
-		# -------------------------------------------------------
-		private function _rewriteLabel(&$pa_params) {
-			$va_formatters = $this->opo_config->getAssoc('title_formatters');
-			if(isset($va_formatters[$pa_params['table_name']]) && isset($va_formatters[$pa_params['table_name']][$pa_params['instance']->getTypeCode()])){
-				$va_templates = $va_formatters[$pa_params['table_name']][$pa_params['instance']->getTypeCode()];
-				foreach ($va_templates as $vs_label_type => $ps_template) {
-					$vs_new_label = caProcessTemplateForIDs($ps_template, $pa_params['table_name'], array($pa_params['id']), array());
-					$pa_params['label_instance']->set($vs_label_type, $vs_new_label);
+
+		/** @var BundlableLabelableBaseModelWithAttributes $vo_instance */
+		$vo_instance = $pa_params['instance'];
+		$vo_instance->setMode(ACCESS_WRITE);
+
+		$va_formatters = $this->opo_config->getAssoc('title_formatters');
+		if (isset($va_formatters[$vs_table_name]) && isset($va_formatters[$vs_table_name][$vo_instance->getTypeCode()])) {
+			$va_templates = $va_formatters[$vs_table_name][$vo_instance->getTypeCode()];
+			foreach ($va_templates as $vs_label_type => $ps_template) {
+				$vs_new_label = caProcessTemplateForIDs($ps_template, $vs_table_name, array( $vn_id ));
+				error_log('new label = ' . $vs_new_label);
+				if ($vo_instance->getPreferredLabelCount() > 0) {
+					error_log('removing existing label: ' . $vo_instance->getPreferredLabelID(1));
+					$vo_instance->removeLabel($vo_instance->getPreferredLabelID(1));
 				}
+				error_log('adding new label');
+				$vo_instance->addLabel(array( $vs_label_type => $vs_new_label ), 1, null, true);
 			}
 		}
-		/**
-		 * Need to specify which actions are overrideable by permissions
-		 * @return [type] [description]
-		 */
-		public static function getRoleActionList(){
-			return array();
+
+		if (!isset(self::$opa_processed_records[$vs_table_name])) {
+			self::$opa_processed_records[$vs_table_name] = array();
 		}
-# -------------------------------------------------------
-  }
-  ?>
+		self::$opa_processed_records[$vs_table_name][$vn_id] = true;
+
+		$vo_instance->update();
+		return $pa_params;
+	}
+	# -------------------------------------------------------
+}
