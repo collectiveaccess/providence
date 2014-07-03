20e579d7540ca718fdf69c880c1c02d490ce2611
Kehan Harman <kehan@gaiaresources.com.au>
2014-07-03 11:53:53 +0800

    Change opb_supports_related_entities to opb_supports_relationships

--- app/refineries/objectRepresentationSplitter/objectRepresentationSplitterRefinery.php
+++ app/refineries/objectRepresentationSplitter/objectRepresentationSplitterRefinery.php
@@ -36,7 +36,7 @@
 			$this->ops_description = _t('Splits object representations');
 			
 			$this->opb_returns_multiple_values = true;
-			$this->opb_supports_related_entities = true;
+			$this->opb_supports_relationships = true;
 			
 			parent::__construct();
 		}
