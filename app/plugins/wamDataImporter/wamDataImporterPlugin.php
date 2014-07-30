0387c5dca70c7150c2e544daf6cca0da7edd656a
Kehan Harman <kehan@gaiaresources.com.au>
2014-07-30 15:18:12 +0800

    Change importer plugin to process list item attributes and work on attribtues as well as interstitials

--- app/plugins/wamDataImporter/wamDataImporterPlugin.php
+++ app/plugins/wamDataImporter/wamDataImporterPlugin.php
@@ -62,66 +62,169 @@ class wamDataImporterPlugin extends BaseApplicationPlugin {
 	 * 'log' => &$o_log
 	 * 'reader' => $o_reader
 	 * 'environment' => $va_environment
+	 * @return array
 	 */
 	public function hookDataImportContentTree(&$pa_params){
-		global $g_ui_locale_id;
+		foreach ($pa_params['content_tree'] as $vs_table_name => $va_table_content_tree) {
 
-		foreach ($pa_params['content_tree'] as $table_name => $table_content_tree) {
-			foreach ($table_content_tree as $table_content_index => $table_content) {
-				if (isset($table_content['_interstitial']) && isset($table_content['_interstitial']['_translations'])) {
-					// Apply all translations
-					foreach ($table_content['_interstitial']['_translations'] as $name => $translation_settings) {
-						$translation_settings = json_decode($translation_settings, true);
-						if (isset($table_content['_interstitial'][$name])) {
-							if (isset($translation_settings['delimiters'])) {
-								// Ensure we have an array
-								if (!is_array($translation_settings['delimiters'])) {
-									$translation_settings['delimiters'] = array( $translation_settings['delimiters'] );
-								}
-								// Quote the delimiters for preg_split
-								$translation_settings['delimiters'] = array_map(
-										function ($delimiter) {
-											return preg_quote($delimiter, '!');
-										},
-										$translation_settings['delimiters']
-								);
-								// Split the value based on given delimiters
-								$table_content['_interstitial'][$name] = preg_split("!(".join("|", $translation_settings['delimiters']).")!", $table_content['_interstitial'][$name]);
-							}
-							foreach ($table_content['_interstitial'][$name] as $value_index => $value) {
-								switch ($translation_settings['type']) {
-									case 'ca_entities':
-										$table_content['_interstitial'][$name][$value_index] = DataMigrationUtils::getEntityID(
-												DataMigrationUtils::splitEntityName($value),
-												'ind',
-												$g_ui_locale_id,
-												null,
-												array( 'matchOnDisplayName' => true )
-										);
-										break;
-
-									default:
-										if (isset($pa_params['log'])) {
-											$pa_params['log']->logError(sprintf('Unknown interstitial translation type "%s" on "%s" for idno "%s"', $translation_settings['type'], $name, $pa_params['idno']));
-										}
-								}
-							}
-						} else {
-							if (isset($pa_params['log'])) {
-								$pa_params['log']->logError(sprintf('Unknown interstitial name "%s" specified in translation of type "%s" for idno "%s"', $name, $translation_settings['type'], $pa_params['idno']));
-							}
-						}
-					}
-
-					// Save the translated value back into the content tree
-					$pa_params['content_tree'][$table_name][$table_content_index] = $table_content;
-
-					// Remove translations special key so it is not added as an interstitial
-					unset($pa_params['content_tree'][$table_name][$table_content_index]['_interstitial']['_translations']);
+			foreach ($va_table_content_tree as $vs_table_content_index => $va_table_content) {
+				$this->_processBundles($pa_params, $va_table_content, $vs_table_name, $vs_table_content_index);
+				if(isset($va_table_content['_interstitials'])){
+					$this->_processBundles($pa_params, $va_table_content, $vs_table_name, $vs_table_content_index, '_interstitials');
 				}
 			}
 		}
 
 		return $pa_params;
+	}
+
+	/**
+	 * @param $pa_params array parameters passed to the data importer plugin
+	 * @param $pa_table_content
+	 * @param $ps_table_name string the target table name
+	 * @param $pn_table_content_index int
+	 * @param null/string $ps_root_element the root of the table content array to process
+	 * @return array
+	 */
+	protected function _processBundles(&$pa_params, $pa_table_content, $ps_table_name, $pn_table_content_index, $ps_root_element = null) {
+		/** @var KLogger $o_log */
+		$o_log = (isset($pa_options['log']) && is_object($pa_options['log'])) ? $pa_options['log'] : null;
+		$o_import_event = caGetOption('importEvent', $pa_params, null);
+		$va_event_source = caGetOption('importEventSource', $pa_params, null);
+		// intertitials have their values stored under an _interstitials key, whereas ordinary attributes have them stored under the root.
+		$va_table_content = isset($ps_root_element) && isset($pa_table_content[$ps_root_element]) ? $pa_table_content[$ps_root_element] : $pa_table_content;
+
+		if (isset($va_table_content) && isset($va_table_content['_translations'])) {
+			// Apply all translations
+			foreach ($va_table_content['_translations'] as $vs_name => $vs_translation_settings_json) {
+
+				$va_translation_settings = json_decode($vs_translation_settings_json, true);
+
+
+				if(json_last_error() !== JSON_ERROR_NONE){
+					if($o_log){
+						$o_log->logError(sprintf('Failed to parse JSON string "%s" while proccessing "%s" for idno "%s"', $vs_translation_settings_json,  $vs_name, $pa_params['idno']));
+					}
+					continue;
+				}
+				if (isset($va_table_content[$vs_name])) {
+					if (isset($va_translation_settings['delimiters'])) {
+						// Ensure we have an array
+						if (!is_array($va_translation_settings['delimiters'])) {
+							$va_translation_settings['delimiters'] = array($va_translation_settings['delimiters']);
+						}
+						// Quote the delimiters for preg_split
+						$va_translation_settings['delimiters'] = array_map(
+								function ($delimiter) {
+									return preg_quote($delimiter, '!');
+								},
+								$va_translation_settings['delimiters']
+						);
+						// Split the value based on given delimiters
+						$va_table_content[$vs_name] = preg_split("!(" . join("|", $va_translation_settings['delimiters']) . ")!", $va_table_content[$vs_name]);
+					}
+					foreach ($va_table_content[$vs_name] as $value_index => $value) {
+						if(isset($va_translation_settings['element']) && $va_translation_settings['element'] !== $value_index){
+							//We only want to set the value of a specific element therefore we can skip this value
+							continue;
+						}
+						switch ($va_translation_settings['table']) {
+							case 'ca_entities':
+								$this->_processEntityBundle($va_table_content, $va_translation_settings, $value, $vs_name, $value_index, $o_import_event, $va_event_source);
+								break;
+							case 'ca_list_items':
+								$this->_processListItemBundle($va_table_content, $va_translation_settings, $value, $vs_name, $value_index, $o_import_event, $va_event_source, $pa_params, $o_log);
+								break;
+
+							default:
+								if ($o_log) {
+									$o_log->logError(sprintf('Unknown "%s" translation type "%s" on "%s" for idno "%s"', $va_translation_settings['table'], $vs_name, $pa_params['idno']));
+								}
+						}
+					}
+				} else {
+					if ($o_log) {
+						$o_log->logError(sprintf('Unknown "%s" name "%s" specified in translation of type "%s" for idno "%s"',  $vs_name, $va_translation_settings['table'], $pa_params['idno']));
+					}
+				}
+			}
+
+			// Save the translated value back into the content tree
+			if(isset($ps_root_element)){
+				$pa_params['content_tree'][$ps_table_name][$pn_table_content_index][$ps_root_element] = $va_table_content;
+				// Remove translations special key so it is not added as an interstitial
+				unset($pa_params['content_tree'][$ps_table_name][$pn_table_content_index][$ps_root_element]['_translations']);
+			} else {
+				$pa_params['content_tree'][$ps_table_name][$pn_table_content_index] = $va_table_content;
+				// Remove translations special key so it is not added as an interstitial
+				unset($pa_params['content_tree'][$ps_table_name][$pn_table_content_index]['_translations']);
+			}
+		}
+	}
+
+	/**
+	 * @param $table_content array
+	 * @param $translation_settings array
+	 * @param $value mixed
+	 * @param $vs_name string name
+	 * @param $value_index int
+	 * @param null/ca_data_import_events $po_import_event
+	 * @param null/array $pa_import_event_source
+	 */
+	protected function _processEntityBundle(&$table_content, $translation_settings, $value, $vs_name, $value_index, $po_import_event = null, $pa_import_event_source = null) {
+		global $g_ui_locale_id;
+		$vs_entity_type = caGetOption('entityType', $translation_settings, 'ind');
+		$table_content[$vs_name][$value_index] = DataMigrationUtils::getEntityID(
+				DataMigrationUtils::splitEntityName($value),
+				$vs_entity_type,
+				$g_ui_locale_id,
+				null,
+				array('matchOnDisplayName' => true, 'importEvent' => $po_import_event, 'importEventSource' => $pa_import_event_source)
+		);
+	}
+
+	/**
+	 * @param $pa_table_content array
+	 * @param $pa_translation_settings array
+	 * @param $value mixed
+	 * @param $vs_name string name
+	 * @param $value_index int
+	 * @param $o_import_event null/ca_data_import_events
+	 * @param $pa_event_source null/array
+	 * @param $pa_params array
+	 * @param $o_log null/KLogger
+	 * @internal int $g_ui_locale_id
+	 */
+	protected function _processListItemBundle(&$pa_table_content, $pa_translation_settings, $value, $vs_name, $value_index, $o_import_event, $pa_event_source, &$pa_params, $o_log) {
+		global $g_ui_locale_id;
+		$vs_list_code = caGetOption('list', $pa_translation_settings);
+		if (!isset($vs_list_code) && $o_log) {
+			$o_log->logError(sprintf('No list set in "%s" translation type "%s" on "%s" for idno "%s"', $pa_translation_settings['table'], $vs_name, $pa_params['idno']));
+			return;
+		}
+		$vn_list_id = caGetListID($vs_list_code);
+		if (!$vn_list_id) {
+			// No list = bail!
+			if ($o_log) {
+				$o_log->logError(_t('[_proccesListItemBundle] Could not find list %1; item was skipped', $vs_list_code));
+			}
+			return;
+		}
+		//We almost always want 'concepts'
+		$ps_type = caGetOption('type', $pa_translation_settings, 'concept');
+		$va_attr_vals_with_parent = is_array($value) ? $value : array('preferred_labels' => array('name_singular' => $value));
+		$vn_parent_id = caGetOption('parent_id', $pa_params, caGetListRootID($vs_list_code));
+
+		$va_attr_vals_with_parent['parent_id'] = $vn_parent_id;
+		$va_attr_vals_with_parent['is_enabled'] = 1;
+		$vs_item_idno = caGetOption('idno', $va_attr_vals_with_parent, is_array($value) ? null : preg_replace('/\W+/', '_', $value));
+
+		$pa_options = array(
+				'matchOn' => array('label', 'idno'),
+				'importEvent' => $o_import_event,
+				'importEventSource' => $pa_event_source,
+		);
+
+		$pa_table_content[$vs_name][$value_index] = DataMigrationUtils::getListItemID($vs_list_code, $vs_item_idno, $ps_type, $g_ui_locale_id, $va_attr_vals_with_parent, $pa_options);
 	}
 }
