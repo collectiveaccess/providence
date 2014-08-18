ded2a59e2faea6d114dd0718c25ab7c70c2b5681
Seth <seth@collectiveaccess.org>
2014-08-18 08:47:52 -0400

    PROV-1010 implemented current disposition/location browse for objects

--- support/sql/migrations/106.sql
+++ support/sql/migrations/106.sql
@@ -4,10 +4,12 @@
 	Description: add current location fields to ca_objects
 */
 
-alter table ca_objects add column current_loc_type tinyint unsigned null;
+alter table ca_objects add column current_loc_class tinyint unsigned null;
+alter table ca_objects add column current_loc_subclass int unsigned null;
 alter table ca_objects add column current_loc_id int unsigned null;
 
-create index i_current_loc_type on ca_objects(current_loc_type);
+create index i_current_loc_class on ca_objects(current_loc_class);
+create index i_current_loc_subclass on ca_objects(current_loc_subclass);
 create index i_current_loc_id on ca_objects(current_loc_id);
 
 /* Always add the update to ca_schema_updates at the end of the file */
