e764e9924265d7fd0035ea59a44293c1ceb229da
Stefan <stefan@whirl-i-gig.com>
2014-09-09 09:42:22 +0200

    PROV-1032 Changed representation annotation property interface to include a way to get a display value for all properties.
    This is very useful for export where you don’t necessarily know what
    kind of annotation you have at hand and want a list of properties
    anyway. This is extensible enough so that we could add display format
    options that are passed down to the coders if we need to.
    
    This is a new function in the ca_representation_annotations BaseModel
    API but can also be accessed using a ‘virtual’ property called
    ‘display’ like so:
    
    $t_ann->get('ca_representation_annotations.props.display')
    
    This makes it usable in display templates and the exporter.

--- app/lib/ca/RepresentationAnnotationPropertyCoders/IRepresentationAnnotationPropertyCoder.php
+++ app/lib/ca/RepresentationAnnotationPropertyCoders/IRepresentationAnnotationPropertyCoder.php
@@ -76,5 +76,8 @@
 		
 		/* Returns name of property to be used to sort annotations in a list */
 		public function getAnnotationSortProperty($ps_type=null);
+
+		/* Get this annotation as a text-style display. Should always include all relevant properties. */
+		public function getPropertiesForDisplay();
 	}
- ?>
+?>
