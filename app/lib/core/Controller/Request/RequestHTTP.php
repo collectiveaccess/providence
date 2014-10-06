4e4b9fff9a2118f8ab2d861fc3ff04dab093a88c
leftclickben <ben@leftclick.com.au>
2014-10-06 16:13:49 +0800

    Use redirects during login process.
    When loading a page while not authenticated, the user is now
    redirected to the login page (`system/auth/login`), with a
    `redirect` URL parameter set to the (encoded) URL of the original
    page.  This `redirect` parameter is passed through any number of
    unsuccessful login attempts, each of which loads the URL
    `system/auth/login` with the `redirect` parameter.  Following a
    successful login, the user is taken back to the URL given by the
    `redirect` parameter (i.e. the original URL), or to the dashboard
    if no `redirect` parameter is given.
    
    Previously, the login form was rendered "at" the original URL, and
    this did not play nicely after a failed authentication attempt -
    the user would always be taken back to the dashboard.
    
    This is also better from a pure URL management perspective -- each
    URL has a single purpose and that purpose is never subverted by the
    login mechanism (only diverted).

--- app/lib/core/Controller/Request/RequestHTTP.php
+++ app/lib/core/Controller/Request/RequestHTTP.php
@@ -586,7 +586,7 @@ class RequestHTTP extends Request {
 		
 		foreach(array(
 			'no_headers', 'dont_redirect_to_login', 'dont_create_new_session', 'dont_redirect_to_welcome',
-			'user_name', 'password', 'options', 'noPublicUsers', 'dont_redirect', 'no_headers'
+			'user_name', 'password', 'options', 'noPublicUsers', 'dont_redirect', 'no_headers', 'redirect'
 		) as $vs_key) {
 			if (!isset($pa_options[$vs_key])) { $pa_options[$vs_key] = null; }
 		}
@@ -646,7 +646,14 @@ class RequestHTTP extends Request {
 				if (!$vb_login_successful) {																	// throw user to login screen
 					if (!$pa_options["dont_redirect_to_login"]) {
 						$o_event_log->log(array("CODE" => "LOGF", "SOURCE" => "Auth", "MESSAGE" => "Failed login with redirect for user id '".$vn_user_id."' (".$_SERVER['REQUEST_URI']."); IP=".$_SERVER["REMOTE_ADDR"]."; user agent='".$_SERVER["HTTP_USER_AGENT"]."'"));
-						$this->opo_response->addHeader("Location", $this->getBaseUrlPath().'/'.$this->getScriptName().'/'.$this->config->get("auth_login_path"));
+						$vs_redirect = $this->getRequestUrl(true);
+
+						if (strpos($vs_redirect, $this->config->get("auth_login_path") !== -1)) {
+							$vs_redirect = '';
+						} else {
+							$vs_redirect = '?redirect=' . urlencode($vs_redirect);
+						}
+						$this->opo_response->addHeader("Location", $this->getBaseUrlPath().'/'.$this->getScriptName().'/'.$this->config->get("auth_login_path") . $vs_redirect);
 					}
 					return false;
 				}
@@ -701,7 +708,9 @@ class RequestHTTP extends Request {
 			
 			//$this->user->close(); ** will be called externally **
 			$AUTH_CURRENT_USER_ID = $vn_user_id;
-			if (!$pa_options["dont_redirect_to_welcome"]) {
+			if ($pa_options['redirect']) {
+				$this->opo_response->addHeader("Location", $pa_options['redirect']);
+			} elseif (!$pa_options["dont_redirect_to_welcome"]) {
 				//header("Location: ".$this->getBaseUrlPath().'/'.$this->getScriptName().'/'.$this->config->get("auth_login_welcome_path"));				// redirect to "welcome" page
 				$this->opo_response->addHeader("Location", $this->getBaseUrlPath().'/'.$this->getScriptName().'/'.$this->config->get("auth_login_welcome_path"));
 				//exit;
