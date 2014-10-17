126365adce15ea06de8693ae537f6372d0e45d90
Seth <seth@inquisite.org>
2014-10-16 22:05:32 -0400

    NOJIRA don't set defaults for text fields

--- support/sql/migrations/110.sql
+++ support/sql/migrations/110.sql
@@ -4,8 +4,8 @@
 	Description: pseudo-migration to notify users of authentication backend rewrite
 */
 
-alter table ca_representation_annotation_labels modify column name text not null default '';
-alter table ca_representation_annotation_labels modify column name_sort text not null default '';
+alter table ca_representation_annotation_labels modify column name text not null;
+alter table ca_representation_annotation_labels modify column name_sort text not null;
 
 /* Always add the update to ca_schema_updates at the end of the file */
 INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (110, unix_timestamp());
