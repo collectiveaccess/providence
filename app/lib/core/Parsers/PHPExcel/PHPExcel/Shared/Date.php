c418aebaa57d3533b716652be41730fdf5d5f90c
Seth <seth@whirl-i-gig.com>
2014-10-30 19:45:49 -0400

    NOJIRA restore dont-convert-timezone behavior removed in merge

--- app/lib/core/Parsers/PHPExcel/PHPExcel/Shared/Date.php
+++ app/lib/core/Parsers/PHPExcel/PHPExcel/Shared/Date.php
@@ -183,8 +183,8 @@ class PHPExcel_Shared_Date
 	 *							or boolean FALSE on failure
 	 */
 	public static function PHPToExcel($dateValue = 0, $adjustToTimezone = FALSE, $timezone = NULL) {
-		$saveTimeZone = date_default_timezone_get();
-		date_default_timezone_set('UTC');
+		//$saveTimeZone = date_default_timezone_get();
+		//date_default_timezone_set('UTC');
 		$retValue = FALSE;
 		if ((is_object($dateValue)) && ($dateValue instanceof DateTime)) {
 			$retValue = self::FormattedPHPToExcel( $dateValue->format('Y'), $dateValue->format('m'), $dateValue->format('d'),
@@ -195,7 +195,7 @@ class PHPExcel_Shared_Date
 												   date('H',$dateValue), date('i',$dateValue), date('s',$dateValue)
 												 );
 		}
-		date_default_timezone_set($saveTimeZone);
+		//date_default_timezone_set($saveTimeZone);
 
 		return $retValue;
 	}	//	function PHPToExcel()
