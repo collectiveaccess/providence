fba85217c8e6ff403b1e8b1c1d21aca2cc96905f
Seth <seth@whirl-i-gig.com>
2015-01-14 22:36:21 -0500

    NOJIRA fix type-based access issues: 	1. Restricting all types for a table to no access would prevent any restriction from happening 	2. Quicksearch now requires "edit" action for a table to search it

--- app/lib/core/Search/SearchEngine.php
+++ app/lib/core/Search/SearchEngine.php
@@ -241,32 +241,39 @@ class SearchEngine extends SearchBase {
 				$va_access_values = $pa_options['checkAccess'];
 				$this->addResultFilter($this->ops_tablename.'.access', 'IN', join(",",$va_access_values));
 			} 
-					
+			
+			$vb_no_types = false;	
 			if (is_array($va_type_ids = $this->getTypeRestrictionList()) && sizeof($va_type_ids)) {
 				if ($t_table->getFieldInfo('type_id', 'IS_NULL')) {
 					$va_type_ids[] = 'NULL';
 				}
 				$this->addResultFilter($this->ops_tablename.'.type_id', 'IN', join(",",$va_type_ids));
+			} else {
+				$vb_no_types = true;		
 			}
 			
-			// Filter on source
-			if (is_array($va_source_ids = $this->getSourceRestrictionList())) {
-				$this->addResultFilter($this->ops_tablename.'.source_id', 'IN', join(",",$va_source_ids));
+			if (!$vb_no_types) {
+				// Filter on source
+				if (is_array($va_source_ids = $this->getSourceRestrictionList())) {
+					$this->addResultFilter($this->ops_tablename.'.source_id', 'IN', join(",",$va_source_ids));
+				}
+			
+				if (in_array($t_table->getHierarchyType(), array(__CA_HIER_TYPE_SIMPLE_MONO__, __CA_HIER_TYPE_MULTI_MONO__))) {
+					$this->addResultFilter($this->ops_tablename.'.parent_id', 'IS NOT', NULL);
+				}
+			
+				if (is_array($va_restrict_to_fields = caGetOption('restrictSearchToFields', $pa_options, null)) && $this->opo_engine->can('restrict_to_fields')) {
+					$this->opo_engine->setOption('restrictSearchToFields', $va_restrict_to_fields);
+				}
+			
+				$o_res =  $this->opo_engine->search($this->opn_tablenum, $vs_search, $this->opa_result_filters, $o_rewritten_query);
+			
+				// cache the results
+				$va_hits = $o_res->getPrimaryKeyValues($vn_limit);
+				$o_res->seek(0);
+			} else {
+				$va_hits = array();			
 			}
-			
-			if (in_array($t_table->getHierarchyType(), array(__CA_HIER_TYPE_SIMPLE_MONO__, __CA_HIER_TYPE_MULTI_MONO__))) {
-				$this->addResultFilter($this->ops_tablename.'.parent_id', 'IS NOT', NULL);
-			}
-			
-			if (is_array($va_restrict_to_fields = caGetOption('restrictSearchToFields', $pa_options, null)) && $this->opo_engine->can('restrict_to_fields')) {
-				$this->opo_engine->setOption('restrictSearchToFields', $va_restrict_to_fields);
-			}
-			
-			$o_res =  $this->opo_engine->search($this->opn_tablenum, $vs_search, $this->opa_result_filters, $o_rewritten_query);
-			
-			// cache the results
-			$va_hits = $o_res->getPrimaryKeyValues($vn_limit);
-			$o_res->seek(0);
 
 			if (isset($pa_options['sets']) && $pa_options['sets']) {
 				$va_hits = $this->filterHitsBySets($va_hits, $pa_options['sets'], array('search' => $vs_search));
