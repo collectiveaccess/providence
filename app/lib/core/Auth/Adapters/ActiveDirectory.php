c9cd81207333cf021930d209439859ed2a1f96f9
Seth <seth@collectiveaccess.org>
2015-03-12 10:24:25 -0400

    NOJIRA More active directory fixes

--- app/lib/core/Auth/Adapters/ActiveDirectory.php
+++ app/lib/core/Auth/Adapters/ActiveDirectory.php
@@ -195,7 +195,7 @@ class ActiveDirectoryAuthAdapter extends BaseAuthAdapter implements IAuthAdapter
 
 		$va_attrs = ldap_get_attributes($po_ldap, $vo_entry);
 		if(is_array($va_group_cn) && sizeof($va_group_cn)>0) {
-			if (sizeof(array_intersect($va_group_cn, $va_attrs[$vs_attribute_member_of])) === 0) {
+			if (sizeof(array_intersect(array_map('strtolower', $va_group_cn), $va_attrs[$vs_attribute_member_of])) === 0) {
 				// user is not in any relevant groups
 				ldap_unbind($po_ldap);
 				return false;
@@ -211,7 +211,8 @@ class ActiveDirectoryAuthAdapter extends BaseAuthAdapter implements IAuthAdapter
 			case __CA_AUTH_ADAPTER_FEATURE_RESET_PASSWORDS__:
 			case __CA_AUTH_ADAPTER_FEATURE_UPDATE_PASSWORDS__:
 			default:
-				return false;
+				#return false;
+				return true; // TODO: temporary hack to allow display of password change fields in user auth config for fallback logins
 		}
 	}
 	# --------------------------------------------------------------------------------
