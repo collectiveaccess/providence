0af8efb19ed2edd04561071362c7e2f7f264a3ff
Seth <seth@collectiveaccess.org>
2015-03-24 23:28:20 -0400

    NOJIRA bump TimelineJS version

--- assets/timelinejs/src/js/VMM.TImeline.Min.js
+++
@@ -1,8 +0,0 @@
-// VMM.Timeline.Min.js
-
-/* 	CodeKit Import
-	http://incident57.com/codekit/
-================================================== */
-// @codekit-prepend "VMM.Timeline.js";
-
-VMM.debug = false;
