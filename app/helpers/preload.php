934695bdb9a89a186ca15ea35890f3cb283bd1af
Stefan <stefan@whirl-i-gig.com>
2015-04-02 16:39:34 -0400

    PROV-1207 Add garbage collector

--- app/helpers/preload.php
+++ app/helpers/preload.php
@@ -71,5 +71,7 @@ require_once(__CA_MODELS_DIR__."/ca_acl.php");
 require_once(__CA_LIB_DIR__."/core/Cache/ExternalCache.php");
 require_once(__CA_LIB_DIR__."/core/Cache/CompositeCache.php");
 
+require_once(__CA_APP_DIR__.'/lib/ca/GarbageCollection.php');
+
 // initialize Tooltip manager
 TooltipManager::init();
--- index.php
+++ index.php
@@ -56,6 +56,9 @@
 		}
 		exit();
 	}
+
+	// run garbage collector
+	GarbageCollection::gc();
 	
 	$app = AppController::getInstance();
 	
