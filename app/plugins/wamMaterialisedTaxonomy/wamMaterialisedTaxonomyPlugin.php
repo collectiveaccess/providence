16decfe17f461b98b24d80f15c119eebc81e0bf6
Kehan Harman <kehan@gaiaresources.com.au>
2015-04-30 17:20:23 +0800

    Interim commit - now we can pull the attribute value out
    * Still need to save them

--- app/plugins/wamMaterialisedTaxonomy/wamMaterialisedTaxonomyPlugin.php
+++ app/plugins/wamMaterialisedTaxonomy/wamMaterialisedTaxonomyPlugin.php
@@ -26,7 +26,10 @@
  * ----------------------------------------------------------------------
  */
 
+require_once(__CA_MODELS_DIR__."/ca_list_items.php");
 require_once(__CA_LIB_DIR__."/core/Datamodel.php");
+require_once(__CA_LIB_DIR__.'/core/Logging/Eventlog.php');
+require_once(__CA_LIB_DIR__.'/core/Db.php');
 
 /**
  * The WAM Materialised Taxonomy plugin keeps copies of ancestral taxonomy ranks in child level names for easy access
@@ -64,7 +67,64 @@ class wamMaterialisedTaxonomyPlugin extends BaseApplicationPlugin {
 	 *
 	 */
 	public function hookPeriodicTask(&$pa_params) {
-		caDebug($this->opo_config, 'config', true);
+//		caDebug($this->opo_config->getAssoc('tables'), 'config', true);
+
+		if ($va_table_config = $this->opo_config->getAssoc('tables')){
+			$t_log = new Eventlog();
+			$o_db = new Db();
+			$o_dm = Datamodel::load();
+			$t_types = new ca_list_items();
+
+			foreach($va_table_config as $vs_table_name => $va_type_info){
+//				caDebug($va_type_info, $vs_table_name, true);
+				/** @var BundlableLabelableBaseModelWithAttributes $t_table */
+				$t_table = $o_dm->getTableInstance($vs_table_name);
+				$vs_type_field = $t_table->getTypeFieldName();
+				$vo_type_list = $t_table->getTypeList();
+
+				foreach($va_type_info as $vs_type => $va_type_to_attribute_map){
+					$vn_type_id = $t_table->getTypeIDForCode($vs_type);
+					/** @var ca_list_items $vo_type_l */
+					$vo_type_l = new ca_list_items($vn_type_id);
+					$vo_child_types = $vo_type_l->getHierarchy(null, array('idsOnly' => true));
+
+					$t_table->getTypeList();
+					$vo_type_instance = $t_table->getTypeList();
+//					caDebug($vo_type_instance->getHierarchyChildrenAsQuery(), 'types', true);
+					/** @var BaseSearchResult $vo_result */
+					$va_ids = $t_table->find(array($vs_type_field => $vo_child_types, 'deleted' => false), array('returnAs' => 'ids'));
+					foreach($va_ids as $vn_id){
+						$t_table->load($vn_id);
+						caDebug($t_table->getLabelForDisplay(false), 'name');
+						$va_new_values = array();
+						$va_ancestors = $t_table->getHierarchyAncestors($vn_id, array('idsOnly' => true));
+						/**
+						 * @var int $vn_ancestor_id
+						 * @var BundlableLabelableBaseModelWithAttributes $vo_ancestor_instance
+						 */
+						foreach($va_ancestors as $vn_ancestor_id){
+							$vo_ancestor_instance = new $vs_table_name($vn_ancestor_id);
+							if($vs_ancestor_type = $vo_ancestor_instance->getTypeCode()){
+								if(isset($va_type_to_attribute_map[$vs_ancestor_type])){
+									$vs_label = $vo_ancestor_instance->getLabelForDisplay(false);
+									$va_new_values[$va_type_to_attribute_map[$vs_ancestor_type]] = $vs_label;
+								}
+							}
+						}
+						if($va_new_values){
+							caDebug($va_new_values, 'new_values', true);
+							$t_table->setMode(ACCESS_WRITE);
+							foreach($va_new_values as $vs_field => $vs_value){
+								$t_table->replaceAttribute(array( $vs_value), $vs_field);
+							}
+							$t_table->update();
+						}
+					}
+				}
+
+
+			}
+		}
 		return true;
 	}
 }
