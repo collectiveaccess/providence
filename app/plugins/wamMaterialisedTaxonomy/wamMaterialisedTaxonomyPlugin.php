8d22c5312d57dbaf54f732d81d469ae3b999b4f9
Kehan Harman <kehan@gaiaresources.com.au>
2015-04-30 09:50:21 +0800

    Base skeleton of a plugin

---
+++ app/plugins/wamMaterialisedTaxonomy/wamMaterialisedTaxonomyPlugin.php
@@ -0,0 +1,61 @@
+<?php
+/* ----------------------------------------------------------------------
+ * wamMaterialisedTaxonomyPlugin.php : Ancestral taxonomy ranks included in child level names
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2015 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+
+require_once(__CA_LIB_DIR__."/core/Datamodel.php");
+
+/**
+ * The WAM Materialised Taxonomy plugin keeps copies of ancestral taxonomy ranks in child level names for easy access
+ */
+class wamMaterialisedTaxonomyPlugin extends BaseApplicationPlugin {
+
+	public function __construct($ps_plugin_path) {
+		parent::__construct();
+		$this->description = _t('Keeps copies of ancestral taxonomy ranks in child level names for easy access' );
+	}
+
+	public function checkStatus() {
+		return array(
+			'description' => $this->getDescription(),
+			'errors' => array(),
+			'warnings' => array(),
+			'available' => true
+		);
+	}
+
+	/**
+	 * Give everybody access to this plugin
+	 *
+	 * @return array of actions that can be assigned to roles
+	 */
+	public static function getRoleActionList(){
+		return array(
+		);
+	}
+
+
+}
