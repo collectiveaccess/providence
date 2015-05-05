097f06865b3e3869498e454062be34fa791d0001
HSP <seth@collectiveaccess.org>
2015-05-04 22:56:04 -0400

    NOJIRA fix export issue from get reimplementation change in return value; ca_objects.preferred_label with returnAsArray true used to return an array of arrays, each containing all fields in the label; new behavior is to return an array of display values unless the "assumeDisplayField" option is set to false; for export we want the full array so we now set "assumeDisplayField" here to false

--- app/lib/ca/LabelableBaseModelWithAttributes.php
+++ app/lib/ca/LabelableBaseModelWithAttributes.php
@@ -1220,7 +1220,7 @@
 			$t_list = new ca_lists();
 			
 			// get labels
-			$va_preferred_labels = $this->get($this->tableName().".preferred_labels", array('returnAsArray' => true, 'returnAllLocales' => true));
+			$va_preferred_labels = $this->get($this->tableName().".preferred_labels", array('returnAsArray' => true, 'returnAllLocales' => true, 'assumeDisplayField' => false));
 			
 			if(is_array($va_preferred_labels) && sizeof($va_preferred_labels)) {
 				$va_preferred_labels_for_export = array();
@@ -1236,7 +1236,7 @@
 				$va_data['preferred_labels'] = $va_preferred_labels_for_export;
 			}
 			
-			$va_nonpreferred_labels = $this->get($this->tableName().".nonpreferred_labels", array('returnAsArray' => true, 'returnAllLocales' => true));
+			$va_nonpreferred_labels = $this->get($this->tableName().".nonpreferred_labels", array('returnAsArray' => true, 'returnAllLocales' => true, 'assumeDisplayField' => false));
 			if(is_array($va_nonpreferred_labels) && sizeof($va_nonpreferred_labels)) {
 				$va_nonpreferred_labels_for_export = array();
 				foreach($va_nonpreferred_labels as $vn_id => $va_labels_by_locale) {
