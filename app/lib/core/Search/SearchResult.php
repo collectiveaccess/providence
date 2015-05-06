5ad035bf0b1460a5bac5eb33fc8aa793c007561e
Stefan <stefan@whirl-i-gig.com>
2015-05-06 15:11:37 -0400

    NOJIRA Fix fatal error

--- app/lib/core/Search/SearchResult.php
+++ app/lib/core/Search/SearchResult.php
@@ -236,6 +236,8 @@ class SearchResult extends BaseObject {
 		if (!($t_rel_instance = SearchResult::$s_instance_cache[$ps_tablename])) {
 			$t_rel_instance = SearchResult::$s_instance_cache[$ps_tablename] = $this->opo_datamodel->getInstanceByTableName($ps_tablename, true);
 		}
+		if(!($ps_tablename instanceof LabelableBaseModelWithAttributes)) { return; }
+
 		$vs_label_table = $t_rel_instance->getLabelTableName();
 		
 		if (!isset($this->opa_tables[$vs_label_table])) {
@@ -1193,7 +1195,7 @@ class SearchResult extends BaseObject {
 //
 // [PRIMARY TABLE] Preferred/nonpreferred labels
 //
-				if (in_array($va_path_components['field_name'], array('preferred_labels', 'nonpreferred_labels'))) {
+				if (in_array($va_path_components['field_name'], array('preferred_labels', 'nonpreferred_labels')) && ($t_instance instanceof LabelableBaseModelWithAttributes)) {
 					$vs_label_table_name = $t_instance->getLabelTableName();
 					if (!isset(self::$s_prefetch_cache[$vs_label_table_name][$vn_row_id])) {
 						$this->prefetchLabels($va_path_components['table_name'], $this->opo_engine_result->currentRow(), $this->getOption('prefetch'), $pa_options);
