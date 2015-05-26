1cb95b4130ff3024c9c42fb600f5b941e6442030
Stefan <stefan@whirl-i-gig.com>
2015-05-26 17:41:24 -0400

    NOJIRA Add option for InformationService implementations to change the display value and minimized display for Getty vocabularies
    Also no longer mandate a URL in the display value for the "More info"
    link to appear

--- app/lib/core/Plugins/InformationService/TGN.php
+++ app/lib/core/Plugins/InformationService/TGN.php
@@ -137,4 +137,19 @@ class WLPlugInformationServiceTGN extends BaseGettyLODServicePlugin implements I
 		return $va_return;
 	}
 	# ------------------------------------------------
+	/**
+	 * Get display value
+	 * @param string $ps_text
+	 * @return string
+	 */
+	public function getDisplayValueFromLookupText($ps_text) {
+		if(!$ps_text) { return ''; }
+		$va_matches = array();
+
+		if(preg_match("/^\[[0-9]+\]\s+([A-Za-z\s]+)\;.+\(.+\)$/", $ps_text, $va_matches)) {
+			return $va_matches[1];
+		}
+		return $ps_text;
+	}
+	# ------------------------------------------------
 }
