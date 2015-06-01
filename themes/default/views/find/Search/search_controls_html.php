f398afc9e4c267afd50b477080609f7e7b138ffd
Seth <seth@collectiveaccess.org>
2015-06-01 08:06:21 -0400

    PROV-1269 make hierarchy browser entry for disabled items clickable in search controls

--- themes/default/views/find/Search/search_controls_html.php
+++ themes/default/views/find/Search/search_controls_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2009-2011 Whirl-i-Gig
+ * Copyright 2009-2015 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -105,6 +105,8 @@
 						editButtonIcon: "<?php print caNavIcon($this->request, __CA_NAV_BUTTON_RIGHT_ARROW__); ?>",
 						disabledButtonIcon: "<?php print caNavIcon($this->request, __CA_NAV_BUTTON_DOT__); ?>",
 
+						disabledItems: 'full',
+						
 						initItemID: '<?php print $this->getVar('browse_last_id'); ?>',
 						indicatorUrl: '<?php print $this->request->getThemeUrlPath(); ?>/graphics/icons/indicator.gif',
 						typeMenuID: 'browseTypeMenu',
