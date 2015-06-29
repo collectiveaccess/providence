7a12089a2673ce02dd820f4885d2ccbddd2be4d8
Stefan <stefan@whirl-i-gig.com>
2015-06-29 11:52:32 -0400

    PROV-1272 Added alternate media metadata embedding function that doesn't require load()-ing each object and representation
    The required info can usually come directly from a search result

--- app/helpers/mediaPluginHelpers.php
+++ app/helpers/mediaPluginHelpers.php
@@ -698,6 +698,58 @@
 
 	# ------------------------------------------------------------------------------------------------
 	/**
+	 * Embed media metadata into given file. Embedding is performed on a copy of the file and placed into the
+	 * system tmp directory. The given file is never modified.
+	 *
+	 * If for some reason the embedding process fails, the original file name is returned.
+	 *
+	 * @param string $ps_file The file to embed metadata into
+	 * @param string $ps_table Table name of the subject record. This is used to figure out the appropriate mapping to use from media_metadata.conf
+	 * @param int $pn_pk Primary key of the subject record. This is used to run the export for the right record.
+	 * @param string $ps_type_code Optional type code for the subject record
+	 * @return string file name of either the original or the modified file
+	 */
+	function caEmbedMediaMetadataIntoFile($ps_file, $ps_table, $pn_pk, $ps_type_code = '__default__') {
+		require_once(__CA_MODELS_DIR__.'/ca_data_exporters.php');
+		$o_app_config = Configuration::load();
+
+		if (!($vs_media_metadata_config = $o_app_config->get('media_metadata'))) { return $ps_file; }
+		$o_metadata_config = Configuration::load($vs_media_metadata_config);
+		if(!caExifToolInstalled()) { return $ps_file; } // we need exiftool for embedding
+		$vs_path_to_exif_tool = caGetExternalApplicationPath('exiftool');
+
+		if (!file_exists($ps_file)) { return $ps_file; }
+		if (!preg_match("/^image\//", mime_content_type($ps_file))) { return $ps_file; } // Don't try to embed in files other than images
+
+		$va_mappings = $o_metadata_config->getAssoc('export_mappings');
+		if(!isset($va_mappings[$ps_table])) { return $ps_file; }
+
+		// make a temporary copy (we won't touch the original)
+		copy($ps_file, $vs_tmp_filepath = caGetTempDirPath()."/".time().md5($ps_file));
+
+		// figure out the mapping we're going to use (for this type or __default__)
+		if(isset($va_mappings[$ps_table][$ps_type_code])) {
+			$vs_export_mapping = $va_mappings[$ps_table][$ps_type_code];
+		} elseif(isset($va_mappings[$ps_table]['__default__'])) {
+			$vs_export_mapping = $va_mappings[$ps_table]['__default__'];
+		} else {
+			return $ps_file; // couldn't find a valid mapping
+		}
+
+		// run the export
+		$vs_export_filename = caGetTempFileName('mediaMetadataExport','xml');
+
+		if(!($vs_export = ca_data_exporters::exportRecord($vs_export_mapping, $pn_pk))) { return $ps_file; }
+		if(@file_put_contents($vs_export_filename, $vs_export) === false) { return $ps_file; }
+
+		exec("{$vs_path_to_exif_tool} -tagsfromfile {$vs_export_filename} -all:all ".caEscapeShellArg($vs_tmp_filepath), $va_output, $vn_return);
+
+		@unlink($vs_export_filename);
+		@unlink("{$vs_tmp_filepath}_original");
+
+		return $vs_tmp_filepath;
+	}
+	/**
 	 * Embed media metadata into representation media. Embedding is performed on a copy of the representation media and placed
 	 * into the system tmp directory. The original media is never modified.
 	 *
