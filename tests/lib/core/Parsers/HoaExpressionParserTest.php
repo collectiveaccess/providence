3c218b86f6202be11741be3265d867409ebe2548
Stefan <stefan@whirl-i-gig.com>
2015-08-04 13:56:47 -0700

    PROV-1335 Start rewriting the old ExpressionParser using the new code while preserving the API

--- tests/lib/core/Parsers/HoaExpressionParserTest.php
+++ tests/lib/core/Parsers/HoaExpressionParserTest.php
@@ -34,110 +34,83 @@ require_once(__CA_LIB_DIR__.'/core/Parsers/ExpressionParser/ExpressionVisitor.ph
 
 class HoaExpressionParserTest extends PHPUnit_Framework_TestCase {
 
-	public function testParseExpressions() {
-		$this->parseExpression('^ca_objects.preferred_labels = "foo"');
-		$this->parseExpression('^5 = "foo"');
+	public function testParse() {
+		$o_parser = new ExpressionParser();
 
-		$this->parseExpression('"Joe" NOT IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]');
-		$this->parseExpression('"Seth" IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]');
-		$this->parseExpression('5 IN [1,2,3,4,5]');
+		// parse() will throw an exception if it fails, so no assertions necessary
 
+		$o_parser->parse('^ca_objects.preferred_labels = "foo"');
+		$o_parser->parse('^5 = "foo"');
 
-		$this->parseExpression('("seth" = "seth")');
-		$this->parseExpression('5 > 10 OR "seth" = "seth"');
-		$this->parseExpression('(5 = 10) AND ("seth" = "seth") AND (6 > 1)');
-		$this->parseExpression('((5 > 10) AND ("seth" = "seth")) OR (6 > 1)');
-
-		$this->parseExpression('5 =~ /foo/');
-		$this->parseExpression('5 =~ /test test/');
-		$this->parseExpression('"foo" =~ /test test/');
+		$o_parser->parse('"Joe" NOT IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]');
+		$o_parser->parse('"Seth" IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]');
+		$o_parser->parse('5 IN [1,2,3,4,5]');
 
 
-		$this->parseExpression('5 + 4');
-		$this->parseExpression('"foo" + "bar"');
+		$o_parser->parse('("seth" = "seth")');
+		$o_parser->parse('5 > 10 OR "seth" = "seth"');
+		$o_parser->parse('(5 = 10) AND ("seth" = "seth") AND (6 > 1)');
+		$o_parser->parse('((5 > 10) AND ("seth" = "seth")) OR (6 > 1)');
 
-		$this->parseExpression('5 > 4');
-		$this->parseExpression('5 >= 4');
-		$this->parseExpression('5 != 4');
-		$this->parseExpression('5 = 4');
-		$this->parseExpression('5+5 >= 4');
-		$this->parseExpression('avg(abs(1.345), max(4,5))');
-		$this->parseExpression('1 ÷ 2 ÷ 3 + 4 * (5 * 2 - 6) * 3.14 ÷ avg(7, 8, 9)');
+		$o_parser->parse('5 =~ /foo/');
+		$o_parser->parse('5 =~ /test test/');
+		$o_parser->parse('"foo" =~ /test test/');
+
+
+		$o_parser->parse('5 + 4');
+		$o_parser->parse('"foo" + "bar"');
+
+		$o_parser->parse('5 > 4');
+		$o_parser->parse('5 >= 4');
+		$o_parser->parse('5 != 4');
+		$o_parser->parse('5 = 4');
+		$o_parser->parse('5+5 >= 4');
+		$o_parser->parse('avg(abs(1.345), max(4,5))');
+		$o_parser->parse('1 ÷ 2 ÷ 3 + 4 * (5 * 2 - 6) * 3.14 ÷ avg(7, 8, 9)');
 	}
 
-	public function testVisitor() {
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 > 1'));
-		$this->assertEquals(false, $this->parseAndVisitExpression('5 > 6'));
+	public function testEvaluate() {
+		$this->assertEquals(true, ExpressionParser::evaluate('5 > 1'));
+		$this->assertEquals(false, ExpressionParser::evaluate('5 > 6'));
 
-		$this->assertEquals(false, $this->parseAndVisitExpression('5 < 1'));
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 < 6'));
+		$this->assertEquals(false, ExpressionParser::evaluate('5 < 1'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 < 6'));
 
-		$this->assertEquals(false, $this->parseAndVisitExpression('5 <= 1'));
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 <= 6'));
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 <= 5'));
+		$this->assertEquals(false, ExpressionParser::evaluate('5 <= 1'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 <= 6'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 <= 5'));
 
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 >= 1'));
-		$this->assertEquals(false, $this->parseAndVisitExpression('5 >= 6'));
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 >= 5'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 >= 1'));
+		$this->assertEquals(false, ExpressionParser::evaluate('5 >= 6'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 >= 5'));
 
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 != 1'));
-		$this->assertEquals(false, $this->parseAndVisitExpression('5 != 5'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 != 1'));
+		$this->assertEquals(false, ExpressionParser::evaluate('5 != 5'));
 
-		$this->assertEquals(false, $this->parseAndVisitExpression('5 = 1'));
-		$this->assertEquals(true, $this->parseAndVisitExpression('5 = 5'));
+		$this->assertEquals(false, ExpressionParser::evaluate('5 = 1'));
+		$this->assertEquals(true, ExpressionParser::evaluate('5 = 5'));
 
-		$this->assertEquals(true, $this->parseAndVisitExpression('"Seth" IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
-		$this->assertEquals(false, $this->parseAndVisitExpression('"Joe" IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
+		$this->assertEquals(true, ExpressionParser::evaluate('"Seth" IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
+		$this->assertEquals(false, ExpressionParser::evaluate('"Joe" IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
 
-		$this->assertEquals(false, $this->parseAndVisitExpression('"Seth" NOT IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
-		$this->assertEquals(true, $this->parseAndVisitExpression('"Joe" NOT IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
+		$this->assertEquals(false, ExpressionParser::evaluate('"Seth" NOT IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
+		$this->assertEquals(true, ExpressionParser::evaluate('"Joe" NOT IN ["Julia", "Allison", "Sophie", "Maria", "Angie", "Seth"]'));
 
-		$this->assertEquals(3.1725, $this->parseAndVisitExpression('avg(abs(-1.345), max(4,5))'));
-		$this->assertEquals(4, $this->parseAndVisitExpression('length("test")'));
-		$this->assertEquals(1, $this->parseAndVisitExpression('length(4)'));
+		$this->assertEquals(3.1725, ExpressionParser::evaluate('avg(abs(-1.345), max(4,5))'));
+		$this->assertEquals(4, ExpressionParser::evaluate('length("test")'));
+		$this->assertEquals(1, ExpressionParser::evaluate('length(4)'));
 
-		$this->assertEquals('Stefan parses', $this->parseAndVisitExpression('"Stefan" + " parses"'));
+		$this->assertEquals('Stefan parses', ExpressionParser::evaluate('"Stefan" + " parses"'));
 
-		$this->assertEquals(11, $this->parseAndVisitExpression('6 + 5'));
-		$this->assertEquals(15, $this->parseAndVisitExpression('6 + 5 + 4'));
-		$this->assertEquals(1, $this->parseAndVisitExpression('6 - 5'));
-		$this->assertEquals(30, $this->parseAndVisitExpression('6 * 5'));
-		$this->assertEquals(2, $this->parseAndVisitExpression('4 ÷ 2'));
+		$this->assertEquals(11, ExpressionParser::evaluate('6 + 5'));
+		$this->assertEquals(15, ExpressionParser::evaluate('6 + 5 + 4'));
+		$this->assertEquals(1, ExpressionParser::evaluate('6 - 5'));
+		$this->assertEquals(30, ExpressionParser::evaluate('6 * 5'));
+		$this->assertEquals(2, ExpressionParser::evaluate('4 ÷ 2'));
 
-		$this->assertEquals(49.5, $this->parseAndVisitExpression('1 ÷ 2 ÷ 3 + 4 * (5 * 2 - 6) * 3'));
+		$this->assertEquals(49.5, ExpressionParser::evaluate('1 ÷ 2 ÷ 3 + 4 * (5 * 2 - 6) * 3'));
 
-		$this->assertEquals(13, $this->parseAndVisitExpression('5 + 4 * 2'));
-		$this->assertEquals(14, $this->parseAndVisitExpression('5 * 2 + 4'));
+		$this->assertEquals(13, ExpressionParser::evaluate('5 + 4 * 2'));
+		$this->assertEquals(14, ExpressionParser::evaluate('5 * 2 + 4'));
 	}
-
-	private function parseExpression($ps_expr) {
-		$o_compiler = Hoa\Compiler\Llk::load(
-			new Hoa\File\Read(__CA_LIB_DIR__.'/core/Parsers/ExpressionParser/ExpressionGrammar.pp')
-		);
-
-		// this throws an exception if it fails, so no assertions necessary
-		$o_ast = $o_compiler->parse($ps_expr);
-
-		// just dump the syntax tree in easy-to-read-format
-		//var_dump($ps_expr);
-		//$o_dumper = new Hoa\Compiler\Visitor\Dump();
-		//print $o_dumper->visit($o_ast);
-	}
-
-	private function parseAndVisitExpression($ps_expr) {
-		$o_compiler = Hoa\Compiler\Llk::load(
-			new Hoa\File\Read(__CA_LIB_DIR__.'/core/Parsers/ExpressionParser/ExpressionGrammar.pp')
-		);
-
-		// this throws an exception if it fails, so no assertions necessary
-		$o_ast = $o_compiler->parse($ps_expr);
-
-		//$o_dumper = new Hoa\Compiler\Visitor\Dump();
-		//print $o_dumper->visit($o_ast);
-
-		// use our visitor
-		$o_expr = new ExpressionVisitor();
-		return $o_expr->visit($o_ast);
-	}
-
 }
