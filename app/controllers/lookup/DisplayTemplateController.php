bbff5f0371d044d9d1f18e84c00c56636dbbef0a
Seth <seth@collectiveaccess.org>
2015-09-24 23:46:38 -0400

    NOJIRA cosmetic change

--- app/controllers/lookup/DisplayTemplateController.php
+++ app/controllers/lookup/DisplayTemplateController.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011 Whirl-i-Gig
+ * Copyright 2011-2015 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -47,4 +47,5 @@ class DisplayTemplateController extends ActionController {
 
 		print $t_instance->getWithTemplate($ps_template);
 	}
+	# -------------------------------------------------------
 }
