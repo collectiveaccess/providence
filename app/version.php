1d183dbafd703f6593a4f1dba82247314c043ccd
Seth <seth@collectiveaccess.org>
2016-01-18 11:33:22 -0500

    Bump version

--- app/version.php
+++ app/version.php
@@ -1,6 +1,6 @@
 <?php
  	# CollectiveAccess version number; don't change
-	define('__CollectiveAccess__', '1.5.2');
+	define('__CollectiveAccess__', '1.5.3');
 	
 	# Schema revision
 	define('__CollectiveAccess_Schema_Rev__', 126);
