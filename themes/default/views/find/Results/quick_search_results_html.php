9070a3fb12c6052ed287be36434adbb9174281f7
Seth <seth@collectiveaccess.org>
2016-01-26 09:40:16 -0500

    NOJIRA reset type restriction when linking to full results

--- themes/default/views/find/Results/quick_search_results_html.php
+++ themes/default/views/find/Results/quick_search_results_html.php
@@ -126,7 +126,7 @@
 			if ($o_res->numHits() >= 1) { 
 ?>
 				<div class="quickSearchResultHeader rounded" >
-					<div class="quickSearchFullResultsLink"><?php print caNavLink($this->request, caNavIcon(__CA_NAV_ICON_FILTER__, 1)." "._t("Full Results &rsaquo;"), null, $va_info['searchModule'], $va_info['searchController'], $va_info['searchAction'], array("search" => urlencode($ps_search))); ?></div>
+					<div class="quickSearchFullResultsLink"><?php print caNavLink($this->request, caNavIcon(__CA_NAV_ICON_FILTER__, 1)." "._t("Full Results &rsaquo;"), null, $va_info['searchModule'], $va_info['searchController'], $va_info['searchAction'], array("search" => urlencode($ps_search), 'type_id' => '*')); ?></div>
 					<a href='#' style="text-decoration:none; color:#333;" id='show<?php print $vs_table; ?>' onclick='return caQuickSearchShowHideResults("show", "<?php print $vs_table; ?>");'><?php print $va_info['displayname']." (".$o_res->numHits().")"; ?> <?php print caNavIcon(__CA_NAV_ICON_EXPAND__, 1); ?></a>
 					<a href='#' id='hide<?php print $vs_table; ?>' style='display:none; text-decoration:none; color:#333;' onclick='return caQuickSearchShowHideResults("hide", "<?php print $vs_table; ?>");'><?php print $va_info['displayname']." (".$o_res->numHits().")"; ?> <?php print caNavIcon(__CA_NAV_ICON_COLLAPSE__, 1); ?></a>
 				</div>
