7932113d9dbeeeae4a14b85ba3aa7708fab675ee
Seth <stefan@hoquet>
2016-02-11 20:45:56 -0500

    PROV-1489 filter future-dated locations

--- app/models/ca_objects.php
+++ app/models/ca_objects.php
@@ -806,7 +806,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 	 * @param array $pa_bundle_settings The settings for a ca_objects_history editing BUNDLES
 	 * @param array $pa_options Array of options. Options include:
 	 *		noCache = Don't use any cached history data. [Default is false]
-	 *		currentOnly = Only return history entries dates before or on the current date. [Default is false]
+	 *		currentOnly = Only return history entries dates that include the current date. [Default is false]
 	 *		limit = Only return a maximum number of history entries. [Default is null; no limit]
 	 *
 	 * @return array A list of life cycle events, indexed by historic timestamp for date of occurrrence. Each list value is an array of history entries.
@@ -863,6 +863,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 					foreach($va_date_elements as $vs_date_element) {
 						$va_dates[] = array(
 							'sortable' => $t_lot->get($vs_date_element, array('getDirectDate' => true)),
+							'bounds' => explode("/", $t_lot->get($vs_date_element, array('sortable' => true))),
 							'display' => $t_lot->get($vs_date_element)
 						);
 					}
@@ -870,6 +871,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				if (!sizeof($va_dates)) {
 					$va_dates[] = array(
 						'sortable' => $vn_date = caUnixTimestampToHistoricTimestamps($t_lot->getCreationTimestamp(null, array('timestampOnly' => true))),
+						'bounds' => array(0, $vn_date),
 						'display' => caGetLocalizedDate($vn_date)
 					);
 				}
@@ -877,7 +879,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				foreach($va_dates as $va_date) {
 					if (!$va_date['sortable']) { continue; }
 					if (!in_array($vn_type_id, $va_lot_types)) { continue; }
-					if ($pb_get_current_only && ($va_date['sortable'] > $vn_current_date)) { continue; }
+					if ($pb_get_current_only && (($va_date['bounds'][0] > $vn_current_date) || ($va_date['bounds'][1] < $vn_current_date))) { continue; }
 				
 				
 					$vs_default_display_template = '^ca_object_lots.preferred_labels.name (^ca_object_lots.idno_stub)';
@@ -926,6 +928,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 					foreach($va_date_elements_by_type[$vn_type_id] as $vs_date_element) {
 						$va_dates[] = array(
 							'sortable' => $qr_loans->get("ca_loans.{$vs_date_element}", array('getDirectDate' => true)),
+							'bounds' => explode("/", $qr_loans->get("ca_loans.{$vs_date_element}", array('sortable' => true))),
 							'display' => $qr_loans->get("ca_loans.{$vs_date_element}")
 						);
 					}
@@ -933,17 +936,18 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				if (!sizeof($va_dates)) {
 					$va_dates[] = array(
 						'sortable' => $vn_date = caUnixTimestampToHistoricTimestamps($qr_loans->get('lastModified')),
+						'bounds' => array(0, $vn_date),
 						'display' => caGetLocalizedDate($vn_date)
 					);
 				}
 				
 				$vs_default_display_template = '^ca_loans.preferred_labels.name (^ca_loans.idno)';
 				$vs_display_template = $pb_display_label_only ? $vs_default_display_template : caGetOption("ca_loans_{$va_loan_type_info[$vn_type_id]['idno']}_displayTemplate", $pa_bundle_settings, $vs_default_display_template);
-				
+		
 				foreach($va_dates as $va_date) {
 					if (!$va_date['sortable']) { continue; }
 					if (!in_array($vn_type_id, $va_loan_types)) { continue; }
-					if ($pb_get_current_only && ($va_date['sortable'] > $vn_current_date)) { continue; }
+					if ($pb_get_current_only && (($va_date['bounds'][0] > $vn_current_date) || ($va_date['bounds'][1] < $vn_current_date))) { continue; }
 					
 					$vs_color = $va_loan_type_info[$vn_type_id]['color'];
 					if (!$vs_color || ($vs_color == '000000')) {
@@ -994,6 +998,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 					foreach($va_date_elements_by_type[$vn_type_id] as $vs_date_element) {
 						$va_dates[] = array(
 							'sortable' => $qr_movements->get("ca_movements.{$vs_date_element}", array('getDirectDate' => true)),
+							'bounds' => explode("/", $qr_movements->get("ca_movements.{$vs_date_element}", array('sortable' => true))),
 							'display' => $qr_movements->get("ca_movements.{$vs_date_element}")
 						);
 					}
@@ -1001,6 +1006,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				if (!sizeof($va_dates)) {
 					$va_dates[] = array(
 						'sortable' => $vn_date = caUnixTimestampToHistoricTimestamps($qr_movements->get('lastModified')),
+						'bound' => array(0, $vn_date),
 						'display' => caGetLocalizedDate($vn_date)
 					);
 				}
@@ -1011,7 +1017,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				foreach($va_dates as $va_date) {
 					if (!$va_date['sortable']) { continue; }
 					if (!in_array($vn_type_id, $va_movement_types)) { continue; }
-					if ($pb_get_current_only && ($va_date['sortable'] > $vn_current_date)) { continue; }
+					if ($pb_get_current_only && (($va_date['bounds'][0] > $vn_current_date) || ($va_date['bounds'][1] < $vn_current_date))) { continue; }
 					
 					$vs_color = $va_movement_type_info[$vn_type_id]['color'];
 					if (!$vs_color || ($vs_color == '000000')) {
@@ -1063,6 +1069,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 					foreach($va_date_elements_by_type[$vn_type_id] as $vs_date_element) {
 						$va_dates[] = array(
 							'sortable' => $qr_occurrences->get("ca_occurrences.{$vs_date_element}", array('getDirectDate' => true)),
+							'bounds' => explode("/", $qr_occurrences->get("ca_occurrences.{$vs_date_element}", array('sortable' => true))),
 							'display' => $qr_occurrences->get("ca_occurrences.{$vs_date_element}")
 						);
 					}
@@ -1070,6 +1077,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				if (!sizeof($va_dates)) {
 					$va_dates[] = array(
 						'sortable' => $vn_date = caUnixTimestampToHistoricTimestamps($qr_occurrences->get('lastModified')),
+						'bounds' => array(0, $vn_date),
 						'display' => caGetLocalizedDate($vn_date)
 					);
 				}
@@ -1080,7 +1088,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				foreach($va_dates as $va_date) {
 					if (!$va_date['sortable']) { continue; }
 					if (!in_array($vn_type_id, $va_occurrence_types)) { continue; }
-					if ($pb_get_current_only && ($va_date['sortable'] > $vn_current_date)) { continue; }
+					if ($pb_get_current_only && (($va_date['bounds'][0] > $vn_current_date) || ($va_date['bounds'][1] < $vn_current_date))) { continue; }
 					
 					$vs_color = $va_occurrence_type_info[$vn_type_id]['color'];
 					if (!$vs_color || ($vs_color == '000000')) {
@@ -1126,6 +1134,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				
 				$va_date = array(
 					'sortable' => $qr_locations->get("ca_objects_x_storage_locations.effective_date", array('getDirectDate' => true)),
+					'bounds' => explode("/", $qr_locations->get("ca_objects_x_storage_locations.effective_date", array('sortable' => true))),
 					'display' => $qr_locations->get("ca_objects_x_storage_locations.effective_date")
 				);
 
@@ -1133,7 +1142,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
 				if (!in_array($vn_rel_type_id = $qr_locations->get('ca_objects_x_storage_locations.type_id'), $va_location_types)) { continue; }
 				$vn_type_id = $qr_locations->get('ca_storage_locations.type_id');
 				
-				if ($pb_get_current_only && ($va_date['sortable'] > $vn_current_date)) { continue; }
+				if ($pb_get_current_only && (($va_date['bounds'][0] > $vn_current_date) || ($va_date['bounds'][1] < $vn_current_date))) { continue; }
 				
 				$vs_color = $va_location_type_info[$vn_type_id]['color'];
 				if (!$vs_color || ($vs_color == '000000')) {
@@ -1854,7 +1863,7 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
  			}
  		}
  		
-		if (is_array($va_history = $this->getObjectHistory($va_bundle_settings, array('displayLabelOnly' => true, 'limit' => 1, 'currentOnly' => false, 'noCache' => true))) && (sizeof($va_history) > 0)) {
+		if (is_array($va_history = $this->getObjectHistory($va_bundle_settings, array('displayLabelOnly' => true, 'limit' => 1, 'currentOnly' => true, 'noCache' => true))) && (sizeof($va_history) > 0)) {
 			$va_current_location = array_shift(array_shift($va_history));
 			
 			if ($va_current_location['type'] == 'ca_storage_locations') {
@@ -1913,6 +1922,18 @@ class ca_objects extends BaseObjectLocationModel implements IBundleProvider {
  				return false;
  			} 
  			
+ 			return true;
+ 		} else {
+ 			$this->setMode(ACCESS_WRITE);
+ 			$this->set('current_loc_class', null);
+ 			$this->set('current_loc_subclass', null);
+ 			$this->set('current_loc_id', null);
+ 			$this->update();
+ 			
+ 			
+ 			if ($this->numErrors()) {
+ 				return false;
+ 			} 
  			return true;
  		}
  		return false;
