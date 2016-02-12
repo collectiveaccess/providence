281654af6b4488d18e335df22662c8eadd0e3db0
Seth <seth@whirl-i-gig.com>
2016-02-11 21:14:41 -0500

    PROV-1490 Add missing entry for objectRepresentations element

--- app/lib/ca/Attributes/Values/AuthorityAttributeValue.php
+++ app/lib/ca/Attributes/Values/AuthorityAttributeValue.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2014 Whirl-i-Gig
+ * Copyright 2014-2016 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -299,6 +299,9 @@ abstract class AuthorityAttributeValue extends AttributeValue {
 				break;
 			case __CA_ATTRIBUTE_VALUE_OBJECTLOTS__:
 				return $o_dm->getInstanceByTableName('ca_object_lots', true);
+				break;
+			case __CA_ATTRIBUTE_VALUE_OBJECTREPRESENTATIONS__:
+				return $o_dm->getInstanceByTableName('ca_object_representations', true);
 				break;
 		}
 		return null;
