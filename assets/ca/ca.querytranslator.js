3ba7e2371f6568ad3d8100a86571efba6576f560
Ben New <ben.new@gaiaresources.com.au>
2016-03-17 19:12:51 +0800

    WIP Add `ca.querytranslator.js` to query builder bundle.

---
+++ assets/ca/ca.querytranslator.js
@@ -0,0 +1,52 @@
+/* ----------------------------------------------------------------------
+ * js/ca/ca.objectcheckout.js
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2014 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This source code is free and modifiable under the terms of
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+
+var caUI = caUI || {};
+
+(function () {
+	/**
+	 * Convert a set of rules from the jQuery query builder into a CA search query.  Performs the inverse operation to
+	 * `convertSearchQueryToQueryBuilderRules`.
+	 * @param {array} rules
+	 * @returns {string}
+	 */
+	caUI.convertQueryBuilderRulesToSearchQuery = function (rules) {
+		// TODO
+		return '';
+	};
+
+	/**
+	 * Convert a CA query string into a set of rules for the jQuery query builder.  Performs the inverse operation to
+	 * `convertQueryBuilderRulesToSearchQuery`.
+	 * @param {string} query
+	 * @returns {object|undefined}
+	 */
+	caUI.convertSearchQueryToQueryBuilderRules = function (query) {
+		// TODO
+		return undefined;
+	}
+}());
