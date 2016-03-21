2863ffb634e0211c5d56138132215fb4dabfb379
Stefan <stefan@whirl-i-gig.com>
2016-03-21 12:54:27 -0400

    PROV-1293 Oops. Allow overriding for label GUIDs as well.

--- app/lib/ca/BaseLabel.php
+++ app/lib/ca/BaseLabel.php
@@ -54,7 +54,7 @@
 				$t_guid->setTransaction($this->getTransaction());
 				$t_guid->set('table_num', $this->tableNum());
 				$t_guid->set('row_id', $this->getPrimaryKey());
-				$t_guid->set('guid', caGenerateGUID());
+				$t_guid->set('guid', caGetOption('setGUIDTo', $pa_options, caGenerateGUID()));
 				$t_guid->insert();
 			}
 
