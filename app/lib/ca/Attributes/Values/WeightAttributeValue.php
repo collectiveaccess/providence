3ca22c23fa70dba4508904150b5f3e9f8d6a77db
Seth <seth@collectiveaccess.org>
2016-05-11 13:59:14 -0400

    NOJIRA catch no decimal case

--- app/lib/ca/Attributes/Values/WeightAttributeValue.php
+++ app/lib/ca/Attributes/Values/WeightAttributeValue.php
@@ -172,7 +172,8 @@
  			}	
  			
  			// Trim off trailing zeros in quantity
- 			$this->ops_text_value = preg_replace("![\.]{0,1}[0]+([A-Za-z ]+)$!", "$1", $this->ops_text_value);
+ 			$this->ops_text_value = preg_replace("!\.([1-9]*)[0]+([A-Za-z ]+)$!", "\.$1 $2", $this->ops_text_value);
+ 			$this->ops_text_value = preg_replace("![\.]([A-Za-z ]+)$!", "$1", $this->ops_text_value);
  			
  			$this->opn_decimal_value = $pa_value_array['value_decimal1'];
  		}
