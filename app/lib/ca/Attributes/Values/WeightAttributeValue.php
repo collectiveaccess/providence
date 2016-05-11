98d5d38635681504fcc16e4722a8bbe5f1a9c8df
Seth <seth@collectiveaccess.org>
2016-05-11 14:15:12 -0400

    NOJIRA try this one more time

--- app/lib/ca/Attributes/Values/WeightAttributeValue.php
+++ app/lib/ca/Attributes/Values/WeightAttributeValue.php
@@ -172,7 +172,7 @@
  			}	
  			
  			// Trim off trailing zeros in quantity
- 			$this->ops_text_value = preg_replace("!\.([1-9]*)[0]+([A-Za-z ]+)$!", "\.$1 $2", $this->ops_text_value);
+ 			$this->ops_text_value = preg_replace("!\.([1-9]*)[0]+([A-Za-z ]+)$!", ".$1 $2", $this->ops_text_value);
  			$this->ops_text_value = preg_replace("![\.]([A-Za-z ]+)$!", "$1", $this->ops_text_value);
  			
  			$this->opn_decimal_value = $pa_value_array['value_decimal1'];
