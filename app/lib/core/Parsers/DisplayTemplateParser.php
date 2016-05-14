c641a86c703fa7ede4142d8c52483d6ba6f40769
Seth <seth@collectiveaccess.org>
2016-05-13 21:19:58 -0400

    PROV-1674 pass relationship types when template is relative to relationship and ^relationship_typename tag is in related unit

--- app/lib/core/Parsers/DisplayTemplateParser.php
+++ app/lib/core/Parsers/DisplayTemplateParser.php
@@ -622,13 +622,15 @@ class DisplayTemplateParser {
 									$va_relationship_type_ids = array();
 									if (is_array($va_relation_ids) && sizeof($va_relation_ids)) {
 										$qr_rels = caMakeSearchResult($t_rel_instance->getRelationshipTableName($ps_tablename), $va_relation_ids);
-										$va_relationship_type_ids = $qr_rels->getAllFieldValues($t_rel_instance->getRelationshipTableName($ps_tablename).'.type_id');
+										$va_relationship_type_ids = $qr_rels->getAllFieldValues($x=$t_rel_instance->getRelationshipTableName($ps_tablename).'.type_id');
+									} elseif($vs_rel_tablename = $t_rel_instance->getRelationshipTableName($ps_tablename)) {
+										$va_relationship_type_ids = $pr_res->getAllFieldValues("{$vs_rel_tablename}.type_id");
 									}
 								}
 							
 								break;
 						}
-						
+						print_R($va_relationship_type_ids);
 						$va_tmpl_val = DisplayTemplateParser::evaluate(
 							$o_node->getInnerText(), $va_relative_to_tmp[0], $va_relative_ids,
 							array_merge(
