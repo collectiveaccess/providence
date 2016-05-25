0140d2d7eed87689c605ebcbdfe68722e3e53a35
Stefan <stefan@whirl-i-gig.com>
2016-05-25 13:05:04 -0400

    PROV-1205 First cut at a rough table structure for metadata alerts

--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -6708,6 +6708,68 @@ create table ca_user_sort_items
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /*==========================================================================*/
+create table ca_metadata_alert_rules (
+  rule_id         int unsigned      not null AUTO_INCREMENT,
+  table_num       tinyint unsigned  not null,
+  name            varchar(255)      not null,
+  code            varchar(20)       not null,
+  settings        longtext          not null,
+
+  primary key (rule_id),
+  index i_table_num (table_num)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+/*==========================================================================*/
+create table ca_metadata_alert_triggers (
+  trigger_id      int unsigned      not null AUTO_INCREMENT,
+  rule_id         int unsigned      not null,
+  settings        longtext          not null,
+  trigger_type    tinyint unsigned  not null,
+
+
+  primary key (trigger_id),
+  constraint fk_alert_rules_rule_id foreign key (rule_id)
+    references ca_metadata_alert_rules(rule_id) on delete restrict on update restrict
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+/*==========================================================================*/
+create table ca_metadata_alert_rules_x_user_groups (
+  relation_id   int unsigned not null auto_increment,
+  rule_id 		  int unsigned not null references ca_metadata_alert_rules(rule_id),
+  group_id 		  int unsigned not null references ca_user_groups(group_id),
+  access 			  tinyint unsigned not null default 0,
+
+  primary key 				(relation_id),
+  index i_rule_id			(rule_id),
+  index i_group_id		(group_id)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+/*==========================================================================*/
+create table ca_metadata_alert_rules_x_users (
+  relation_id 	int unsigned not null auto_increment,
+  rule_id 	int unsigned not null references ca_metadata_alert_rules(rule_id),
+  user_id 		int unsigned not null references ca_users(user_id),
+  access 			tinyint unsigned not null default 0,
+
+  primary key 				(relation_id),
+  index i_rule_id			(rule_id),
+  index i_user_id			(user_id)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+/*==========================================================================*/
+create table ca_metadata_alert_rule_type_restrictions (
+  restriction_id                 int unsigned                   not null AUTO_INCREMENT,
+  type_id                        int unsigned,
+  table_num                      tinyint unsigned               not null,
+  rule_id                        int unsigned                   not null,
+  include_subtypes               tinyint unsigned               not null default 0,
+  settings                       longtext                       not null,
+  rank                           smallint unsigned              not null default 0,
+  primary key (restriction_id),
+
+  index i_rule_id			(rule_id),
+  index i_type_id				(type_id),
+  constraint fk_ca_metadata_alert_rule_type_restrictions_rule_id foreign key (rule_id)
+    references ca_metadata_alert_rules(rule_id) on delete restrict on update restrict
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+/*==========================================================================*/
 /* Schema update tracking                                                   */
 /*==========================================================================*/
 create table ca_schema_updates (
@@ -6718,5 +6780,5 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-/* CURRENT MIGRATION: 133 */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (133, unix_timestamp());
+/* CURRENT MIGRATION: 134 */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (134, unix_timestamp());
