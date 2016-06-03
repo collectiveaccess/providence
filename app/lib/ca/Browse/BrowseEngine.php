e68311239d833f61cecf45c86baa17a6628ba2b7
Pedro Amorim <pedro.j.amorim@globaleda.pt>
2016-06-03 14:38:16 +0000

    Bug solving in source id restrictions for browsing and accessing object through URL

--- app/lib/ca/Browse/BrowseEngine.php
+++ app/lib/ca/Browse/BrowseEngine.php
@@ -974,7 +974,6 @@
 			$vs_cache_key = $this->opo_ca_browse_cache->getCurrentCacheKey();
 
 			if ($this->opo_ca_browse_cache->load($vs_cache_key)) {
-
 				$vn_created_on = $this->opo_ca_browse_cache->getParameter('created_on'); //$t_new_browse->get('created_on', array('getDirectDate' => true));
 				$vn_cache_timeout = (int) $this->opo_ca_browse_config->get('cache_timeout');
 
@@ -1935,7 +1934,8 @@
 							$va_wheres[] = '('.$this->ops_browse_table_name.'.'.$t_subject->getTypeFieldName().' IN ('.join(', ', $va_browse_type_ids).')'.($t_subject->getFieldInfo('type_id', 'IS_NULL') ? " OR (".$this->ops_browse_table_name.'.'.$t_subject->getTypeFieldName()." IS NULL)" : '').')';
 						}
 
-						if (is_array($va_browse_source_ids) && sizeof($va_browse_source_ids)) {
+						if (($va_browse_source_ids = $this->getSourceRestrictionList()) && sizeof($va_browse_source_ids)) {
+							$t_subject = $this->getSubjectInstance();
 							$va_wheres[] = '('.$this->ops_browse_table_name.'.'.$t_subject->getSourceFieldName().' IN ('.join(', ', $va_browse_source_ids).') OR ('.$this->ops_browse_table_name.'.'.$t_subject->getSourceFieldName().' IS NULL))';
 						}
 
@@ -2357,6 +2357,24 @@
 				}
 			}
 
+			$vs_browse_source_limit_sql = '';
+			if (($va_browse_source_ids = $this->getSourceRestrictionList()) && sizeof($va_browse_source_ids)) {		// source restrictions
+				$vs_browse_source_limit_sql = '('.$t_subject->tableName().'.'.$t_subject->getSourceFieldName().' IN ('.join(', ', $va_browse_source_ids).')'.($t_subject->getFieldInfo('source_id', 'IS_NULL') ? " OR (".$this->ops_browse_table_name.'.'.$t_subject->getSourceFieldName()." IS NULL)" : '').')';
+
+				if (is_array($va_facet_info['source_restrictions'])) { 		// facet source restrictions bind a facet to specific sources; we check them here
+					$va_restrict_to_sources = $this->_convertSourceCodesToIDs($va_facet_info['source_restrictions']);
+					$vb_is_ok_to_browse = false;
+					foreach($va_browse_source_ids as $vn_source_id) {
+						if (in_array($vn_source_id, $va_restrict_to_sources)) {
+							$vb_is_ok_to_browse = true;
+							break;
+						}
+					}
+
+					if (!$vb_is_ok_to_browse) { return array(); }
+				}
+			}
+
 			// Values to exclude from list attributes and authorities; can be idnos or ids
 			$va_exclude_values = caGetOption('exclude_values', $va_facet_info, array(), array('castTo' => 'array'));
 
@@ -2676,6 +2694,10 @@
 						$va_where_sql[] = $vs_browse_type_limit_sql;
 					}
 
+					if ($vs_browse_source_limit_sql) {
+						$va_where_sql[] = $vs_browse_source_limit_sql;
+					}
+
 					if (isset($va_facet_info['preferred_labels_only']) && $va_facet_info['preferred_labels_only'] && $t_label->hasField('is_preferred')) {
 						$va_where_sql[] = "l.is_preferred = 1";
 					}
@@ -2848,6 +2870,10 @@
 
 					if ($vs_browse_type_limit_sql) {
 						$va_wheres[] = $vs_browse_type_limit_sql;
+					}
+
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
 					}
 
 					if ($t_item->hasField('deleted')) {
@@ -3115,6 +3141,10 @@
 						$va_wheres[] = $vs_browse_type_limit_sql;
 					}
 
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
+					}
+
 					if ($t_item->hasField('deleted')) {
 						$va_wheres[] = "(".$vs_browse_table_name.".deleted = 0)";
 					}
@@ -3268,6 +3298,10 @@
 							$va_wheres[] = $vs_browse_type_limit_sql;
 						}
 
+						if ($vs_browse_source_limit_sql) {
+							$va_wheres[] = $vs_browse_source_limit_sql;
+						}
+
 						if ($t_subject->hasField('deleted')) {
 							$va_wheres[] = "(".$t_subject->tableName().".deleted = 0)";
 						}
@@ -3401,6 +3435,11 @@
 							if ($vs_browse_type_limit_sql) {
 								$va_wheres[] = $vs_browse_type_limit_sql;
 							}
+
+							if ($vs_browse_source_limit_sql) {
+								$va_wheres[] = $vs_browse_source_limit_sql;
+							}							
+
 							if ($t_subject->hasField('deleted')) {
 								$va_wheres[] = "(".$t_subject->tableName().".deleted = 0)";
 							}
@@ -3503,6 +3542,10 @@
 
 								if ($vs_browse_type_limit_sql) {
 									$va_wheres[] = $vs_browse_type_limit_sql;
+								}
+
+								if ($vs_browse_source_limit_sql) {
+									$va_wheres[] = $vs_browse_source_limit_sql;
 								}
 
 								if ($va_facet_info['relative_to']) {
@@ -3641,6 +3684,10 @@
 						$va_wheres[] = $vs_browse_type_limit_sql;
 					}
 
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
+					}
+
 					if ($t_item->hasField('deleted')) {
 						$va_wheres[] = "(".$vs_browse_table_name.".deleted = 0)";
 					}
@@ -3755,6 +3802,10 @@
 
 					if ($vs_browse_type_limit_sql) {
 						$va_wheres[] = $vs_browse_type_limit_sql;
+					}
+
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
 					}
 
 					if ($t_item->hasField('deleted')) {
@@ -3875,6 +3926,10 @@
 
 					if ($vs_browse_type_limit_sql) {
 						$va_wheres[] = $vs_browse_type_limit_sql;
+					}
+
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
 					}
 
 					if ($t_item->hasField('deleted')) {
@@ -4093,6 +4148,10 @@
 
 					if ($vs_browse_type_limit_sql) {
 						$va_wheres[] = $vs_browse_type_limit_sql;
+					}
+
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
 					}
 
 					if ($t_item->hasField('deleted')) {
@@ -4379,6 +4438,10 @@
 
 					if ($vs_browse_type_limit_sql) {
 						$va_wheres[] = $vs_browse_type_limit_sql;
+					}
+
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
 					}
 
 					if ($t_item->hasField('deleted')) {
@@ -4708,6 +4771,10 @@ if (!$va_facet_info['show_all_when_first_facet'] || ($this->numCriteria() > 0)) 
 					}
 					if ($vs_browse_type_limit_sql) {
 						$va_wheres[] = $vs_browse_type_limit_sql;
+					}
+
+					if ($vs_browse_source_limit_sql) {
+						$va_wheres[] = $vs_browse_source_limit_sql;
 					}
 
 					if ($this->opo_config->get('perform_item_level_access_checking')) {
