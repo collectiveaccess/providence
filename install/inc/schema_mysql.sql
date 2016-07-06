22c9b9fadd4c4451cec7ed876db96bcf5e04cab8
Stefan <stefan@whirl-i-gig.com>
2016-07-06 17:36:28 -0400

    PROV-1770 Add table structure for notifications

--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -6710,6 +6710,33 @@ create table ca_user_sort_items
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /*==========================================================================*/
+
+create table ca_notifications (
+  notification_id     int unsigned        not null AUTO_INCREMENT,
+  notification_type   tinyint unsigned    not null default 0,
+  user_id             int unsigned,
+  group_id            int unsigned,
+  datetime            int unsigned        not null,
+  table_num           tinyint unsigned,
+  row_id              int unsigned,
+  was_read            tinyint unsigned    not null default 0,
+  message             longtext,
+
+  primary key (notification_id),
+
+  constraint fk_ca_users_user_id foreign key (user_id)
+    references ca_users (user_id) on delete restrict on update restrict,
+
+  constraint fk_ca_user_groups_group_id foreign key (group_id)
+    references ca_user_groups (group_id) on delete restrict on update restrict,
+
+  index i_table_num_row_id (table_num, row_id),
+  index i_datetime (datetime),
+  index i_notification_type (notification_type)
+
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+/*==========================================================================*/
 /* Schema update tracking                                                   */
 /*==========================================================================*/
 create table ca_schema_updates (
@@ -6720,5 +6747,5 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-/* CURRENT MIGRATION: 133 */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (134, unix_timestamp());
+/* CURRENT MIGRATION: 135 */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (135, unix_timestamp());
