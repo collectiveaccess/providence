b7ffe46eea3054d68c3466eadb20ccd51c85c90b
Seth <seth@collectiveaccess.org>
2016-08-07 14:32:37 -0400

    PROV-1293 Enable logging on ca_users to allow for sync

--- app/models/ca_users.php
+++ app/models/ca_users.php
@@ -239,7 +239,7 @@ class ca_users extends BaseModel {
 	# Change logging
 	# ------------------------------------------------------
 	protected $UNIT_ID_FIELD = null;
-	protected $LOG_CHANGES_TO_SELF = false;
+	protected $LOG_CHANGES_TO_SELF = true;
 	protected $LOG_CHANGES_USING_AS_SUBJECT = array(
 		"FOREIGN_KEYS" => array(
 		
