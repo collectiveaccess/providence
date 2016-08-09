6d0b31c4677618b1c4ed7abb8711099bd81f571b
Seth <seth@collectiveaccess.org>
2016-08-09 17:20:50 -0400

    NOJIRA enable logging for all relationships; needed for sync

--- app/models/ca_bundle_displays_x_user_groups.php
+++ app/models/ca_bundle_displays_x_user_groups.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2010 Whirl-i-Gig
+ * Copyright 2010-2016 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -141,7 +141,7 @@ class ca_bundle_displays_x_user_groups extends BaseRelationshipModel {
 	protected $LOG_CHANGES_TO_SELF = false;
 	protected $LOG_CHANGES_USING_AS_SUBJECT = array(
 		"FOREIGN_KEYS" => array(
-			"display_id"
+			'display_id', 'group_id'
 		),
 		"RELATED_TABLES" => array(
 		
