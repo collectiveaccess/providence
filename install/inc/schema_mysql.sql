329643a4bf535e3abce093a0f9bf3d53c8360c55
Seth <seth@collectiveaccess.org>
2016-08-09 18:25:14 -0400

    NOJIRA Force fields to have defaults; some older systems do not have these set, causing errors in MySQL 5.7

--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -6710,6 +6710,7 @@ create table ca_user_sort_items
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /*==========================================================================*/
+
 create table ca_notifications (
   notification_id     int unsigned        not null AUTO_INCREMENT,
   notification_type   tinyint unsigned    not null default 0,
@@ -6724,6 +6725,7 @@ create table ca_notifications (
 
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 /*==========================================================================*/
+
 create table ca_notification_subjects (
   subject_id      int unsigned        not null auto_increment,
   notification_id int unsigned        not null references ca_notifications(notification_id),
@@ -6734,6 +6736,31 @@ create table ca_notification_subjects (
   primary key (subject_id),
   index i_notification_id (notification_id),
   index i_table_num_row_id (table_num, row_id)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+/*==========================================================================*/
+
+create table ca_download_log (
+  log_id		      	int unsigned        not null AUTO_INCREMENT,
+  log_datetime        	int unsigned        not null,
+  user_id             	int unsigned        null,
+  ip_addr			  	char(15)			null,
+  table_num    			tinyint unsigned    not null,
+  row_id       			int unsigned        not null,
+  representation_id     int unsigned      	null,
+  download_source		varchar(40)			null,
+
+  primary key (log_id),
+
+  constraint fk_ca_download_log_user_id foreign key (user_id)
+    references ca_users (user_id) on delete restrict on update restrict,
+
+  constraint fk_ca_download_log_representation_id foreign key (representation_id)
+    references ca_object_representations (representation_id) on delete restrict on update restrict,
+
+  index i_table_num_row_id (table_num, row_id),
+  index i_log_datetime (log_datetime)
+
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
@@ -6748,5 +6775,5 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-/* CURRENT MIGRATION: 136 */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (136, unix_timestamp());
+/* CURRENT MIGRATION: 138 */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (138, unix_timestamp());
