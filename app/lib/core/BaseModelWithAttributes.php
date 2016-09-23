2e7c3f6d39e0b5530e67b3ca95974381caada5d0
Seth <seth@collectiveaccess.org>
2016-09-23 10:52:28 -0400

    PROV-1847 Hide container labels

--- app/lib/core/BaseModelWithAttributes.php
+++ app/lib/core/BaseModelWithAttributes.php
@@ -1536,7 +1536,7 @@
 				if(!is_array($va_label)) { 
 					$va_label = array('name' => '???', 'description' => '');
 				}
-				$va_elements_by_container[$va_element['parent_id']][] = $vs_br.ca_attributes::attributeHtmlFormElement($va_element, array_merge($pa_bundle_settings, array_merge($pa_options, array(
+				$va_elements_by_container[$va_element['parent_id']][] = ($va_element['datatype'] == 0) ? '' : $vs_br.ca_attributes::attributeHtmlFormElement($va_element, array_merge($pa_bundle_settings, array_merge($pa_options, array(
 					'label' => (sizeof($va_element_set) > 1) ? $va_label['name'] : '',
 					'description' => $va_label['description'],
 					't_subject' => $this,
