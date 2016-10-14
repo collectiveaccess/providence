d1df6e45a410f402643e97d74187568e39ea8c9e
Seth <seth@collectiveaccess.org>
2016-10-14 16:25:09 -0400

    NOJIRA attempt to speed up reindexing

--- app/lib/core/Plugins/SearchEngine/SqlSearch.php
+++ app/lib/core/Plugins/SearchEngine/SqlSearch.php
@@ -1836,7 +1836,13 @@ class WLPlugSearchEngineSqlSearch extends BaseSearchPlugin implements IWLPlugSea
 		// add new indexing
 		if (is_array(WLPlugSearchEngineSqlSearch::$s_doc_content_buffer) && sizeof(WLPlugSearchEngineSqlSearch::$s_doc_content_buffer)) {
 			while(sizeof(WLPlugSearchEngineSqlSearch::$s_doc_content_buffer) > 0) {
+				$this->opo_db->query("SET unique_checks=0");
+				$this->opo_db->query("SET foreign_key_checks=0");
+				$this->opo_db->query("SET autocommit=0");
 				$this->opo_db->query($this->ops_insert_word_index_sql."\n".join(",", array_splice(WLPlugSearchEngineSqlSearch::$s_doc_content_buffer, 0, $vn_max_word_segment_size)));
+				$this->opo_db->query("COMMIT");
+				$this->opo_db->query("SET unique_checks=1");
+				$this->opo_db->query("SET foreign_key_checks=1");
 			}
 			if ($this->debug) { Debug::msg("[SqlSearchDebug] Commit row indexing"); }
 		}
