006af95a03f03a1c9cbea3be203c07faf7fdd56a
Seth <seth@collectiveaccess.org>
2016-11-02 20:20:20 -0400

    NOJIRA adjust path for current HTMLPurifier

--- app/lib/ca/ConfigurationCheck.php
+++ app/lib/ca/ConfigurationCheck.php
@@ -312,7 +312,7 @@ final class ConfigurationCheck {
 	 * Does the HTMLPurifier DefinitionCache dir exist and is it writable?
 	 */
 	public static function htmlPurifierDirQuickCheck() {
-		$vs_purifier_path = __CA_BASE_DIR__.'/vendor/ezyang/htmlpurifier/library/HTMLPurifier/DefinitionCache/Serializer';
+		$vs_purifier_path = __CA_BASE_DIR__.'/vendor/ezyang/htmlpurifier/library/HTMLPurifier/DefinitionCache';
 
 		if(!file_exists($vs_purifier_path) || !is_writable($vs_purifier_path)){
 			self::addError(_t("It looks like the directory for HTML filtering caches is not writable by the webserver. Please change the permissions of %1 and enable the user which runs the webserver to write this directory.", $vs_purifier_path));
