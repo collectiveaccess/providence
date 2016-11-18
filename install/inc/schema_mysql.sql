9979c34f4cddb8718825756e20b96a4f2eaa2813
Seth <seth@collectiveaccess.org>
2016-11-18 09:36:15 -0500

    PAWX-63 Add media table and models for content management interface

--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -6813,6 +6813,34 @@ create table ca_site_pages (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /*==========================================================================*/
+
+create table ca_site_page_media (
+  media_id		      	int unsigned        not null  AUTO_INCREMENT,
+  page_id               int unsigned        not null references ca_site_pages(page_id),
+  title					varchar(255)		not null,
+  caption			    text				not null,
+  idno                  varchar(255)        not null,
+  idno_sort             varchar(255)        not null,
+  media        			longblob            not null,
+  media_metadata        longblob            not null,
+  media_content			longtext			not null,
+  md5                   varchar(32)         not null,
+  mimetype              varchar(255)        null,
+  original_filename     varchar(1024)       not null, 
+  rank					int unsigned		not null default 0,
+  access                tinyint unsigned    not null default 0,
+  deleted               tinyint unsigned    not null default 0,
+
+  primary key (media_id),
+  key (page_id),
+  key (rank),
+  key (md5),
+  key (idno),
+  key (idno_sort),
+  unique index u_idno (page_id, idno)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+/*==========================================================================*/
 /* Schema update tracking                                                   */
 /*==========================================================================*/
 create table ca_schema_updates (
@@ -6823,5 +6851,5 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-/* CURRENT MIGRATION: 141 */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (141, unix_timestamp());
+/* CURRENT MIGRATION: 142 */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (142, unix_timestamp());
