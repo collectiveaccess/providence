d5b4bce044fcf30cd708e64c3685b4a125cd1acb
Seth <seth@collectiveaccess.org>
2017-02-18 14:50:40 -0500

    NOJIRA be specific

--- app/models/ca_sets.php
+++ app/models/ca_sets.php
@@ -1847,7 +1847,7 @@ LEFT JOIN ca_object_representations AS cor ON coxor.representation_id = cor.repr
 		}
 		
 		$qr_res = $o_db->query("
-			SELECT count(distinct row_id) c
+			SELECT count(distinct ca_set_items.row_id) c
 			FROM ca_set_items
 			INNER JOIN {$vs_rel_table_name} ON {$vs_rel_table_name}.{$vs_rel_table_pk} = ca_set_items.row_id
 			WHERE
