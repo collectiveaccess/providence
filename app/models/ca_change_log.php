7a1a126cb23d86da44a96cd023851d08cac975a0
Seth <seth@collectiveaccess.org>
2017-09-09 17:21:24 -0400

    NOJIRA refactor MediaInfoCoder to avoid reparsing metadata blocks

--- app/models/ca_change_log.php
+++ app/models/ca_change_log.php
@@ -552,8 +552,8 @@ class ca_change_log extends BaseModel {
 												$va_snapshot[$vs_fld."_media_desc"] = array_shift($t_instance->get($vs_fld, array('returnWithStructure' => true)));
 											}
 										} elseif(is_array($va_snapshot[$vs_fld])) { // back in the day it would store the full media array here
-											$o_coder = MediaInfoCoder::load();
-											$va_snapshot[$vs_fld] = $o_coder->getMediaUrl($va_snapshot[$vs_fld], 'original');
+											$o_coder = new MediaInfoCoder($va_snapshot[$vs_fld]);
+											$va_snapshot[$vs_fld] = $o_coder->getMediaUrl('original');
 										}
 									} else { // if it's not the latest, we don't care about the media
 										unset($va_snapshot[$vs_fld]);
