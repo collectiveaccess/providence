98916d7866984b70b45df7ef7eee0839ffd30fad
Seth <seth@collectiveaccess.org>
2018-01-22 23:41:54 -0500

    NOJIRA Preview warning

--- app/lib/ca/BaseEditorController.php
+++ app/lib/ca/BaseEditorController.php
@@ -625,7 +625,7 @@ class BaseEditorController extends ActionController {
 			$vn_display_id = $this->request->user->getVar($t_subject->tableName().'_summary_display_id');
 		}
 		if (!isset($va_displays[$vn_display_id]) || (is_array($va_displays[$vn_display_id]['settings']['show_only_in']) && sizeof($va_displays[$vn_display_id]['settings']['show_only_in']) && !in_array('editor_summary', $va_displays[$vn_display_id]['settings']['show_only_in']))) {
-		    $va_tmp = array_filter($va_displays, function($v) { return in_array('editor_summary', $v['settings']['show_only_in']); });
+		    $va_tmp = array_filter($va_displays, function($v) { return is_array($v['settings']['show_only_in']) ? in_array('editor_summary', $v['settings']['show_only_in']) : false; });
 		    $vn_display_id = sizeof($va_tmp) > 0 ? array_shift(array_keys($va_tmp)) : 0;
 		}
 
