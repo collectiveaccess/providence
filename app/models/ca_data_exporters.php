ef3491643903bb075665bf41de71524ef8588e5a
Seth <seth@collectiveaccess.org>
2018-04-05 17:56:53 -0400

    PROV-2392 Allow vars in templates and skipIfExpression; skip single values in repeats with skipIfExpression; allow CSV to return repeating values

--- app/models/ca_data_exporters.php
+++ app/models/ca_data_exporters.php
@@ -1514,6 +1514,16 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 
 		$va_export = array();
 
+		foreach($t_exporter->getTopLevelItems() as $va_item) {			
+			// get variables
+			if(preg_match("/^_VARIABLE_:(.*)$/", $va_item['element'], $va_matches)) {
+				if(!$t_instance) {
+					$t_instance = Datamodel::load()->getInstance($t_exporter->get('table_num'));
+					$t_instance->load($pn_record_id);
+				}
+				ca_data_exporters::$s_variables[$va_matches[1]] = $t_instance->get($va_item['source']);
+			}
+		}
 		foreach($t_exporter->getTopLevelItems() as $va_item) {
 			$va_export = array_merge($va_export, $t_exporter->processExporterItem($va_item['item_id'], $t_exporter->get('table_num'), $pn_record_id, $pa_options));
 		}
@@ -1850,6 +1860,7 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 					$va_item_info[] = array(
 						'text' => $vs_get_with_template,
 						'element' => $vs_element,
+						'template' => $vs_template
 					);
 				}
 			} elseif($vs_source) { // trying to find the source only makes sense if the source is set
@@ -1973,14 +1984,11 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 					);
 				} else { // user wants current element repeated in case of multiple returned values
 					
-					$va_get_options['delimiter'] = ';#;';
-					$vs_values = $t_instance->get($vs_source,$va_get_options);
+					$va_values = $t_instance->get($vs_source, array_merge($va_get_options, ['returnAsArray' => true]));
 					$o_log->logDebug(_t("Source is a get() that should be repeated for multiple values. Value for this mapping is '%1'. It includes the custom delimiter ';#;' that is later used to split the value into multiple values.", $vs_values));
 					$o_log->logDebug(_t("get() options are: %1", print_r($va_get_options,true)));
 
-					$va_tmp = explode(";#;",$vs_values);
-					
-					foreach($va_tmp as $vn_i => $vs_text) {
+					foreach($va_values as $vn_i => $vs_text) {
 						// handle skipIfExpression setting
 						if($vs_skip_if_expr) {
 							// Add current value as variable "value", accessible in expressions as ^value
@@ -1993,7 +2001,6 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 								}
 							}
 							if(ExpressionParser::evaluate($vs_skip_if_expr, $va_vars)) {
-								unset($va_item_info[$vn_key]);
 								continue;
 							}
 						}
@@ -2014,9 +2021,9 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 			$va_item_info[] = array(
 				'element' => $vs_element,
 				'text' => $vs_proc_template,
-			);
+				'template' => $vs_template
+			);		
 		} else { // no source, no template -> probably wrapper
-
 			$o_log->logDebug(_t("Current mapping has no source and no template and is probably an XML/MARC wrapper element"));
 
 			$va_item_info[] = array(
@@ -2059,13 +2066,15 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 				continue; 
 			}
 
-			// handle skipIfExpression setting
-			if($vs_skip_if_expr) {
+			// handle skipIfExpression setting for non-repeating 
+			if($vs_skip_if_expr && !$vb_repeat) {
 				// Add current value as variable "value", accessible in expressions as ^value
-				$va_vars = array_merge(array('value' => $va_item['text']), ca_data_exporters::$s_variables);
+				$va_vars = ca_data_exporters::$s_variables;
+				$va_vars['value'] = $va_item['text'];
 				
 				if(is_array($va_expr_tags)) {
 					foreach($va_expr_tags as $vs_expr_tag) {
+						if (isset($va_vars[$vs_expr_tag])) { continue; }
 						$va_vars[$vs_expr_tag] = $t_instance->get($vs_expr_tag, ['convertCodesToIdno' => true]);
 					}
 				}
@@ -2078,6 +2087,11 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 
 			// do replacements
 			$va_item['text'] = ca_data_exporter_items::replaceText($va_item['text'],$va_replacements);
+			
+			// do templates
+			if (isset($va_item['template']) || (isset($va_get_options['template']) && $va_get_options['template'])) {
+				$va_item['text'] = caProcessTemplate($va_item['text'], ca_data_exporters::$s_variables);
+			}
 
 			// if text is empty, fill in default
 			// if text isn't empty, respect prefix and suffix
@@ -2089,14 +2103,6 @@ class ca_data_exporters extends BundlableLabelableBaseModelWithAttributes {
 
 			if($vn_max_length && (strlen($va_item['text']) > $vn_max_length)) {
 				$va_item['text'] = substr($va_item['text'], 0, $vn_max_length)." ...";
-			}
-
-			// if this is a variable, set the value and delete it from the export tree
-			$va_matches = array();
-			if(preg_match("/^_VARIABLE_:(.*)$/",$va_item['element'],$va_matches)) {
-				ca_data_exporters::$s_variables[$va_matches[1]] = $va_item['text'];
-				unset($va_item_info[$vn_key]);
-				continue;
 			}
 
 			// if returned value is null then we skip the item
