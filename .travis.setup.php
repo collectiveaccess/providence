9ec35b0f10a0da06eed81dd839f9b10f87b0bf79
Seth <seth@collectiveaccess.org>
2018-04-12 11:15:36 -0400

    NOJIRA fix tests?

--- .travis.setup.php
+++ .travis.setup.php
@@ -17,7 +17,7 @@ if (!defined("__CA_APP_DISPLAY_NAME__")) {
 	define("__CA_APP_DISPLAY_NAME__", 'CollectiveAccess Unit Tests');
 }
 if (!defined("__CA_DB_TYPE__")) {
-	define("__CA_DB_TYPE__", 'pdo_mysql');
+	define("__CA_DB_TYPE__", 'mysqli');
 }
 
 # Include the defaults.
