580d4d3ebcc8306b9504946d047371cad2723ee1
Seth <seth@collectiveaccess.org>
2018-05-23 12:47:09 -0400

    PROV-2454 Filter current for multiple items

--- app/lib/ca/BundlableLabelableBaseModelWithAttributes.php
+++ app/lib/ca/BundlableLabelableBaseModelWithAttributes.php
@@ -5909,7 +5909,12 @@ $this->update();
 			//
 		}
 		if ($pb_show_current_only) {
-		    $va_rels = array_slice($va_rels, sizeof($va_rels)-1, 1);
+		    $va_tmp = [];
+		    foreach(array_reverse($va_rels) as $rel) {
+		        if(isset($va_tmp[$rel['row_id']])) { continue; }
+		        $va_tmp[$rel['row_id']] = $rel;
+		    }
+		    $va_rels = array_values($va_tmp);
 		}
 
 		// Apply restrictToBundleValues
