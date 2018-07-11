b28471ddf1d2f139d559f55efd5a5055977532d3
Julia <julia@whirl-i-gig.com>
2018-07-11 17:02:23 -0400

    PROV-2498 Honor restrictions for sub-items

--- app/lib/BaseEditorController.php
+++ app/lib/BaseEditorController.php
@@ -1374,11 +1374,11 @@ class BaseEditorController extends ActionController {
 		$va_subtypes_proc = array();
 
         $va_limit_to_types = $this->getRequest()->config->get($this->ops_table_name.'_navigation_new_menu_limit_types_to');
-        
+        $va_limit_to_type_ids = (is_array($va_limit_to_types) && sizeof($va_limit_to_types)) ? caMakeTypeIDList($this->ops_table_name, $va_limit_to_types, ['dontIncludeSubtypesInTypeRestriction' => true]) : null;
 		foreach($va_subtypes as $vs_sort_key => $va_type) {
 			foreach($va_type as $vn_item_id => $va_item) {
 				if (is_array($pa_restrict_to_types) && !in_array($vn_item_id, $pa_restrict_to_types)) { continue; }
-				if (is_array($va_limit_to_types) && sizeof($va_limit_to_types) && !in_array($va_item['idno'], $va_limit_to_types)) { continue; }
+				if (is_array($va_limit_to_type_ids) && sizeof($va_limit_to_type_ids) && !in_array($va_item['parameters']['type_id'], $va_limit_to_type_ids)) { continue; }
 			    
 				$va_subtypes_proc[$vn_item_id] = $va_item;
 			}
