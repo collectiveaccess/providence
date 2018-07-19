0119a093b10bbc13b4ee9bffdc1c7fc76d36711d
Seth <seth@collectiveaccess.org>
2018-07-18 20:12:46 -0400

    NOJIRA Update Guzzle auth calls

--- app/lib/Plugins/InformationService/ResourceSpace.php
+++ app/lib/Plugins/InformationService/ResourceSpace.php
@@ -39,8 +39,6 @@ require_once(__CA_LIB_DIR__."/Plugins/IWLPlugInformationService.php");
 require_once(__CA_LIB_DIR__."/Plugins/InformationService/BaseInformationServicePlugin.php");
 require_once(__CA_LIB_DIR__."/Zend/Feed.php");
 
-use Guzzle\Http\Client;
-
 global $g_information_service_settings_ResourceSpae;
 $g_information_service_settings_ResourceSpace = array(
 		'user' => array(
