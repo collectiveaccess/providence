aed377b529e7800bc4f6d6c9c02597dc849676dc
Seth <seth@collectiveaccess.org>
2019-01-19 11:03:04 -0500

    NOJIRA add OpenStreetMaps

--- index.php
+++ index.php
@@ -83,8 +83,8 @@
 		// Security headers
 		$resp->addHeader("X-XSS-Protection", "1; mode=block");
 		$resp->addHeader("X-Frame-Options", "SAMEORIGIN");
-		$resp->addHeader("Content-Security-Policy", "script-src 'self' ajax.googleapis.com maps.googleapis.com cdn.knightlab.com 'unsafe-inline' 'unsafe-eval';"); 
-		$resp->addHeader("X-Content-Security-Policy", "script-src 'self' ajax.googleapis.com maps.googleapis.com cdn.knightlab.com 'unsafe-inline' 'unsafe-eval';"); 
+		$resp->addHeader("Content-Security-Policy", "script-src 'self' ajax.googleapis.com nominatim.openstreetmap.org maps.googleapis.com cdn.knightlab.com 'unsafe-inline' 'unsafe-eval';"); 
+		$resp->addHeader("X-Content-Security-Policy", "script-src 'self' ajax.googleapis.com nominatim.openstreetmap.org maps.googleapis.com cdn.knightlab.com 'unsafe-inline' 'unsafe-eval';"); 
 
 		//
 		// Don't try to authenticate when doing a login attempt or trying to access the 'forgot password' feature
