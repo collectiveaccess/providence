aad4399b1708ae8508ccd36b017ed4355c381b49
Seth <seth@collectiveaccess.org>
2019-06-16 18:23:54 -0400

    PROV-2704 Automate alt tag creation

--- app/lib/RepresentableBaseModel.php
+++ app/lib/RepresentableBaseModel.php
@@ -69,6 +69,12 @@
 			if (caGetBundleAccessLevel($this->tableName(), 'ca_object_representations') == __CA_BUNDLE_ACCESS_NONE__) {
 				return null;
 			}
+			
+			if ($alt_text_template = Configuration::load()->get($this->tableName()."_alt_text_template")) { 
+                $alt_text = $this->getWithTemplate($alt_text_template);
+            } else {
+                $alt_text = $this->get($this->tableName().".preferred_labels");
+            }
 		
 			if (!is_array($pa_versions)) { 
 				$pa_versions = array('preview170');
@@ -148,9 +154,9 @@
 					}
 				
 					if ($vn_width && $vn_height) {
-						$va_tmp['tags'][$vs_version] = $qr_reps->getMediaTag('media', $vs_version, array_merge($pa_options, array('viewer_width' => $vn_width, 'viewer_height' => $vn_height)));
+						$va_tmp['tags'][$vs_version] = $qr_reps->getMediaTag('media', $vs_version, array_merge($pa_options, array('alt' => $alt_text, 'viewer_width' => $vn_width, 'viewer_height' => $vn_height)));
 					} else {
-						$va_tmp['tags'][$vs_version] = $qr_reps->getMediaTag('media', $vs_version, $pa_options);
+						$va_tmp['tags'][$vs_version] = $qr_reps->getMediaTag('media', $vs_version, array_merge($pa_options, ['alt' => $alt_text]));
 					}
 					$va_tmp['urls'][$vs_version] = $qr_reps->getMediaUrl('media', $vs_version);
 					$va_tmp['paths'][$vs_version] = $qr_reps->getMediaPath('media', $vs_version);
@@ -1043,12 +1049,22 @@
 					(oxor.{$vs_pk} IN (".join(',', $pa_ids).")) AND oxor.is_primary = 1 AND orep.deleted = 0 {$vs_access_where}
 			");
 		
-			$va_media = array();
+		    $qr = caMakeSearchResult($this->tableName(), $pa_ids);
+		    $alt_texts = [];
+		    while($qr->nextHit()) {
+                if ($alt_text_template = Configuration::load()->get($this->tableName()."_alt_text_template")) { 
+                    $alt_texts[$qr->get($vs_pk)] = $qr->getWithTemplate($alt_text_template);
+                } else {
+                    $alt_texts[$qr->get($vs_pk)] = $qr->get($this->tableName().".preferred_labels");
+                }
+            }
+            
+			$va_media = [];
 			while($qr_res->nextRow()) {
-				$va_media_tags = array();
+				$va_media_tags = [];
 				foreach($pa_versions as $vs_version) {
 					$va_media_tags['representation_id'] = $qr_res->get('ca_object_representations.representation_id');
-					$va_media_tags['tags'][$vs_version] = $qr_res->getMediaTag('ca_object_representations.media', $vs_version);
+					$va_media_tags['tags'][$vs_version] = $qr_res->getMediaTag('ca_object_representations.media', $vs_version, ['alt' => $alt_texts[$qr_res->get($vs_pk)]]);
 					$va_media_tags['info'][$vs_version] = $qr_res->getMediaInfo('ca_object_representations.media', $vs_version);
 					$va_media_tags['urls'][$vs_version] = $qr_res->getMediaUrl('ca_object_representations.media', $vs_version);
 				}
@@ -1056,7 +1072,7 @@
 			}
 		
 			// Preserve order of input ids
-			$va_media_sorted = array();
+			$va_media_sorted = [];
 			foreach($pa_ids as $vn_id) {
 				$va_media_sorted[$vn_id] = $va_media[$vn_id];
 			} 
