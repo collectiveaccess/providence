59392b4ee553c508dd264b73c053667299e5029b
Seth <seth@collectiveaccess.org>
2019-08-21 23:46:36 -0400

    NOJIRA add XML => delimited data converter tool

--- app/lib/Utils/CLITools.php
+++ app/lib/Utils/CLITools.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2014-2016 Whirl-i-Gig
+ * Copyright 2014-2019 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -102,11 +102,11 @@
 				}
 			}
 			
-			$vs_locale = 'en_US';
+			$locale = 'en_US';
 		
 			$vs_output = "<list code=\"LIST_CODE_HERE\" hierarchical=\"0\" system=\"0\" vocabulary=\"0\">\n";
 			$vs_output .= "\t<labels>
-		<label locale=\"{$vs_locale}\">
+		<label locale=\"{$locale}\">
 			<name>ListNameHere</name>
 		</label>
 	</labels>\n";
@@ -126,7 +126,7 @@
 		# -------------------------------------------------------
 		private static function _makeList($pa_list, $pn_indent=0, $pa_stack=null) {
 			if(!is_array($pa_stack)) { $pa_stack = array(); }
-			$vs_locale = 'en_US';
+			$locale = 'en_US';
 			$vn_ident = $pn_indent ? str_repeat("\t", $pn_indent) : '';
 			$vs_buf = '';
 			foreach($pa_list as $vn_i => $va_item) {
@@ -135,7 +135,7 @@
 				if ($vs_label_prefix = join('_', $pa_stack)) { $vs_label_prefix .= '_'; }
 				$vs_buf .= "{$vn_ident}<item idno=\"{$vs_label_prefix}{$vs_label_proc}\" enabled=\"1\" default=\"0\">
 {$vn_ident}\t<labels>
-{$vn_ident}\t\t<label locale=\"{$vs_locale}\" preferred=\"1\">
+{$vn_ident}\t\t<label locale=\"{$locale}\" preferred=\"1\">
 {$vn_ident}\t\t\t<name_singular>{$vs_label}</name_singular>
 {$vn_ident}\t\t\t<name_plural>{$vs_label}</name_plural>
 {$vn_ident}\t\t</label>
@@ -178,6 +178,144 @@
 		 */
 		public static function make_list_from_excelShortHelp() {
 			return _t("Create a profile <list> element from an Excel spreadsheet. ");
+		}
+		# -------------------------------------------------------
+		# Create a profile <relationshipTable> element from an Excel spreadsheet. 
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function make_relationship_types_from_excel($po_opts=null) {
+			$vs_filepath = (string)$po_opts->getOption('file');
+			if (!$vs_filepath) { 
+				CLITools::addError(_t("You must specify a file", $vs_filepath));
+				return false;
+			}
+			if (!file_exists($vs_filepath)) { 
+				CLITools::addError(_t("File '%1' does not exist", $vs_filepath));
+				return false;
+			}
+			
+			if ($vs_output_path = (string)$po_opts->getOption('out')) {
+				if (!is_writeable(pathinfo($vs_output_path, PATHINFO_DIRNAME))) { 
+					CLITools::addError(_t("Cannot write to %1", $vs_output_path));
+					return false;
+				}
+			}
+			
+			$vn_skip = (int)$po_opts->getOption('skip');
+			
+			$o_handle = PHPExcel_IOFactory::load($vs_filepath);
+			$o_sheet = $o_handle->getActiveSheet();
+			
+			
+			$vn_c = 0;
+			$vn_last_level = 0;
+			$va_list = array();
+			$va_stack = array(&$va_list);
+			
+			foreach ($o_sheet->getRowIterator() as $o_row) {
+				$vn_c++;
+				if ($vn_skip >= $vn_c) { continue; }
+				
+				$va_row = array();
+				$o_cells = $o_row->getCellIterator();
+				$o_cells->setIterateOnlyExistingCells(false); 
+
+				$vn_col = 0;
+				foreach ($o_cells as $o_cell) {
+					if($vs_val = trim((string)$o_cell->getValue())) {
+						if ($vn_col > $vn_last_level) {
+							$va_stack[] = &$va_stack[sizeof($va_stack)-1][sizeof($va_stack[sizeof($va_stack)-1]) - 1]['subitems'];
+						} elseif ($vn_col < $vn_last_level) {
+							while(sizeof($va_stack) && ($va_stack[sizeof($va_stack)-1][0]['level'] > $vn_col)) {
+								array_pop($va_stack);
+							}
+						}
+						$va_stack[sizeof($va_stack)-1][] = array('value' => $vs_val, 'subitems' => array(), 'level' => $vn_col);
+						$vn_last_level = $vn_col;
+						
+						$vn_col++;
+						break;
+					}
+			
+					$vn_col++;
+				}
+			}
+			
+			$locale = 'en_US';
+		
+			$vs_output = "<relationshipTable name=\"TABLE_CODE_HERE\">\n\t<types>\n";
+			$vs_output .= CLITools::_makeTypeList($va_list, 2);
+			$vs_output .= "\t</types>\n</relationshipTable>\n";
+			
+			if ($vs_output_path) {
+				file_put_contents($vs_output_path, $vs_output);
+				CLITools::addMessage(_t("Wrote output to %1", $vs_output_path));
+			} else {
+				print $vs_output;
+			}
+			return true;
+		}
+		# -------------------------------------------------------
+		private static function _makeTypeList($pa_list, $pn_indent=0, $pa_stack=null) {
+			if(!is_array($pa_stack)) { $pa_stack = []; }
+			$locale = 'en_US';
+			$vn_ident = $pn_indent ? str_repeat("\t", $pn_indent) : '';
+			$vs_buf = '';
+			foreach($pa_list as $vn_i => $va_item) {
+				$labels = explode(";", caEscapeForXML($va_item['value']));
+				$label_forward = trim($labels[0]);
+				$label_reverse = (isset($labels[1]) && strlen($labels[1]) ? $labels[1] : $labels[0]);
+				if (!strlen($label_forward) || !strlen($label_reverse)) { continue; }
+				
+				$label_proc = preg_replace("![^A-Za-z0-9]+!", "_", $label_forward);
+				if ($label_prefix = join('_', $pa_stack)) { $label_prefix .= '_'; }
+				$vs_buf .= "{$vn_ident}<type code=\"{$label_prefix}{$label_proc}\" default=\"0\">
+{$vn_ident}\t<labels>
+{$vn_ident}\t\t<label locale=\"{$locale}\">
+{$vn_ident}\t\t\t<typename>{$label_forward}</typename>
+{$vn_ident}\t\t\t<typename_reverse>{$label_reverse}</typename_reverse>
+{$vn_ident}\t\t</label>
+{$vn_ident}\t</labels>\n".
+	((is_array($va_item['subitems']) && sizeof($va_item['subitems'])) ? "{$vn_ident}\t<types>\n{$vn_indent}".CLITools::_makeTypeList($va_item['subitems'], $pn_indent + 2, array_merge($pa_stack, array(substr($vs_label_proc, 0, 10))))."{$vn_ident}\t</types>" : '')."
+{$vn_ident}</type>\n";
+				
+			}
+			
+			return $vs_buf;
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function make_relationship_types_from_excelParamList() {
+			return array(
+				"file|f-s" => _t('Excel file to convert to profile <relationshipTable> element.'),
+				"out|o-s" => _t('File to write output to.'),
+				"skip|s-s" => _t('Number of rows to skip before reading data.')
+			);
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function make_relationship_types_from_excelUtilityClass() {
+			return _t('Profile development tools');
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function make_relationship_types_from_excelHelp() {
+			return _t("Create a profile <relationshipTable> element from an Excel spreadsheet. Your relationship type list should have one type per row, with hierarchical level indicated by indentation. Row text will be used for both forward and reverse types. If you wish to have different text for forward and reverse separate the text for each with a semicolon. For example, if you want to have a list with A, B, C, D, E and F, with B and C sub-items of A and F a sub-item of E your Excel document should look like this:\n\n\tA\n\t\tB\n\t\tC\n\tD\n\tE\n\t\tF\n\n\tIf your Excel document has column headers you can skip them by specifying the number of rows to skip using the \"skip\" option.");
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function make_relationship_types_from_excelShortHelp() {
+			return _t("Create a profile <relationshipTable> element from an Excel spreadsheet. ");
 		}
 		# -------------------------------------------------------
 		# Get EXIF tags
@@ -278,7 +416,134 @@
 		 *
 		 */
 		public static function get_exif_tagsShortHelp() {
-			return _t("Analyzes a directory of images and returns a tab-delimited  list of import-mapping compatible EXIF and IPTC tags containing data.");
+			return _t("Analyzes a directory of images and returns a tab-delimited list of import-mapping compatible EXIF and IPTC tags containing data.");
+		}
+		# -------------------------------------------------------
+		# Convert XML files to delimited
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function convert_xml_to_delimited($po_opts=null) {
+			require_once(__CA_LIB_DIR__."/Import/DataReaders/ExifDataReader.php");
+			
+			$file_path = (string)$po_opts->getOption('file');
+			if (!$file_path) { 
+				CLITools::addError(_t("You must specify a file", $file_path));
+				return false;
+			}
+			if (!file_exists($file_path)) { 
+				CLITools::addError(_t("File '%1' does not exist", $file_path));
+				return false;
+			}
+			if (is_dir($file_path)) { 
+				CLITools::addError(_t("'%1' must not be a directory", $file_path));
+				return false;
+			}
+			if (!($output_path = (string)$po_opts->getOption('out'))) {
+				CLITools::addError(_t("You must specify an output file"));
+				return false;
+			}
+			if (!is_writeable(pathinfo($output_path, PATHINFO_DIRNAME))) { 
+				CLITools::addError(_t("Cannot write to %1", $output_path));
+				return false;
+			}
+			
+			
+			if (!($xpath = strtolower((string)$po_opts->getOption('xpath')))) {
+				$input_format = strtolower((string)$po_opts->getOption('format'));
+			
+				switch($input_format) {
+					case 'pastperfect':
+					case 'pp':
+						$xpath = "//export";
+						break;
+					default:
+						if ($input_format) {
+							CLITools::addError(_t("%1 is not a valid input format. Defaulting to PastPerfect.", $input_format));
+						} else {
+							CLITools::addMessage(_t("No input format was specified. Defaulting to PastPerfect."));
+						}
+						$xpath = "//export";
+						break;
+				}
+			}
+			
+			$output_format = strtolower((string)$po_opts->getOption('outputFormat'));
+			if (!in_array($output_format, ['csv', 'tab'])) { 
+				CLITools::addMessage(_t("No output format was specified. Defaulting to CSV."));
+				$format = 'csv'; 
+			}
+			
+			$xml = simplexml_load_file($file_path);
+			
+			$q = $xml->xpath($xpath);
+			
+			$headers = [];
+			$c = 0;
+			
+			$delimiter = ($output_format === 'csv') ? "," : "\t";
+			
+			if (!($fp = fopen($output_path, "w"))) {
+				CLITools::addError(_t("Could not open output file %1", $output_path));
+				return;
+			}
+			foreach($q as $e) {
+				$line = [];
+				foreach($e->children() as $t) {
+					if ($c == 0) {
+						$headers[] = $t->getName();
+					}
+					$line[] = (string)$t;
+				}
+				if ($c == 0) {
+					fwrite($fp, join($delimiter, array_map(function($v) {
+						return '"'.str_replace('"', '""', $v).'"';
+					}, $headers))."\n");
+				}
+				fwrite($fp, join($delimiter, array_map(function($v) {
+					return '"'.str_replace('"', '""', $v).'"';
+				}, $line))."\n");
+				
+				$c++;
+			}
+			fclose($fp);
+			
+			CLITools::addMessage(_t("Wrote output to '%1'", $output_path));
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function convert_xml_to_delimitedParamList() {
+			return array(
+				"file|f-s" => _t('XML file to convert.'),
+				"out|o-s" => _t('File to write delimited output to.'),
+				"format|i=s" => _t('XML format of input file. Only valid option is "PastPerfect" (PastPerfect XML export files).'),
+				"xpath|x=s" => _t('XPath expression to select XML tags for conversion. If set, input format option is igored.'),
+				"outputFormat|t=s" => _t('Format of output (CSV or tab-delimited). Default is CSV.'),
+			);
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function convert_xml_to_delimitedUtilityClass() {
+			return _t('Data conversion tools');
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function convert_xml_to_delimitedHelp() {
+			return _t("Convert a selected tags in an XML file to delimited text (CSV or tab-delimited).");
+		}
+		# -------------------------------------------------------
+		/**
+		 *
+		 */
+		public static function convert_xml_to_delimitedShortHelp() {
+			return _t("Convert a selected tags in an XML file to delimited text (CSV or tab-delimited). All sub-tags of selected tags are output into delimited rows of data. Tags may be selected using an XPath expression or with file format presets. For example, the input format \"PastPerfect\" will employ the XPath expression //export to select all <export> tags and output data tags contained within into rows of delimited data.");
 		}
 		# -------------------------------------------------------
 	}
