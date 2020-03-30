3aba7a7a0bf0f88a0232515c761a4561ebbcac2a
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-03-30 17:12:59 +0200

    NOJIRA Travis improvements.

--- tests/database_setup.sh
+++ tests/database_setup.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-# Set environment variables
-export PROFILE=${1:-testing}
-export DB_NAME=ca_test
-export DB_USER=ca_test
-export DB_PASSWORD=password
-export COLLECTIVEACCESS_HOME="$(dirname $(dirname "$0"))"
-export PATH="$PATH:$COLLECTIVEACCESS_HOME/support/bin"
+# Set environment variables.
+export DB_NAME=${DB_NAME:-ca_test}
+export DB_USER=${DB_USER:-ca_test}
+export DB_PASSWORD=${DB_PASSWORD:-password}
 
-# Initialise the database instance for the test
+# Initialise the database instance for the test.
 sudo mysql -uroot -e "DROP DATABASE ${DB_NAME};"
 sudo mysql -uroot -e "create database ${DB_NAME};"
 sudo mysql -uroot -e "grant all on ${DB_NAME}.* to '${DB_USER}'@'localhost' identified by '${DB_PASSWORD}';"
 
-# Install the testing profile
-"$COLLECTIVEACCESS_HOME"/support/bin/caUtils install --hostname=localhost --setup="tests/setup-tests.php" \
-  --skip-roles --profile-name="$PROFILE" --admin-email=support@collectiveaccess.org
+# Add custom configuration from file.
+MYCNF_CONFIG="${MYCNF_CONFIG:-$TRAVIS_BUILD_DIR/tests/my.cnf}"
+if test -e "$MYCNF_CONFIG";
+then
+  cat "$MYCNF_CONFIG" | sudo tee -a /etc/mysql/my.cnf;
+fi
 
+# Restart service.
+sudo service mysql restart
+
+# Show variables.
+sudo mysql -uroot -e 'show variables;'
