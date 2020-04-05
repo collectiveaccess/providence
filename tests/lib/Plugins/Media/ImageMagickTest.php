f60ab0c981b2386f7150ce74fbcbb6de5ebc94ad
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-04-05 20:05:10 +0200

    Move common testing code to setup method.

--- tests/lib/Plugins/Media/ImageMagickTest.php
+++ tests/lib/Plugins/Media/ImageMagickTest.php
@@ -23,53 +23,60 @@
  * the "license.txt" file for details, or visit the CollectiveAccess web site at
  * http://www.CollectiveAccess.org
  *
- * @package CollectiveAccess
+ * @package    CollectiveAccess
  * @subpackage tests
- * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
- * @author Orestes Sanchez <orestes@estotienearreglo.es>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+ * @author     Orestes Sanchez <orestes@estotienearreglo.es>
  *
  * ----------------------------------------------------------------------
  */
+
 use PHPUnit\Framework\TestCase;
 
-require_once(__CA_APP_DIR__."/helpers/systemHelpers.php");
-require_once(__CA_APP_DIR__."/lib/Plugins/Media/ImageMagick.php");
+require_once( __CA_APP_DIR__ . "/helpers/systemHelpers.php" );
+require_once( __CA_APP_DIR__ . "/lib/Plugins/Media/ImageMagick.php" );
 
+/**
+ * Class ImageMagickTest
+ *
+ * Allow testing imagemagick media plugin.
+ * See configuration on tests/conf.
+ *
+ */
 class ImageMagickTest extends TestCase {
+
+	protected $im_plugin = null;
+	protected $prophet = null;
+
+	protected function setUp(): void {
+		$this->im_plugin = new WLPlugMediaImageMagick();
+		$this->im_plugin->register();
+	}
+
 	# -------------------------------------------------------
 	public function testConvertCmd() {
-		$im_plugin = new WLPlugMediaImageMagick();
-		$im_plugin->register();
-		$command = $im_plugin->command('convert');
-		$this->assertEquals('/usr/bin/convert', $command);
+		$command = $this->im_plugin->command( 'convert' );
+		$this->assertEquals( '/usr/bin/convert', $command );
 	}
 
 	public function testIdentifyCmd() {
-		$im_plugin = new WLPlugMediaImageMagick();
-		$im_plugin->register();
-		$command = $im_plugin->command('identify');
-		$this->assertEquals('/usr/bin/identify', $command);
+		$command = $this->im_plugin->command( 'identify' );
+		$this->assertEquals( '/usr/bin/identify', $command );
 	}
 
 	public function testWhateverCmd() {
-		$im_plugin = new WLPlugMediaImageMagick();
-		$im_plugin->register();
-		$command = $im_plugin->command('whatever');
-		$this->assertEquals('/usr/bin/whatever', $command);
+		$command = $this->im_plugin->command( 'whatever' );
+		$this->assertEquals( '/usr/bin/whatever', $command );
 	}
 
 	public function testMissingCmdWithEmptyArgs() {
-		$im_plugin = new WLPlugMediaImageMagick();
-		$im_plugin->register();
-		$command = $im_plugin->commandWithDefaultArgs('whatever');
-		$this->assertEquals('/usr/bin/whatever ', $command);
+		$command = $this->im_plugin->commandWithDefaultArgs( 'whatever' );
+		$this->assertEquals( '/usr/bin/whatever ', $command );
 	}
 
 	public function testConvertCmdWithDefaultArgs() {
-		$im_plugin = new WLPlugMediaImageMagick();
-		$im_plugin->register();
-		$command = $im_plugin->commandWithDefaultArgs('convert');
-		$this->assertEquals('/usr/bin/convert -limit thread 1', $command);
+		$command = $this->im_plugin->commandWithDefaultArgs( 'convert' );
+		$this->assertEquals( '/usr/bin/convert -limit thread 1', $command );
 	}
 
 }
