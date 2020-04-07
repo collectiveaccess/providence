7dad637a0eeab1bf59acea71517a536caf4aba2e
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-04-07 12:49:12 +0200

    Improve tracking widget: - Last update time was not shown when tabs were shown. - Refactor common code. - Use display helper for file size. - Allow horizontal scroll.

--- app/helpers/displayHelpers.php
+++ app/helpers/displayHelpers.php
@@ -4800,3 +4800,13 @@ require_once(__CA_LIB_DIR__.'/Media/MediaInfoCoder.php');
 		return $t->getDisplayLabel($bundle);
 	}
 	# ------------------------------------------------------------------
+	/**
+	 *
+	 */
+	function caDisplayName($fname, $lname, $default=null){
+		if ($fname && $lname){
+			return $fname . ' ' . $lname;
+		}
+		return _t($default);
+	}
+	# ------------------------------------------------------------------
