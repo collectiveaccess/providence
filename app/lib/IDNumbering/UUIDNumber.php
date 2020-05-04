86f7ad84ea85a102a3d3eab80b0dd86dda40648e
CollectiveAccess <seth@collectiveaccess.org>
2020-05-04 17:12:42 -0400

    NOJIRA Mind quotes

--- app/lib/IDNumbering/UUIDNumber.php
+++ app/lib/IDNumbering/UUIDNumber.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2007-2018 Whirl-i-Gig
+ * Copyright 2007-2020 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -225,8 +225,8 @@
 				$va_lookup_url_info = caJSONLookupServiceUrl($pa_options['request'], $pa_options['table']);
 				$vs_js .= "
 					caUI.initIDNoChecker({
-						errorIcon: '".$pa_options['error_icon']."',
-						processIndicator: '".$pa_options['progress_indicator']."',
+						errorIcon: \"".$pa_options['error_icon']."\",
+						processIndicator: \"".$pa_options['progress_indicator']."\",
 						idnoStatusID: 'idnoStatus',
 						lookupUrl: '".$va_lookup_url_info['idno']."',
 						searchUrl: '".$pa_options['search_url']."',
