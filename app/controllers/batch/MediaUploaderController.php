bb7498d3516388ccecdff346c00808d77ebd1379
CollectiveAccess <seth@collectiveaccess.org>
2020-06-17 00:46:14 -0400

    PROV-2912 Shell

---
+++ app/controllers/batch/MediaUploaderController.php
@@ -0,0 +1,86 @@
+<?php
+/** ---------------------------------------------------------------------
+ * app/lib/MediaUploaderController.php : 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2020 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * @package CollectiveAccess
+ * @subpackage UI
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+ *
+ * ----------------------------------------------------------------------
+ */
+ 
+ /**
+  *
+  */
+ 
+ 	require_once(__CA_APP_DIR__."/helpers/batchHelpers.php");
+ 
+ 	class MediaUploaderController extends ActionController {
+ 		# -------------------------------------------------------
+ 		#
+ 		# -------------------------------------------------------
+ 		public function __construct(&$po_request, &$po_response, $pa_view_paths=null) {
+ 			parent::__construct($po_request, $po_response, $pa_view_paths);
+ 			
+ 			// Can user batch import media?
+ 			if (!$po_request->user->canDoAction('can_batch_import_media')) {
+ 				$po_response->setRedirect($po_request->config->get('error_display_url').'/n/3410?r='.urlencode($po_request->getFullUrlPath()));
+ 				return;
+ 			}
+ 			
+ 			//AssetLoadManager::register('react');
+ 			
+ 		}
+ 		# -------------------------------------------------------
+ 		/**
+ 		 * Generates a form for specification of media import settings. The form is rendered into the current view, inherited from ActionController
+ 		 *
+ 		 * @param array $pa_values An optional array of values to preset in the format, overriding any existing values in the model of the record being editing.
+ 		 * @param array $pa_options Array of options passed through to _initView
+ 		 *
+ 		 */
+ 		public function Index($pa_values=null, $pa_options=null) {
+ 			AssetLoadManager::register("directoryBrowser");
+ 			
+ 		
+ 			$this->render('mediauploader/index_html.php');
+ 		}
+ 		
+		# ------------------------------------------------------------------
+ 		# Sidebar info handler
+ 		# ------------------------------------------------------------------
+ 		/**
+ 		 * Sets up view variables for upper-left-hand info panel (aka. "inspector"). Actual rendering is performed by calling sub-class.
+ 		 *
+ 		 * @param array $pa_parameters Array of parameters as specified in navigation.conf, including primary key value and type_id
+ 		 */
+ 		public function info($pa_parameters) {
+ 			
+			//$this->view->setVar('screen', $this->request->getActionExtra());						// name of screen
+			//$this->view->setVar('result_context', $this->getResultContext());
+			
+ 			return $this->render('mediauploader/widget_media_uploader_html.php', true);
+ 		}
+		# ------------------------------------------------------------------
+ 	}
---
+++ themes/default/views/batch/mediauploader/index_html.php
@@ -0,0 +1,14 @@
+<h1>Hello</h2>
+
+<div id="mediaUploaderUI">
+	UI goes here
+</div>
+
+<script type="text/javascript">	
+	providenceUIApps['mediauploader'] = {
+        'selector': '#mediaUploaderUI',
+        'data': {
+            'message': 'Wow, it works!'
+        }
+    };
+</script>
