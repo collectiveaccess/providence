b712a5fc47fff744544ee7769f5253f52f0c983a
CollectiveAccess <seth@collectiveaccess.org>
2020-07-03 12:10:08 -0400

    NOJIRA restore setting of system guid constant

--- app/helpers/preload.php
+++ app/helpers/preload.php
@@ -78,7 +78,7 @@ require_once(__CA_LIB_DIR__.'/Controller/ActionController.php');
 require_once(__CA_MODELS_DIR__.'/ca_acl.php');
 
 require_once(__CA_APP_DIR__.'/lib/GarbageCollection.php');
-#require_once(__CA_APP_DIR__.'/helpers/guidHelpers.php');
+require_once(__CA_APP_DIR__.'/helpers/guidHelpers.php');
 
 
 require_once(__CA_LIB_DIR__."/Datamodel.php");
