75651b63df0dfbaae40c472abf59458ebe8f417b
CollectiveAccess <seth@collectiveaccess.org>
2020-07-31 10:01:15 -0400

    TD-54 Add label text to media overlay "close" icon

--- themes/default/views/editor/object_representations/ajax_representation_annotation_editor_html.php
+++ themes/default/views/editor/object_representations/ajax_representation_annotation_editor_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013-2015 Whirl-i-Gig
+ * Copyright 2013-2020 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -45,7 +45,7 @@
 
 <div class="caMediaOverlayControls">
 	<div class="objectInfo"><?php print "{$vs_media_type}; ".caGetRepresentationDimensionsForDisplay($t_rep, 'original'); ?></div>
-	<div class='close'><a href="#" onclick="caMediaPanel.hidePanel(); return false;" title="close"><?php print caNavIcon(__CA_NAV_ICON_CLOSE__, "18px", [], ['color' => 'white']); ?></a></div>
+	<div class='close'><a href="#" onclick="caMediaPanel.hidePanel(); return false;" title="close"><?php print caNavIcon(__CA_NAV_ICON_CLOSE__, "18px", [], ['color' => 'white']).' '._t('Close annotations'); ?></a></div>
 </div>
 	
 <div class="caAnnoEditorTlContainer">
