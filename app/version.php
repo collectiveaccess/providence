aac6a74cb345deee490e5b732a86c13db6311fb2
Orestes Sanchez <orestes@estotienearreglo.es>
2020-11-29 16:45:16 +0100

    Update schema version.

--- app/version.php
+++ app/version.php
@@ -3,7 +3,7 @@
 	define('__CollectiveAccess__', '1.7.9');
 
 	# Schema revision
-	define('__CollectiveAccess_Schema_Rev__', 165);
+	define('__CollectiveAccess_Schema_Rev__', 170);
 
 	# Release type
 	define('__CollectiveAccess_Release_Type__', 'GIT');
