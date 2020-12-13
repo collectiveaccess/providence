9a8b40d2ac52862504e3151496555dd926367a17
Orestes Sanchez <orestes@estotienearreglo.es>
2020-12-13 19:27:07 +0100

    Add locale option to updateIndexingInPlace.

--- app/lib/Plugins/SearchEngine/SqlSearch.php
+++ app/lib/Plugins/SearchEngine/SqlSearch.php
@@ -2207,10 +2207,13 @@ class WLPlugSearchEngineSqlSearch extends BaseSearchPlugin implements IWLPlugSea
 	 *		literalContent = array of text content to be applied without tokenization
 	 *		BOOST = Indexing boost to apply
 	 *		PRIVATE = Set indexing to private
+	 *		locale_id = Locale to use for indexing
 	 */
 	public function updateIndexingInPlace($pn_subject_tablenum, $pa_subject_row_ids, $pn_content_tablenum, $ps_content_fieldnum, $pn_content_container_id, $pn_content_row_id, $ps_content, $pa_options=null) {
 		if(!is_array($pa_options)) { $pa_options = []; }
-		// Find existing indexing for this subject and content 	
+        $vn_locale_id = caGetOption('locale_id', $pa_options, null);
+
+        // Find existing indexing for this subject and content
 		foreach($pa_subject_row_ids as $vn_subject_row_id) {
 			$this->removeRowIndexing($pn_subject_tablenum, $vn_subject_row_id, $pn_content_tablenum, array($ps_content_fieldnum), $pn_content_row_id, caGetOption('relationship_type_id', $pa_options, null));
 		}
@@ -2266,16 +2269,16 @@ class WLPlugSearchEngineSqlSearch extends BaseSearchPlugin implements IWLPlugSea
 			}
 			$vn_seq = 0;
 			foreach($va_words as $vs_word) {
-			    // TODO: Use locale
-				if (!($vn_word_id = $this->getWordID($vs_word))) { continue; }
+                // Get locale and use it to get the word id
+				if (!($vn_word_id = $this->getWordID($vs_word, $vn_locale_id))) { continue; }
 				$va_row_insert_sql[] = "({$pn_subject_tablenum}, {$vn_row_id}, {$pn_content_tablenum}, '{$ps_content_fieldnum}', ".($pn_content_container_id ? $pn_content_container_id : 'NULL').", {$pn_content_row_id}, {$vn_word_id}, {$vn_boost}, {$vn_private}, {$vn_rel_type_id})";
 				$vn_seq++;
 			}
 
 			if (is_array($va_literal_content)) {
 				foreach($va_literal_content as $vs_literal) {
-			        // TODO: Use locale
-					if (!($vn_word_id = $this->getWordID($vs_literal))) { continue; }
+                    // Get locale and use it to get the word id
+					if (!($vn_word_id = $this->getWordID($vs_literal, $vn_locale_id))) { continue; }
 					$va_row_insert_sql[] = "({$pn_subject_tablenum}, {$vn_row_id}, {$pn_content_tablenum}, '{$ps_content_fieldnum}', ".($pn_content_container_id ? $pn_content_container_id : 'NULL').", {$pn_content_row_id}, {$vn_word_id}, {$vn_boost}, {$vn_private}, {$vn_rel_type_id})";
 					$vn_seq++;
 				}
