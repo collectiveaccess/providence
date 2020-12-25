f7a5384a49adb15f3f6422a74e869ee316b07513
Orestes Sanchez <orestes@estotienearreglo.es>
2020-12-25 20:49:55 +0100

    Update schema version.

--- install/inc/schema_mysql.sql
+++ install/inc/schema_mysql.sql
@@ -7357,4 +7357,4 @@ create table ca_schema_updates (
 ) engine=innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 /* Indicate up to what migration this schema definition covers */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (166, unix_timestamp());
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (167, unix_timestamp());
