5f1a432dff8e2989452c8c4a3fa6e36b0e03ca64
CollectiveAccess <seth@collectiveaccess.org>
2021-02-09 14:27:08 -0500

    PROV-3079 Force file extensions to lowercase to ensure they are serve-able by URL

--- app/lib/BaseModel.php
+++ app/lib/BaseModel.php
@@ -5518,7 +5518,7 @@ if (!isset($pa_options['dontSetHierarchicalIndexing']) || !$pa_options['dontSetH
 				$magic = rand(0,99999);
 
 				$va_pieces = explode("/", $this->_SET_FILES[$field]['original_filename']);
-				$ext = array_pop($va_tmp = explode(".", array_pop($va_pieces)));
+				$ext = strtolower(array_pop($va_tmp = explode(".", array_pop($va_pieces))));
 				if ($properties["dangerous"]) { $ext .= ".bin"; }
 				if (!$ext) $ext = "bin";
 
