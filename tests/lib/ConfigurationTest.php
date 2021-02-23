20a74e517052a20a886f671b0fcace4d432548d7
CollectiveAccess <seth@collectiveaccess.org>
2021-02-23 12:50:03 -0500

    PRO-3087 Make handling of backslashes in scalars consistent with other entry types

--- tests/lib/ConfigurationTest.php
+++ tests/lib/ConfigurationTest.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2009-2020 Whirl-i-Gig
+ * Copyright 2009-2021 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -46,6 +46,8 @@ class ConfigurationTest extends TestCase {
 		$this->assertEquals('This scalar is embedded: "/usr/local/fish"', $o_config->get('a_scalar_using_an_embedded_macro'));
 		$this->assertEquals('Expreß zug: חי תהער', $o_config->get('a_scalar_with_utf_8_chars'));
 		$this->assertEquals( "Foo\nHello\nWorld\n", $o_config->get('a_scalar_with_line_breaks'));
+		$this->assertEquals( 'This is a " test of escaped @ characters', $o_config->get('a_scalar_with_escaped_characters'));
+		$this->assertEquals( 'This is a \" test of escaped \@ characters', $o_config->get('a_scalar_with_escaped_backslashes'));
 	}
 
 	public function testLists() {
@@ -136,7 +138,7 @@ class ConfigurationTest extends TestCase {
 
 		$va_keys = $o_config->getScalarKeys();
 		$this->assertTrue(is_array($va_keys));
-		$this->assertEquals(14, sizeof($va_keys));		// 13 in config file + 1 "LOCALE" value that's automatically inserted
+		$this->assertEquals(16, sizeof($va_keys));		// 15 in config file + 1 "LOCALE" value that's automatically inserted
 		$va_keys = $o_config->getListKeys();
 		$this->assertTrue(is_array($va_keys));
 		$this->assertEquals(6, sizeof($va_keys));
