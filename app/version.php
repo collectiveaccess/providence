f571cb3437d4e9136a20d0994577d9d47a3358f9
CollectiveAccess <seth@collectiveaccess.org>
2021-02-25 09:06:13 -0500

    NOJIRA remove font tracker; update font loading

--- app/version.php
+++ app/version.php
@@ -1,6 +1,6 @@
 <?php
  	# CollectiveAccess version number; don't change
-	define('__CollectiveAccess__', '1.7.10');
+	define('__CollectiveAccess__', '1.7.11');
 
 	# Schema revision
 	define('__CollectiveAccess_Schema_Rev__', 158);
