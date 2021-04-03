fe030a8e41b42bcb924d75dd4c1a012588bfa7f2
CollectiveAccess <seth@collectiveaccess.org>
2021-04-03 10:28:27 -0400

    PROV-2788 Escape sort field (which may be "rank")

--- app/lib/BaseModel.php
+++ app/lib/BaseModel.php
@@ -11838,13 +11838,13 @@ $pa_options["display_form_field_tips"] = true;
 				switch($va_tmp[0]) {
 					case $vs_table:
 						if ($t_instance->hasField($va_tmp[1])) {
-							$vs_orderby = " ORDER BY {$vs_sort} {$vs_sort_direction}";
+							$vs_orderby = " ORDER BY `{$vs_sort}` {$vs_sort_direction}";
 						}
 						break;
 					default:
 						if (sizeof($va_tmp) == 1) {
 							if ($t_instance->hasField($va_tmp[0])) {
-								$vs_orderby = " ORDER BY {$vs_sort} {$vs_sort_direction}";
+								$vs_orderby = " ORDER BY `{$vs_sort}` {$vs_sort_direction}";
 							}
 						}
 						break;
