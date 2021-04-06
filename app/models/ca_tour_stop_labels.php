5a921a5adf6a891036b8fae3ba6300b76558179c
CollectiveAccess <seth@collectiveaccess.org>
2021-04-06 09:27:51 -0400

    PROV-3109 Extend label length

--- app/models/ca_tour_stop_labels.php
+++ app/models/ca_tour_stop_labels.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011-2012 Whirl-i-Gig
+ * Copyright 2011-2021 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -68,7 +68,7 @@ BaseModel::$s_ca_models_definitions['ca_tour_stop_labels'] = array(
 				'IS_NULL' => false, 
 				'DEFAULT' => '',
 				'LABEL' => _t('Name'), 'DESCRIPTION' => _t('Name of tour'),
-				'BOUNDS_LENGTH' => array(1,255)
+				'BOUNDS_LENGTH' => array(1,16384)
 		),
 		'name_sort' => array(
 				'FIELD_TYPE' => FT_TEXT, 'DISPLAY_TYPE' => DT_OMIT, 
@@ -199,4 +199,3 @@ class ca_tour_stop_labels extends BaseLabel {
 	}
 	# ------------------------------------------------------
 }
-?>
---
+++ support/sql/migrations/169.sql
@@ -0,0 +1,44 @@
+/*
+	Date: 6 April 2021
+	Migration: 169
+	Description:    Extend maximum length of primary type label fields
+*/
+
+/*==========================================================================*/
+
+ALTER TABLE ca_occurrence_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_occurrence_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+DROP INDEX u_all on ca_collection_labels;
+ALTER TABLE ca_collection_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_collection_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+create unique index u_all on ca_collection_labels
+(
+   collection_id,
+   name(255),
+   type_id,
+   locale_id
+);
+
+ALTER TABLE ca_object_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_object_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+ALTER TABLE ca_loan_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_loan_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+ALTER TABLE ca_movement_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_movement_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+ALTER TABLE ca_object_representation_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_object_representation_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+ALTER TABLE ca_tour_stop_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_tour_stop_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+ALTER TABLE ca_object_lot_labels MODIFY COLUMN name varchar(16384) NOT NULL;
+ALTER TABLE ca_object_lot_labels MODIFY COLUMN name_sort varchar(255) NOT NULL;
+
+/*==========================================================================*/
+
+/* Always add the update to ca_schema_updates at the end of the file */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (169, unix_timestamp());
