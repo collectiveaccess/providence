dbd1fea1dbf11e9afdecab96f42dc2ae17580c41
Seth <seth@whirl-i-gig.com>
2021-05-04 10:11:59 -0400

    NOJIRA update branding

--- themes/default/views/system/configuration_error_html.php
+++ themes/default/views/system/configuration_error_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011-2020 Whirl-i-Gig
+ * Copyright 2011-2021 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -44,7 +44,7 @@ if (!is_array($opa_error_messages)) {
 </head>
 <body>
 	<div id='box'>
-	<div id="logo"><img src="<?php print __CA_THEME_URL__ ?>/graphics/logos/ca_logo.png"/></div><!-- end logo -->
+	<div id="logo"><?= caGetLoginLogo(); ?></div><!-- end logo -->
 	<div id="content">
 		<?php print "<div class='error'>Issues with your system configuration have been detected</div>
 			General installation instructions can be found
