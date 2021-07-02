293a2a3d62f038113b3b886c7c2e1300303967a2
CollectiveAccess <seth@collectiveaccess.org>
2021-07-02 09:38:14 -0400

    Use original as default

--- app/lib/BaseFindController.php
+++ app/lib/BaseFindController.php
@@ -881,7 +881,7 @@
 						while($qr_res->nextHit()) {
 							if(!$element_code) {
 								// representation
-								$version = (!is_array($version_list = $qr_res->getMediaVersions('ca_object_representations.media')) || !in_array($preferred_version, $version_list)) ? $preferred_version : 'original';
+								$version = (!is_array($version_list = $qr_res->getMediaVersions('ca_object_representations.media')) || !in_array($preferred_version, $version_list)) ? 'original' : $preferred_version;
 							
 								$paths = $qr_res->getMediaPaths('ca_object_representations.media', $version);
 								$infos = $qr_res->getMediaInfos('ca_object_representations.media');
