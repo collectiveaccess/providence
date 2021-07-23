b969cd7cefc37e00c4597be816572258b876ee7e
Seth <seth@collectiveaccess.org>
2021-07-22 22:58:33 -0400

    Fix formatting on summaries

--- themes/default/views/editor/tours/summary_html.php
+++ themes/default/views/editor/tours/summary_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011-2018 Whirl-i-Gig
+ * Copyright 2011-2021 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -26,37 +26,34 @@
  * ----------------------------------------------------------------------
  */
  	$t_item 				= $this->getVar('t_subject');
-	$vn_item_id 			= $this->getVar('subject_id');
+	$item_id 			= $this->getVar('subject_id');
 	
 	$t_display 				= $this->getVar('t_display');
-	$va_placements 			= $this->getVar("placements");
+	$placements 			= $this->getVar("placements");
 ?>
-	<div id="summary" style="clear: both;">
-<?php
-    print caEditorPrintSummaryControls($this);
-?>
+<div id="summary" style="clear: both;">
+	<?= caEditorPrintSummaryControls($this); ?>
 	<div id="title">
-		<?php print $t_item->getLabelForDisplay(); ?>
+		<?= $t_item->getLabelForDisplay(); ?>
 	</div><!-- end title -->
 	<table border="0" cellpadding="0" cellspacing="0" width="100%">
 		<tr>
 			<td valign="top" align="left" style="padding-right:10px;">
 <?php
-		foreach($va_placements as $vn_placement_id => $va_info) {
-			$vs_class = "";
-			if (!strlen($vs_display_value = $t_display->getDisplayValue($t_item, $vn_placement_id, array_merge(array('request' => $this->request), is_array($va_info['settings']) ? $va_info['settings'] : array())))) {
+		foreach($placements as $placement_id => $info) {
+			$class = "";
+			if (!strlen($display_value = $t_display->getDisplayValue($t_item, ($placement_id > 0) ? $placement_id : $info['bundle_name'], array_merge(['request' => $this->request], is_array($info['settings']) ? $info['settings'] : [])))) {
 				if (!(bool)$t_display->getSetting('show_empty_values')) { continue; }
-				$vs_display_value = "&lt;"._t('not defined')."&gt;";
-				$vs_class = " notDefined";
+				$display_value = "&lt;"._t('not defined')."&gt;";
+				$class = " notDefined";
 			}
-			print "<div class=\"unit".$vs_class."\"><span class=\"heading".$vs_class."\">".$va_info['display'].":</span> ".$vs_display_value."</div>\n";
+			print "<div class=\"unit{$class}\"><span class=\"heading{$class}\">{$info['display']}</span><span class='summaryData'> {$display_value}</span></div>\n";
 		}
 ?>
-			</td>
 			</td>
 		</tr>
 	</table>
 </div><!-- end summary -->
 <?php
-		TooltipManager::add('#printButton', _t("Download Summary as PDF"));
-		TooltipManager::add('a.downloadMediaContainer', _t("Download Media"));
+TooltipManager::add('#printButton', _t("Download Summary as PDF"));
+TooltipManager::add('a.downloadMediaContainer', _t("Download Media"));
