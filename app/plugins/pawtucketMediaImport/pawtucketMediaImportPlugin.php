7466c4ee3b05700d8911c897e2d741af2af7238a
Seth <seth@whirl-i-gig.com>
2021-09-05 12:46:19 -0400

    Working, but still kinda sketchy, media import processor

---
+++ app/plugins/pawtucketMediaImport/pawtucketMediaImportPlugin.php
@@ -0,0 +1,61 @@
+<?php
+/* ----------------------------------------------------------------------
+ * pawtucketMediaImportPlugin.php : 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2021 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+  
+	class pawtucketMediaImportPlugin extends BaseApplicationPlugin {
+		# -------------------------------------------------------
+		private $opo_config;
+		# -------------------------------------------------------
+		public function __construct($ps_plugin_path) {
+			$this->description = _t('Pawtucket Media Import Processor');
+			$this->opo_config = Configuration::load($ps_plugin_path . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'plugin.conf');
+
+			parent::__construct();
+		}
+		# -------------------------------------------------------
+		/**
+		 * Override checkStatus() to return true - the historyMenu plugin always initializes ok
+		 */
+		public function checkStatus() {
+			return array(
+				'description' => $this->getDescription(),
+				'errors' => [],
+				'warnings' => [],
+				'available' => true
+			);
+		}
+		# -------------------------------------------------------
+		/**
+		 * Insert activity menu
+		 */
+		public function hookPeriodicTask() {
+			$ret = ca_media_upload_sessions::processSessions(['limit' => 20]);
+			
+			print "Processed {$ret} sessions\n";
+		}
+		# -------------------------------------------------------
+	}
