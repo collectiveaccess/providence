06af3defbda3ff2811d5c62dfbcc65bd101deb32
Seth <seth@collectiveaccess.org>
2021-10-03 23:00:41 -0400

    Fix typo

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
 		$g_response = $resp = $app->getResponse();
 
 		// Prevent caching
-		$resp->addHeader("Cache-Control", "no-cache, , no-store, must-revalidate");
+		$resp->addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 		$resp->addHeader("Expires", "Mon, 26 Jul 1997 05:00:00 GMT");
 		
 		
