1b0982404ff9caad053c5756d6cd678e618fbe51
CollectiveAccess <seth@collectiveaccess.org>
2021-12-21 14:41:24 -0500

    Revert filtering to default to falseM

--- app/lib/Parsers/DisplayTemplateParser.php
+++ app/lib/Parsers/DisplayTemplateParser.php
@@ -200,7 +200,7 @@ class DisplayTemplateParser {
 
 		if(!$qr_res) { return $pb_return_as_array ? array() : ""; }
 
-        $filter_non_primary_reps = self::_setPrimaryRepresentationFiltering($qr_res, caGetOption('filterNonPrimaryRepresentations', $pa_options, true));
+        $filter_non_primary_reps = self::_setPrimaryRepresentationFiltering($qr_res, caGetOption('filterNonPrimaryRepresentations', $pa_options, false));
         
 		$pa_check_access = ($t_instance->hasField('access')) ? caGetOption('checkAccess', $pa_options, null) : null;
 		if (!is_array($pa_check_access) || !sizeof($pa_check_access)) { $pa_check_access = null; }
