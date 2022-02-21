e60d40c5d4e11ae1baecf1c4a5ad56934cd40f7c
CollectiveAccess <seth@collectiveaccess.org>
2022-02-21 00:10:50 -0500

    Pass current language to Timeline.js. Resolves #1322

--- assets/timelinejs/js/timeline-embed.js
+++ assets/timelinejs/js/timeline-embed.js
@@ -547,7 +547,7 @@ function createStoryJS(c, src) {
       width:    '100%',
       height:   '100%',
       source:   'https://docs.google.com/spreadsheet/pub?key=0Agl_Dv6iEbDadFYzRjJPUGktY0NkWXFUWkVIZDNGRHc&output=html',
-      lang:   'en',
+      lang:   c.language ? c.language : 'en',
       font:   'default',
       start_at_end: false,
       timenav_position: 'bottom',
