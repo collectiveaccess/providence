c6af1b2e71247a80430030360f494b5b05c84b22
CollectiveAccess <seth@collectiveaccess.org>
2022-03-13 23:35:46 -0400

    Add guid lookup commands

--- app/lib/Utils/CLIUtils/Developer.php
+++ app/lib/Utils/CLIUtils/Developer.php
@@ -32,7 +32,7 @@
 trait CLIUtilsDeveloper{ 
 	# -------------------------------------------------------
 	/**
-	 *
+	 * Dump change log for a record
 	 */
 	public static function dump_log($opts=null) {	
 		$o_db = new Db();
@@ -103,6 +103,97 @@ trait CLIUtilsDeveloper{
 	public static function dump_logHelp() {
 		return _t('Dumps change log data for a row (specified by row ID) in a table.');
 	}
-	
+	# -------------------------------------------------------
+	/**
+	 * Get GUID for a record
+	 */
+	public static function get_guid($opts=null) {	
+		$table = $opts->getOption('table');
+		$id = $opts->getOption('id');
+		
+		if(!($t_instance = Datamodel::getInstanceByTableName($table))) {
+			CLIUtils::addError(_t('A table must be specified'));
+			return false;
+		}
+		if((int)$id <= 0) {
+			CLIUtils::addError(_t('A valid row id must be specified'));
+			return false;
+		}
+		
+		if($guid = ca_guids::getForRow($t_instance->tableNum(), $id, ['dontAdd' => true])) {
+			CLIUtils::addMessage(_t('GUID: %1', $guid));
+		} else {
+			CLIUtils::addError(_t('Record does not exist'));
+		}
+	}
+	# -------------------------------------------------------
+	public static function get_guidParamList() {
+		return [
+			"table|t-s" => _t('Table to return GUID for.'),
+			"id|i-s" => _t('ID of row to return GUID for.'),
+		];
+	}
+	# -------------------------------------------------------
+	/**
+	 *
+	 */
+	public static function get_guidUtilityClass() {
+		return _t('Developer');
+	}
+	# -------------------------------------------------------
+	/**
+	 *
+	 */
+	public static function get_guidShortHelp() {
+		return _t('Get GUID for a record.');
+	}
+	# -------------------------------------------------------
+	/**
+	 *
+	 */
+	public static function get_guidHelp() {
+		return _t('Returns GUID for a record specified by table and row ID.');
+	}
+	# -------------------------------------------------------
+	/**
+	 * Get table/row id pair for a GUID
+	 */
+	public static function get_info_for_guid($opts=null) {	
+		$guid = $opts->getOption('guid');
+		
+		if($info = ca_guids::find(['guid' => $guid], ['returnAs' => 'arrays'])) {
+			$info = array_shift($info);
+			CLIUtils::addMessage(_t("[%1] => %2:%3", $guid, Datamodel::getTableName($info['table_num']), $info['row_id']));
+		} else {
+			CLIUtils::addError(_t('GUID does not exist'));
+		}
+	}
+	# -------------------------------------------------------
+	public static function get_info_for_guidParamList() {
+		return [
+			"guid|g=s" => _t('GUID to resolve.'),
+		];
+	}
+	# -------------------------------------------------------
+	/**
+	 *
+	 */
+	public static function get_info_for_guidUtilityClass() {
+		return _t('Developer');
+	}
+	# -------------------------------------------------------
+	/**
+	 *
+	 */
+	public static function get_info_for_guidShortHelp() {
+		return _t('Return information for a GUID.');
+	}
+	# -------------------------------------------------------
+	/**
+	 *
+	 */
+	public static function guid_info_for_guidHelp() {
+		return _t('Returns table and row ID for a GUID.');
+	}
 	# -------------------------------------------------------
 }
