a4c89c5a75ad824f190912acefa4e9d0cb9f824a
CollectiveAccess <seth@collectiveaccess.org>
2022-04-26 18:35:52 -0400

    Filter for xss

--- app/controllers/lookup/DisplayTemplateController.php
+++ app/controllers/lookup/DisplayTemplateController.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011-2015 Whirl-i-Gig
+ * Copyright 2011-2022 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -30,7 +30,7 @@ require_once(__CA_LIB_DIR__ . '/Controller/ActionController.php');
 class DisplayTemplateController extends ActionController {
 	# -------------------------------------------------------
 	public function Get() {
-		$ps_template = $this->getRequest()->getParameter('template', pString);
+		$ps_template = $this->getRequest()->getParameter('template', pString, 'GET', ['purify' => true, 'urldecode' => false]);
 		$ps_table = $this->getRequest()->getParameter('table', pString);
 		$pn_id = $this->getRequest()->getParameter('id', pString);
 
@@ -42,7 +42,7 @@ class DisplayTemplateController extends ActionController {
 		if(!($t_instance->load($pn_id))) {
 			return false;
 		}
-
+		
 		print @$t_instance->getWithTemplate($ps_template);
 	}
 	# -------------------------------------------------------
