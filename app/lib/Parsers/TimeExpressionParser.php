0c580fc8c8d923dbaf943f0d7aac8f6d2df139be
CollectiveAccess <seth@collectiveaccess.org>
2022-10-26 11:35:30 -0400

    Initialize before date time components if empty

--- app/lib/Parsers/TimeExpressionParser.php
+++ app/lib/Parsers/TimeExpressionParser.php
@@ -622,6 +622,10 @@ class TimeExpressionParser {
 						'hours' => null, 'minutes' => null, 'seconds' => null,
 						'uncertainty' => false, 'uncertainty_units' => '', 'is_circa' => false, 'is_probably' => false
 					);
+					if(!isset($va_date['hours'])) { $va_date['hours'] = 0; }
+					if(!isset($va_date['minutes'])) { $va_date['minutes'] = 0; }
+					if(!isset($va_date['seconds'])) { $va_date['seconds'] = 0; }
+					
 					$va_dates['end'] = $va_date;
 					$this->skipToken();
 					$vn_state = TEP_STATE_ACCEPT;
