5ddb8705f4ffb66ebe3875e8396a2bdc756819bc
CollectiveAccess <seth@collectiveaccess.org>
2022-11-28 12:53:58 -0500

    Highlighting

--- app/lib/MetaTagManager.php
+++ app/lib/MetaTagManager.php
@@ -152,23 +152,25 @@ class MetaTagManager {
 	/**
 	 * Set text highlight
 	 *
-	 * @param string $ps_title The window title
+	 * @param array $highlight_text List of strings to highlight
 	 * @return bool Always returns true
 	 */
-	static function setHighlightText(?string $highlight_text) : bool {
-		if(!is_null($highlight_text) && strlen($highlight_text)) {
-			Session::setVar('text_highlight', $highlight_text);
-		}
+	static function setHighlightText(?array $highlight_text) : bool {
+		global $g_highlight_text;
+		$g_highlight_text = $highlight_text;
+		Session::setVar('text_highlight', $highlight_text);
 		return true;
 	}
 	# --------------------------------------------------------------------------------
 	/**
-	 * Get current text highlight
+	 * Get list of text to highlight
 	 *
-	 * @return string
+	 * @return array
 	 */
-	static function getHighlightText() : ?string {
-		return Session::getVar('text_highlight');
+	static function getHighlightText() : ?array {
+		global $g_highlight_text;
+		if(!is_null($g_highlight_text)) { return $g_highlight_text; }
+		return $g_highlight_text = Session::getVar('text_highlight');
 	}
 	# --------------------------------------------------------------------------------
 }
