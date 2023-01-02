e924740804bbdb5e2687e1d97e4bbfee1c2f2e5b
CollectiveAccess <seth@collectiveaccess.org>
2023-01-02 11:06:35 -0500

    Honor raw settings in formatWithTemplate

--- app/models/ca_data_importers.php
+++ app/models/ca_data_importers.php
@@ -2493,18 +2493,18 @@ class ca_data_importers extends BundlableLabelableBaseModelWithAttributes {
 									$va_item['settings']['applyRegularExpressions'], $vm_val, $va_row,
 									$va_row_with_replacements );
 							}
-
-							if ( $use_raw && isset( $va_item['settings']['formatWithTemplate'] )
+							
+							if (isset( $va_item['settings']['formatWithTemplate'] )
 							     && strlen( $va_item['settings']['formatWithTemplate'] )
 							) {
 								$vm_val
 									= DisplayTemplateParser::processTemplate( $va_item['settings']['formatWithTemplate'],
-									array_replace( $va_row_with_replacements, array(
+									array_replace( $use_raw ? $va_raw_row : $va_row_with_replacements , array(
 										(string) $va_item['source'] => ca_data_importers::replaceValue( $vm_val,
 											$va_item, [ 'log' => $o_log, 'logReference' => $vs_idno ] )
 									) ), array( 'getFrom' => $o_reader ) );
 							}
-
+							
 							if ( isset( $va_item['settings']['skipIfExpression'] )
 							     && strlen( trim( $va_item['settings']['skipIfExpression'] ) )
 							) {
