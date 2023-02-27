6be4228cab6721fcc33f6458530a66fd9e80f409
CollectiveAccess <seth@collectiveaccess.org>
2023-02-27 15:35:55 -0500

    Add missing semicolon

--- support/sql/migrations/182.sql
+++ support/sql/migrations/182.sql
@@ -9,7 +9,7 @@
 ALTER TABLE ca_site_pages ADD COLUMN locale_id smallint unsigned null;
 CREATE INDEX i_locale_id ON ca_site_pages(locale_id);
 ALTER TABLE ca_site_pages ADD constraint fk_ca_site_pages_locale_id foreign key (locale_id)
-      references ca_locales (locale_id) on delete restrict on update restrict
+      references ca_locales (locale_id) on delete restrict on update restrict;
 
 /*==========================================================================*/
 
