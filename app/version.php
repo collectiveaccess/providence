a57eaaf2abb91939f52ba8db5e4158cc02a95f6f
CollectiveAccess <seth@collectiveaccess.org>
2023-04-02 16:11:58 -0400

    Bump version to 2.0

--- app/version.php
+++ app/version.php
@@ -1,6 +1,6 @@
 <?php
  	# CollectiveAccess version number; don't change
-	define('__CollectiveAccess__', '1.8');
+	define('__CollectiveAccess__', '2.0');
 
 	# Schema revision
 	define('__CollectiveAccess_Schema_Rev__', 182);
