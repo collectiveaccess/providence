67578a22bae2487248a03c14e780c43a0e0bcd3c
Bri <53800058+bri-gaia@users.noreply.github.com>
2023-06-24 19:44:04 +1000

    Update themes/default/views/editor/objects/summary_html_ajax.php
    Co-authored-by: Kehan Harman <kehh@users.noreply.github.com>

--- themes/default/views/editor/objects/summary_html_ajax.php
+++ themes/default/views/editor/objects/summary_html_ajax.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2010-2021 Whirl-i-Gig
+ * Copyright 2010-2023 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
