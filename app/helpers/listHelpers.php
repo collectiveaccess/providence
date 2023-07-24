ba6ccc0805a0161a3c299863319ea772aeae12c0
Seth <seth@collectiveaccess.org>
2023-07-24 09:25:56 -0400

    Add missing helper functions

--- app/helpers/listHelpers.php
+++ app/helpers/listHelpers.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011-2020 Whirl-i-Gig
+ * Copyright 2011-2023 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -285,7 +285,8 @@ require_once(__CA_MODELS_DIR__.'/ca_list_items.php');
 	 * @return string|null
 	 */
 	function caGetListItemIDForValue($ps_list_code, $ps_value, $pa_options=null) {
-		return array_shift(array_keys(caGetListItemForValue($ps_list_code, $ps_value, $pa_options)));
+		$v = caGetListItemForValue($ps_list_code, $ps_value, $pa_options);
+		return is_array($v) ? array_shift(array_keys($v)) : null;
 	}
 	# ---------------------------------------
 	/**
@@ -390,6 +391,43 @@ require_once(__CA_MODELS_DIR__.'/ca_list_items.php');
 	}
 	# ---------------------------------------
 	/**
+	 * Check if item with label or idno exists in list. 
+	 *
+	 * @param string $list_code List code
+	 * @param string $value A label or idno value
+	 * @param array $pa_options Options include:
+	 *		transaction = transaction to execute queries within. [Default=null]
+	 *      noCache = Don't use cache. [Default is false]
+	 *      dontCache = Synonym for noCache
+	 *      checkAccess = Array of access values to filter returned values on. If omitted no filtering is performed. [Default is null]
+	 *		return = Type of return value. Valid types aare "bool", "id" and "modelInstance" [Default is book]
+     *
+	 * @return mixed bool, int or model instance depending upon "return" option
+	 */
+	function caItemExists(string $list_code, string $value, $options=null) {
+		$t_list = new ca_lists();
+		if ($o_trans = caGetOption('transaction', $pa_options, null)) { $t_list->setTransaction($o_trans); }
+		
+		if(!($item_id = $t_list->getItemIDFromListByLabel($list_code, $value, $options))) {
+			if(!($item_id = ca_list_items::find(['list_id' => $list_code, 'idno' => $value], ['returnAs' => 'firstId']))) {
+				$item_id = caGetListItemIDForValue($list_code, $value, $options);
+			}
+		}
+		switch(strtolower(caGetOption('return', $options, null))) {
+			case 'id':
+				return $item_id ? (int)$item_id : null;
+				break;
+			case 'modelinstance':
+				return $item_id ? ca_list_items::findAsInstance(['item_id' => $item_id]) : null;
+				break;
+			case 'bool':
+			default:
+				return (bool)$item_id;
+				break;
+		}
+	}
+	# ---------------------------------------
+	/**
 	 * Return simple array with items from a list. Array keys and values may be any list item field.
 	 *
 	 * @param string $ps_list_code List code
@@ -427,7 +465,7 @@ require_once(__CA_MODELS_DIR__.'/ca_list_items.php');
 			if ($default_only && !$item['is_default']) { continue; }
 			$list[isset($item[$key]) ? $item[$key] : $id] = isset($item[$value]) ? $item[$value] : $item['name_plural'];
 		}
-		ksort($list);
+		
 		return $g_list_items_cache[$vs_cache_key] = $list;
 	}
 	# ---------------------------------------
@@ -456,6 +494,64 @@ require_once(__CA_MODELS_DIR__.'/ca_list_items.php');
 		if ($o_trans = caGetOption('transaction', $pa_options, null)) { $t_list->setTransaction($o_trans); }
 		
 		return $g_default_list_item_id_cache[$vs_cache_key] = $t_list->getDefaultItemID($ps_list_code, array_merge($pa_options, ['useFirstElementAsDefaultDefault' => true]));
+	}
+	# ---------------------------------------
+	/**
+	 * Fetch value for default item in list
+	 *
+	 * @param string $ps_list_code List code
+	 * @param array $pa_options Options include:
+	 *		transaction = transaction to execute queries within. [Default=null]
+	 *      noCache = Don't use cache. [Default is false]
+	 *      dontCache = Synonym for noCache
+     *
+	 * @return string value of list item or null if no default item was found
+	 */
+	$g_default_list_item_value_cache = [];
+	function caGetDefaultItemValue($ps_list_code, $pa_options=null) {
+		global $g_default_list_item_value_cache;
+		$vs_cache_key = caMakeCacheKeyFromOptions($pa_options ?? [], $ps_list_code);
+		
+		if(!caGetOption(['noCache', 'dontCache'], $pa_options, false)) {
+		    if(isset($g_default_list_item_value_cache[$vs_cache_key])) { return $g_default_list_item_value_cache[$vs_cache_key]; }
+		}
+		
+		$default_item_id = caGetDefaultItemID($ps_list_code, $pa_options);
+		return $g_default_list_item_value_cache[$vs_cache_key] = caGetListItemValueForID($default_item_id, $pa_options);
+	}
+	# ---------------------------------------
+	/**
+	 * Fetch setting value for list item
+	 *
+	 * @param string $list_code List code
+	 * @param string $idno
+	 * @param string $setting
+	 * @param array $pa_options Options include:
+	 *		transaction = transaction to execute queries within. [Default=null]
+	 *      noCache = Don't use cache. [Default is false]
+	 *      dontCache = Synonym for noCache
+     *
+	 * @return mixed Value of setting, or null if item or setting do not exist.
+	 */
+	$g_default_list_item_setting_cache = [];
+	function caGetListItemSettingValue(string $list_code, string $idno, string $setting, ?array $options=null) {
+		global $g_default_list_item_setting_cache;
+		if(!is_array($options)) { $options = []; }
+		if(!is_array($g_default_list_item_setting_cache)) { $g_default_list_item_setting_cache = []; }
+		$cache_key = caMakeCacheKeyFromOptions($options, $list_code.'/'.$idno.'/'.$setting);
+		
+		if(!caGetOption(['noCache', 'dontCache'], $options, false)) {
+		    if(array_key_exists($cache_key, $g_default_list_item_setting_cache)) { return $g_default_list_item_setting_cache[$cache_key]; }
+		}
+		
+		if(!($item_id = caGetListItemID($list_code, $idno))) {
+			return $g_default_list_item_setting_cache[$cache_key] = null;
+		}
+		
+		$t_item = new ca_list_items();
+		if ($o_trans = caGetOption('transaction', $options, null)) { $t_item->setTransaction($o_trans); }
+		if(!$t_item->load($item_id)) { return null; }
+		return $g_default_list_item_setting_cache[$cache_key] = $t_item->getSetting($setting);
 	}
 	# ---------------------------------------
 	/**
