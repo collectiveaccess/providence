df7f034f897c4de035ae2a36c1e48c089948a846
Kehan Harman <kehan.harman@gaiaresources.com.au>
2023-08-21 12:16:57 +0800

    Add migration 186 to synchronize legacy and current data models
    This commit includes adding the SQL migration 186 and corresponding VersionUpdate186.php. The migration is crafted to resolve the differences between the legacy data model and the current data model. Some changes include modifying the schema of various tables, adding/removing indexes, and making adjustments to foreign keys.

---
+++ support/sql/migrations/186.sql
@@ -0,0 +1,12 @@
+/*
+	Date: 11 August 2023
+	Migration: 186
+	Description: Resolve the differences between legacy data model and current data model
+*/
+
+/*==========================================================================*/
+
+/* Always add the update to ca_schema_updates at the end of the file */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime)
+ VALUES (186, unix_timestamp());
+
