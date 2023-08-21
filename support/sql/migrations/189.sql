3eb2da8b4f678ab790811da6daf1596279c95e21
CollectiveAccess <seth@collectiveaccess.org>
2023-08-21 09:26:09 -0400

    Correct migration

--- support/sql/migrations/189.sql
+++ support/sql/migrations/189.sql
@@ -1,27 +1,10 @@
 /*
-<<<<<<< HEAD
-	Date: 14 June 2023
-	Migration: 186
-	Description:  Add sortable set_code field
-*/
-/*==========================================================================*/
-
-ALTER TABLE ca_sets ADD COLUMN set_code_sort varchar(100) null;
-CREATE INDEX i_set_code_sort ON ca_sets(set_code_sort);
-=======
 	Date: 11 August 2023
-	Migration: 186
+	Migration: 189
 	Description: Resolve the differences between legacy data model and current data model
 */
->>>>>>> df7f034f8 (Add migration 186 to synchronize legacy and current data models)
 
 /*==========================================================================*/
 
 /* Always add the update to ca_schema_updates at the end of the file */
-<<<<<<< HEAD
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (186, unix_timestamp());
-=======
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime)
- VALUES (186, unix_timestamp());
-
->>>>>>> df7f034f8 (Add migration 186 to synchronize legacy and current data models)
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (189, unix_timestamp());
