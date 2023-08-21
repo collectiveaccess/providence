6fc11859fa995b561c14c7054d91f9e7e1dddc0e
CollectiveAccess <seth@collectiveaccess.org>
2023-08-21 09:59:03 -0400

    Correct schema migration number

--- support/sql/migrations/189.sql
+++ support/sql/migrations/189.sql
@@ -1,12 +1,11 @@
 /*
 	Date: 11 August 2023
-	Migration: 186
+	Migration: 189
 	Description: Resolve the differences between legacy data model and current data model
 */
 
 /*==========================================================================*/
 
 /* Always add the update to ca_schema_updates at the end of the file */
-INSERT IGNORE INTO ca_schema_updates (version_num, datetime)
- VALUES (186, unix_timestamp());
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (189, unix_timestamp());
 
