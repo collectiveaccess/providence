aa3b00a961155617a32c714d6e7c045925848bb7
CollectiveAccess <seth@collectiveaccess.org>
2023-12-24 14:04:55 -0500

    Add rough version of themed IIIF manifests

--- app/lib/Service/IIIFService.php
+++ app/lib/Service/IIIFService.php
@@ -515,7 +515,7 @@ class IIIFService {
 	/**
 	 *
 	 */
-	private static function parseIdentifier(string $identifier) {
+	public static function parseIdentifier(string $identifier) {
 		$pa_identifier = explode(':', $identifier);
 		
 		if (sizeof($pa_identifier) > 1) {
@@ -532,7 +532,7 @@ class IIIFService {
 	/**
 	 *
 	 */
-	private static function getMediaInstance(string $identifier, RequestHTTP $request) {
+	public static function getMediaInstance(string $identifier, RequestHTTP $request) {
 		list($ps_type, $pn_id, $pn_page) = self::parseIdentifier($identifier);
 		
 		switch($ps_type) {
@@ -613,295 +613,20 @@ class IIIFService {
 	/**
 	 *
 	 */
-	public static function manifest($identifiers, RequestHTTP $request) : array {
-		$o_config = Configuration::load();
-		$base_url = $o_config->get('site_host').$o_config->get('ca_url_root'); //.$request->getBaseUrlPath();
-		
-		$manifest_url = '';
-		if(isset($_SERVER['REQUEST_URI'])) {
-			$manifest_url = $o_config->get('site_host').$_SERVER['REQUEST_URI'];
-		} else {
-			$manifest_url = $base_url.join(":", $identifiers)."/manifest";
-		}
-		
+	public static function manifest($identifiers, ?array $options=null) : array {
+		if(!$identifiers) { return null; }
 		if(!is_array($identifiers)) { $identifiers = [$identifiers]; }
-		$json = [
-			'@context' => 'http://iiif.io/api/presentation/3/context.json',
-			'id' => $manifest_url,
-			'type' => 'Manifest',
-			'label' => ['none' => []],
-			'metadata' => [],
-			//'requiredStatement' => ['label' => ['none' => ['TODO']]],
-			//'rights' => 'TODO',
-			//'thumbnail' => null,
-			//'seeAlso' => null,
-			//'homepage' => null,
-			//'partOf' => null,
-			'items' => []
-		];
-	
-		foreach($identifiers as $identifier) {
-			if(!is_array($media = self::getMediaInstance($identifier, $request))) {
-				throw new IIIFAccessException(_t('Unknown error'), 400);
-			}
-			
-			// $item = [
-// 				'id' => $base_url.$identifier,
-// 				'type' => 'Canvas',
-// 				'label' => ['none' => [$media['instance']->get('preferred_labels')]],
-// 				'width' => null,
-// 				'height' => null
-// 			];
-			$mwidth = $mheight = null;
-			
-			switch($media['type']) {
-				case 'representation':
-				
-					break;
-				case 'attribute':
-				
-					break;
-				default:
-					$reps = $media['instance']->getRepresentations(['original', 'thumbnail', 'preview170', 'medium', 'h264_hi', 'mp3'], null, ['includeAnnotations' => true]);
-					
-					$replist = [];
-					
-					foreach($reps as $rep) {
-						$w = $rep['info']['original']['WIDTH'];
-						$h = $rep['info']['original']['HEIGHT'];
-						
-						if(is_null($mwidth) || ($w > $mwidth)) { $mwidth = $w; }
-						if(is_null($mheight) || ($h > $mheight)) { $mheight = $h; }
-						
-						$page = 1; // @TODO: fix
-						
-						$service_url = "{$base_url}/service.php/IIIF/representation:{$rep['representation_id']}:{$page}";
-						
-						$thumb_width = $rep['info']['thumbnail']['WIDTH'];
-						$thumb_height = $rep['info']['thumbnail']['HEIGHT'];
-						$thumb_mimetype = $rep['info']['thumbnail']['MIMETYPE'];
-						
-						$base_iiif_id = $manifest_url.'-'.$rep['representation_id'];
-						
-						$rep_mimetype = $rep['info']['original']['MIMETYPE'];
-						
-						$rep_media_class = caGetMediaClass($rep_mimetype, ['forIIIF' => true]);
-						
-						$services = null;
-						$media_url = null;
-						$placeholder_url = $placeholder_width = $placeholder_height = $placeholder_mimetype = null;
-						$thumb_url = $thumb_width = $thumb_height = $thumb_mimetype = null;
-						$d = null;
-						
-						$annotations = [];
-						switch($rep_media_class) {
-							case 'Image':
-								$services = [
-									[
-										'id' => $service_url,
-										'type' => 'ImageService2',
-										'profile' => 'http://iiif.io/api/image/2/level2.json"'
-									]
-								];
-								$media_url = $service_url.'/full/max/0/default.jpg';
-								$placeholder_url = $rep['urls']['medium'];								
-								$placeholder_width = $rep['info']['medium']['WIDTH'];
-								$placeholder_height = $rep['info']['medium']['HEIGHT'];
-								$placeholder_mimetype = $rep['info']['medium']['MIMETYPE'];
-												
-								$thumb_url = $rep['urls']['thumbnail'];				
-								$thumb_width = $rep['info']['thumbnail']['WIDTH'];
-								$thumb_height = $rep['info']['thumbnail']['HEIGHT'];
-								$thumb_mimetype = $rep['info']['thumbnail']['MIMETYPE'];
-								break;
-							case 'Video':
-								if(!($media_url = ($rep['urls']['h264_hi'] ?? null))) {
-									$media_url = $rep['urls']['original'];
-								}
-								$d = $rep['info']['original']['PROPERTIES']['duration'];
-								
-								$placeholder_url = $rep['urls']['medium'];								
-								$placeholder_width = $rep['info']['medium']['WIDTH'];
-								$placeholder_height = $rep['info']['medium']['HEIGHT'];
-								$placeholder_mimetype = $rep['info']['medium']['MIMETYPE'];
-								
-								$thumb_url = $rep['urls']['thumbnail'];
-								$thumb_width = $rep['info']['thumbnail']['WIDTH'];
-								$thumb_height = $rep['info']['thumbnail']['HEIGHT'];
-								$thumb_mimetype = $rep['info']['thumbnail']['MIMETYPE'];
-								if(is_array($rep['captions']) && sizeof($rep['captions'])) {
-									foreach($rep['captions'] as $ci => $caption_info) {
-										$annotations[] =
-											[
-												'id' => $base_iiif_id.'-annotation-subtitles-'.$ci,
-												'type' => 'AnnotationPage',
-												'items' => [
-													[
-													  'id' => $base_iiif_id.'-annotation-subtitles-vtt-'.$ci,
-													  'type' => 'Annotation',
-													  'motivation' => 'supplementing',
-													  'body' => [
-														'id' => $caption_info['url'],
-														'type' => 'Text',
-														'format' => 'text/vtt',
-														'label' => [
-														  'en' => ['Subtitles']
-														],
-														'language' => substr($caption_info['locale_code'], 0, 2)
-													  ],
-													  'target' => $base_iiif_id
-													],
-												],
-											];
-									}
-								}
-								if($rep['num_annotations'] > 0) {
-									$annotations[] =
-											[
-												'id' => $base_iiif_id.'-annotation-clips-'.$ci,
-												'type' => 'AnnotationPage',
-												'items' => [
-													[
-													  'id' => $base_iiif_id.'-annotation-clips-json-'.$ci,
-													  'type' => 'Annotation',
-													  'motivation' => 'supplementing',
-													  'body' => [
-														'id' => preg_replace("!/IIIF/manifest/.*$!", "/IIIF/cliplist/", $manifest_url)."representation:".$rep['representation_id'],
-														'type' => 'Text',
-														'format' => 'application/json',
-														'label' => [
-														  'en' => ['Clips']
-														],
-														'language' => substr($caption_info['locale_code'], 0, 2)
-													  ],
-													  'target' => $base_iiif_id
-													],
-												],
-											];
-								}
-								break;
-							case 'Sound':
-								if(!($media_url = ($rep['urls']['mp3'] ?? null))) {
-									$media_url = $rep['urls']['original'];
-								}
-								$d = $rep['info']['original']['PROPERTIES']['duration'];
-								
-								$placeholder_url = $rep['urls']['medium'];								
-								$placeholder_width = $rep['info']['medium']['WIDTH'];
-								$placeholder_height = $rep['info']['medium']['HEIGHT'];
-								$placeholder_mimetype = $rep['info']['medium']['MIMETYPE'];
-								
-								$thumb_url = $rep['urls']['thumbnail'];
-								$thumb_width = $rep['info']['thumbnail']['WIDTH'];
-								$thumb_height = $rep['info']['thumbnail']['HEIGHT'];
-								$thumb_mimetype = $rep['info']['thumbnail']['MIMETYPE'];
-								break;
-							case 'Text':
-								if(!($media_url = $rep['urls']['compressed'] ?? null)) {
-									$media_url = $rep['urls']['original'];
-								}
-								
-								$placeholder_url = $rep['urls']['medium'];								
-								$placeholder_width = $rep['info']['medium']['WIDTH'];
-								$placeholder_height = $rep['info']['medium']['HEIGHT'];
-								$placeholder_mimetype = $rep['info']['medium']['MIMETYPE'];
-								
-								$thumb_url = $rep['urls']['thumbnail'];
-								$thumb_width = $rep['info']['thumbnail']['WIDTH'];
-								$thumb_height = $rep['info']['thumbnail']['HEIGHT'];
-								$thumb_mimetype = $rep['info']['thumbnail']['MIMETYPE'];
-								break;
-						}
-						
-						if($rep['label'] === '[BLANK]') { $rep['label'] = ''; }
-						$repinfo = [
-							'id' => $base_iiif_id,
-							'type' => 'Canvas',
-							'label' => ['none' => [$rep['label']]],
-							'width' => $w,
-							'height' => $h,
-							'duration' => $d,
-							'thumbnail' => [[
-								'id' => $thumb_url,
-								'type' => 'Image',
-								'format' => $thumb_mimetype,
-								'width' => $thumb_width,
-								'height' => $thumb_height
-							]],
-							'items' => [
-								[
-									'id' => $base_iiif_id.'-item-page',
-									'type' => 'AnnotationPage',
-									'items' => [
-										[
-											'id' => $base_iiif_id.'-annotation',
-											'type' => 'Annotation',
-											'motivation' => 'painting',
-											'target' => $base_iiif_id,
-											'body' => [
-												'id' => $media_url,
-												'type' => $rep_media_class,
-												'format' => $rep_mimetype,
-												'width' => $w,
-												'height' => $h,
-												'duration' => $d,
-												'service' => $services
-											],
-										]
-									],
-								]
-							],
-							'annotations' => $annotations,
-							'placeholderCanvas' => [
-								'id' => $base_iiif_id.'-placeholder',
-								'type' => 'Canvas',
-								'width' => $placeholder_width,
-								'height' => $placeholder_height,
-								'items' => [
-									[
-										'id' => $base_iiif_id.'-placeholder-annotation-page',
-										'type' => 'AnnotationPage',
-										'items' => [
-											[
-												'id' => $base_iiif_id.'-placeholder-annotation',
-												'type' => 'Annotation',
-												'motivation' => 'painting',
-												'body' => [
-												  'id' => $placeholder_url,
-												  'type' => 'Image',
-												  'format' => $placeholder_mimetype,
-												  'width' => $placeholder_width,
-												  'height' => $placeholder_height
-												],
-												'target' => $base_iiif_id.'-placeholder'
-											]
-										]
-									]
-								]
-							]
-						];
-						
-						if(!$services) {
-							unset($repinfo['items'][0]['items'][0]['body']['service']);
-						}
-						if(!$d) {
-							unset($repinfo['duration']);
-							unset($repinfo['items'][0]['items'][0]['body']['duration']);
-						}
-						
-						if(!$w) {
-							unset($repinfo['items'][0]['items'][0]['body']['width']);
-							unset($repinfo['items'][0]['items'][0]['body']['height']);
-						}
-						
-						$replist[] = $repinfo;
-					}
-					break;
-			}
-			
-			$json['items'] = $replist;
+		
+		$render = caGetOption('render', $options, 'MixedMedia');
+		if(!$render) { $render = 'MixedMedia'; }
+		$class = "\\CA\\Media\\IIIFManifests\\{$render}";
+		if(class_exists($class))  {
+			$manifest = new $class();
+		} else {
+			throw new IIIFAccessException(_t('Invalid render mode %1', $render), 400);	
 		}
-		return $json;
+	
+		return $manifest->manifest($identifiers);
 	}
 	# -------------------------------------------------------
 	/**
