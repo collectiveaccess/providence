5c812f2938100c5cececd9b2ee7a382da1c82426
CollectiveAccess <seth@collectiveaccess.org>
2024-02-07 14:08:47 -0500

    Disable version check to buy time while we upgrade to CKEditor 5

--- assets/ck/config.js
+++ assets/ck/config.js
@@ -29,6 +29,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = '';
+	
+	config.versionCheck = false;
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
