ce0d44a02941cd4c8004b414633f525a0bb222f3
CollectiveAccess <seth@collectiveaccess.org>
2024-09-15 23:42:35 -0400

    Revisions to exporter based upon WF findings of changes to previous behavior:
    1. Default to output of display text for list items rather than internal ids
    2. Fix broken support for templates in some contexts
    3. Default filtering of non-primary representations to false when pulling object data into a representation context; using true in this context causes object data to fail to be pulled (rightly), but this is almost never what one would want
    4. Update JSON schema for exporter item options to allow arrays in the "context" parameter
    5. Add deduplicate option for XML output; removed redundant tags sharing a parent; needed to deal with WF's pattern of pulling entity names from both related objects and occurrences (works), with oft-redundant cataloguing

--- app/lib/Export/ExportFormats/ExportXML.php
+++ app/lib/Export/ExportFormats/ExportXML.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013 Whirl-i-Gig
+ * Copyright 2013-2024 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -29,15 +29,18 @@
  *
  * ----------------------------------------------------------------------
  */
-
 require_once(__CA_LIB_DIR__.'/Export/BaseExportFormat.php');
-require_once(__CA_MODELS_DIR__.'/ca_data_exporters.php');
-require_once(__CA_MODELS_DIR__.'/ca_data_exporter_items.php');
 
 class ExportXML extends BaseExportFormat {
 	# ------------------------------------------------------
+	/**
+	 *
+	 */
 	private $opo_dom;
 	# ------------------------------------------------------
+	/**
+	 *
+	 */
 	public function __construct(){
 		$this->ops_name = 'XML';
 		$this->ops_element_description = _t('Values prefixed with @ reference XML attributes. All other values define XML elements. The usual restrictions and naming conventions for XML elements and attributes apply.');
@@ -49,137 +52,187 @@ class ExportXML extends BaseExportFormat {
 		parent::__construct();
 	}
 	# ------------------------------------------------------
-	public function getFileExtension($pa_settings) {
+	/**
+	 *
+	 */
+	public function getFileExtension($settings) {
 		return 'xml';
 	}
 	# ------------------------------------------------------
-	public function getContentType($pa_settings) {
+	/**
+	 *
+	 */
+	public function getContentType($settings) {
 		return 'text/xml';
 	}
 	# ------------------------------------------------------
-	public function processExport($pa_data,$pa_options=array()){
-		$pb_single_record = caGetOption('singleRecord', $pa_options);
-		//$pb_rdf_mode = caGetOption('rdfMode', $pa_options);
-		$pb_strip_cdata = (bool) caGetOption('stripCDATA', $pa_options['settings'], false);
+	/**
+	 *
+	 */
+	public function processExport($data,$options=array()){
+		$single_record = caGetOption('singleRecord', $options);
+		//$rdf_mode = caGetOption('rdfMode', $options);
+		
+		$settings = $options['settings'] ?? [];
+		
+		$strip_cdata = (bool)caGetOption('stripCDATA', $settings, false);
+		$dedupe = (bool)caGetOption('deduplicate', $settings, false);
 
-		//caDebug($pa_data,"Data to build XML from");
+		//caDebug($data,"Data to build XML from");
 		
 		$this->log("XML export formatter: Now processing export tree ...");
 
 		// XML exports should usually have only one top-level element (i.e. one root).
-		if(sizeof($pa_data)!=1){ return false; }
+		if(sizeof($data)!=1){ return false; }
 
-		$this->processItem(array_pop($pa_data),$this->opo_dom, ['stripCDATA' => $pb_strip_cdata]);
-
+		$this->processItem(array_pop($data),$this->opo_dom, ['stripCDATA' => $strip_cdata]);
+		
+		if($dedupe) {
+			$this->log("XML export formatter: Deduplicating export tree ...");
+			$this->opo_dom = $this->dedupe($this->opo_dom, $this->opo_dom);
+		}
+		
 		$this->log(_t("XML export formatter: Done processing export tree ..."));
 
 		// when dealing with a record set export, we don't want <?xml tags in front of each record
 		// that way we can simply dump a sequence of records in a file and have well-formed XML as result
-		$vs_return = ($pb_single_record ? $this->opo_dom->saveXML() : $this->opo_dom->saveXML($this->opo_dom->firstChild));
+		$return = ($single_record ? $this->opo_dom->saveXML() : $this->opo_dom->saveXML($this->opo_dom->firstChild));
 
-		if($pb_strip_cdata) {
-			$vs_return = str_replace('<![CDATA[', '', $vs_return);
-			$vs_return = str_replace(']]>','',$vs_return);
+		if($strip_cdata) {
+			$return = str_replace('<![CDATA[', '', $return);
+			$return = str_replace(']]>','',$return);
 		}
 
-		return $vs_return;
+		return $return;
 	}
 	# ------------------------------------------------------
-	private function processItem($pa_item, $po_parent, $pa_options=null){
+	/**
+	 *
+	 */
+	private function dedupe($doc, $parent) {
+		$acc = [];
+		$to_remove = [];
+		
+		$c = $parent->childNodes;
+		foreach($c as $n) {
+			$t = $doc->saveXML($n);
+			$ts = md5($t);
+			 if($acc[$ts]) {	// is dupe
+			 	array_push($to_remove, $n);
+			 } else {
+			 	$acc[$ts] = true;
+			 	$this->dedupe($doc, $n);
+			 }
+		}
+		foreach($to_remove as $r) {
+			$parent->removeChild($r);
+		}
+		return $doc;
+	}
+	# ------------------------------------------------------
+	/**
+	 *
+	 */
+	private function processItem($item, $po_parent, $options=null){
 		if(!($po_parent instanceof DOMNode)) return false;
 
-		//caDebug($pa_item,"Data passed to XML item processor");
+		//caDebug($item,"Data passed to XML item processor");
 
-		$vs_element = $pa_item['element'];
-		$vs_text = (isset($pa_item['text']) ? $pa_item['text'] : null);
+		$element = $item['element'];
+		$text = (isset($item['text']) ? $item['text'] : null);
 
-		$this->log(_t("XML export formatter: Processing element or attribute '%1' with text '%2' and parent element '%3' ...", $vs_element, $vs_text, $po_parent->nodeName));
+		$this->log(_t("XML export formatter: Processing element or attribute '%1' with text '%2' and parent element '%3' ...", $element, $text, $po_parent->nodeName));
 
-		$vs_first = substr($vs_element,0,1);
+		$first = substr($element,0,1);
 
-		if($vs_first == "@"){ // attribute
+		if($first == "@"){ // attribute
 			// attributes are only valid for DOMElement, not for DOMDocument
 			if(!($po_parent instanceof DOMElement)) return false;
 
-			$vs_rest = substr($vs_element,1);
-			$po_parent->setAttribute($vs_rest, $vs_text);
+			$rest = substr($element,1);
+			$po_parent->setAttribute($rest, $text);
 			$vo_new_element = $po_parent; // attributes shouldn't have children, but still ...
 		} else { // element
-			$vs_escaped_text = caEscapeForXML($vs_text);
+			$escaped_text = caEscapeForXML($text);
 
-			if(strlen($vs_text)>0){
+			if(strlen($text)>0){
 
-				if($vs_escaped_text != $vs_text) { // sth was escaped by caEscapeForXML -> wrap in CDATA
-					if(caGetOption('stripCDATA', $pa_options, false)) {
-						$vo_new_element = $this->opo_dom->createElement($vs_element, $vs_escaped_text);
+				if($escaped_text != $text) { // sth was escaped by caEscapeForXML -> wrap in CDATA
+					if(caGetOption('stripCDATA', $options, false)) {
+						$vo_new_element = $this->opo_dom->createElement($element, $escaped_text);
 					} else {
-						$vo_new_element = $this->opo_dom->createElement($vs_element);
-						$vo_new_element->appendChild(new DOMCdataSection($vs_text));
+						$vo_new_element = $this->opo_dom->createElement($element);
+						$vo_new_element->appendChild(new DOMCdataSection($text));
 					}
 				}  else { // add text as-is using DOMDocument
-					$vo_new_element = $this->opo_dom->createElement($vs_element,$vs_text);
+					$vo_new_element = $this->opo_dom->createElement($element,$text);
 				}
 				
 			} else {
-				$vo_new_element = $this->opo_dom->createElement($vs_element);
+				$vo_new_element = $this->opo_dom->createElement($element);
 			}
 			$po_parent->appendChild($vo_new_element);
 		}
 
-		if(is_array($pa_item['children'])){
-			foreach($pa_item['children'] as $va_child){
-				if(!empty($va_child)){
-					$this->processItem($va_child,$vo_new_element, $pa_options);
+		if(is_array($item['children'])){
+			foreach($item['children'] as $child){
+				if(!empty($child)){
+					$this->processItem($child,$vo_new_element, $options);
 				}
 			}
 		}
 	}
 	# ------------------------------------------------------
+	/**
+	 *
+	 */
 	public function getMappingErrors($t_mapping){
-		$va_errors = array();
+		$errors = array();
 
-		$va_top = $t_mapping->getTopLevelItems(array('dontIncludeVariables' => true));
-		if(sizeof($va_top)!==1){
-			$va_errors[] = _t("XML documents must have exactly one root element. Found %1.", sizeof($va_top));
+		$top = $t_mapping->getTopLevelItems(array('dontIncludeVariables' => true));
+		if(sizeof($top)!==1){
+			$errors[] = _t("XML documents must have exactly one root element. Found %1.", sizeof($top));
 		}
 
-		foreach($va_top as $va_item){
-			$va_errors = array_merge($va_errors,$this->getMappingErrorsForItem($va_item));
+		foreach($top as $item){
+			$errors = array_merge($errors,$this->getMappingErrorsForItem($item));
 		}
 
-		return $va_errors;
+		return $errors;
 	}
 	# ------------------------------------------------------
-	private function getMappingErrorsForItem($pa_item){
-		$va_errors = array();
-		$t_item = new ca_data_exporter_items($pa_item['item_id']);
-
-
+	/**
+	 *
+	 */
+	private function getMappingErrorsForItem($item){
+		$errors = array();
+		$t_item = new ca_data_exporter_items($item['item_id']);
+		
 		// check if element is attribute and if so, if it's valid and if it has a non-attribute parent it belongs to
-		$vs_element = $t_item->get('element');
-		$vs_first = substr($vs_element,0,1);
-		if($vs_first == "@"){
-			$vs_attribute_name = substr($vs_element,1);
-			if(!preg_match("/^[_:A-Za-z][-._:A-Za-z0-9]*$/",$vs_attribute_name)){
-				$va_errors[] = _t("Invalid XML attribute name '%1'",$vs_attribute_name);
+		$element = $t_item->get('element');
+		$first = substr($element,0,1);
+		if($first == "@"){
+			$attribute_name = substr($element,1);
+			if(!preg_match("/^[_:A-Za-z][-._:A-Za-z0-9]*$/",$attribute_name)){
+				$errors[] = _t("Invalid XML attribute name '%1'",$attribute_name);
 			}
 
 			$t_parent = new ca_data_exporter_items($t_item->get('parent_id'));
-			$vs_parent_first = substr($t_parent->get('element'),0,1);
-			if($vs_parent_first == "@" || !$t_parent->get('element')){
-				$va_errors[] = _t("XML attribute '%1' doesn't have a valid parent element",$vs_attribute_name);	
+			$parent_first = substr($t_parent->get('element'),0,1);
+			if($parent_first == "@" || !$t_parent->get('element')){
+				$errors[] = _t("XML attribute '%1' doesn't have a valid parent element",$attribute_name);	
 			}
 		} else { // plain old XML element -> check for naming convention
-			if(!preg_match("/^[_:A-Za-z][-._:A-Za-z0-9]*$/",$vs_element)){
-				$va_errors[] = _t("Invalid XML element name '%1'",$vs_element);
+			if(!preg_match("/^[_:A-Za-z][-._:A-Za-z0-9]*$/",$element)){
+				$errors[] = _t("Invalid XML element name '%1'",$element);
 			}			
 		}
 
-		foreach($t_item->getHierarchyChildren() as $va_child){
-			$va_errors = array_merge($va_errors,$this->getMappingErrorsForItem($va_child));
+		foreach($t_item->getHierarchyChildren() as $child){
+			$errors = array_merge($errors,$this->getMappingErrorsForItem($child));
 		}
 
-		return $va_errors;
+		return $errors;
 	}
 	# ------------------------------------------------------
 }
@@ -192,5 +245,13 @@ BaseExportFormat::$s_format_settings['XML'] = array(
 		'width' => 1, 'height' => 1,
 		'label' => _t("Strip CDATA"),
 		'description' => _t("By default the exporter wraps field content that contains invalid XML in CDATA sections to make sure the XML is valid regardless of the field content. If this option is set, the exporter explicitly strips the CDATA tags before returning the XML text. Use only if you know what you're doing!")
+	),
+	'deduplicate' => array(
+		'formatType' => FT_NUMBER,
+		'displayType' => DT_CHECKBOXES,
+		'default' => 0,
+		'width' => 1, 'height' => 1,
+		'label' => _t("Remove duplicated tags?"),
+		'description' => _t("Remove duplicate tags when sharing a parent.")
 	),
 );
