4fcd8794dface5f7895e63112fad4b1cb5d6a740
CollectiveAccess <seth@collectiveaccess.org>
2024-12-07 18:40:42 -0500

    Add support for attribute-specific delimiters in splitter refineries

--- app/refineries/loanSplitter/loanSplitterRefinery.php
+++ app/refineries/loanSplitter/loanSplitterRefinery.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013-2023 Whirl-i-Gig
+ * Copyright 2013-2024 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -161,6 +161,15 @@ BaseRefinery::$s_refinery_settings['loanSplitter'] = array(
 		'default' => '',
 		'label' => _t('Attributes'),
 		'description' => _t('Sets or maps metadata for the loan record by referencing the metadataElement code and the location in the data source where the data values can be found.')
+	),
+	'loanSplitter_attributeDelimiters' => array(
+		'formatType' => FT_TEXT,
+		'displayType' => DT_SELECT,
+		'width' => 10, 'height' => 1,
+		'takesLocale' => false,
+		'default' => '',
+		'label' => _t('Attribute delimiters'),
+		'description' => _t('Delimiters to use for each mapped attribute.')
 	),
 	'loanSplitter_parents' => array(
 		'formatType' => FT_TEXT,
