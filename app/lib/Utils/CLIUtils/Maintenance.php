373ca85826506be0f6c6466caf9d9b9f71ada5e5
CollectiveAccess <seth@collectiveaccess.org>
2024-12-22 09:42:07 -0500

    Provide additional information in progress bar

--- app/lib/Utils/CLIUtils/Maintenance.php
+++ app/lib/Utils/CLIUtils/Maintenance.php
@@ -32,7 +32,7 @@
 trait CLIUtilsMaintenance { 
 	# -------------------------------------------------------
 	/**
-	 * Rebuild search indices
+	 * Rebuild sort values
 	 */
 	public static function rebuild_sort_values($po_opts=null) {
 		$o_db = new Db();
@@ -62,12 +62,14 @@ trait CLIUtilsMaintenance {
 				$t_label = new $vs_label_table_name;
 				$vs_label_pk = $t_label->primaryKey();
 				$qr_labels = $o_db->query("SELECT {$vs_label_pk} FROM {$vs_label_table_name}");
+				
+				$table_name_display = $t_label->getProperty('NAME_PLURAL');
 
 				print CLIProgressBar::start($qr_labels->numRows(), _t('Processing %1', $t_label->getProperty('NAME_PLURAL')));
 				while($qr_labels->nextRow()) {
 					$vn_label_pk_val = $qr_labels->get($vs_label_pk);
 					
-					CLIProgressBar::setMessage(_t("Memory: %1", caGetMemoryUsage()));
+					CLIProgressBar::setMessage(_t("[Sort: %1][Mem: %2]", $table_name_display, caGetMemoryUsage()));
 					print CLIProgressBar::next();
 					if ($t_label->load($vn_label_pk_val)) {
 						$t_table->logChanges(false);
@@ -78,10 +80,12 @@ trait CLIUtilsMaintenance {
 			}
 
 			print CLIProgressBar::start($qr_res->numRows(), _t('Processing %1 identifiers', $t_table->getProperty('NAME_SINGULAR')));
+			
+			$table_name_display = $t_table->getProperty('NAME_PLURAL');
 			while($qr_res->nextRow()) {
 				$vn_pk_val = $qr_res->get($vs_pk);
 				
-				CLIProgressBar::setMessage(_t("Memory: %1", caGetMemoryUsage()));
+				CLIProgressBar::setMessage(_t("[Sort: %1 identifiers][Mem: %2]", $table_name_display, caGetMemoryUsage()));
 				print CLIProgressBar::next();
 				if ($t_table->load($vn_pk_val)) {
 					$t_table->logChanges(false);
@@ -1058,6 +1062,7 @@ trait CLIUtilsMaintenance {
 			CLIUtils::addMessage(_t('Clearing application caches...'));
 			if (is_writable($config->get('taskqueue_tmp_directory'))) {
 				caRemoveDirectory($config->get('taskqueue_tmp_directory'), false);
+				mkdir($config->get('purify_serializer_path'));
 			} else {
 				CLIUtils::addError(_t('Skipping clearing of application cache because it is not writable'));
 			}
@@ -2094,9 +2099,12 @@ trait CLIUtilsMaintenance {
 				$t = Datamodel::getInstance($table, true);
 				$qr = $table::find('*', ['returnAs' => 'searchResult']);
 				print CLIProgressBar::start($qr->numHits(), _t('Starting...'));
+				
+				$table_name_display = $t->getProperty('NAME_PLURAL');
+				
 				while($qr->nextHit()) {
 					if ($t->load($qr->getPrimaryKey())) {
-						print CLIProgressBar::next(1, _t('Processing %1', $t->getWithTemplate("^{$table}.preferred_labels (^{$table}.idno)")));
+						print CLIProgressBar::next(1, _t('[History: %1][Mem: %2] %3', $table_name_display, caGetMemoryUsage(), $t->getWithTemplate("^{$table}.preferred_labels (^{$table}.idno)")));
 						if ($t->deriveHistoryTrackingCurrentValue()) {
 							$c++;
 						}
