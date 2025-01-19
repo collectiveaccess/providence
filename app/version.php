c9e3c237321b7e43d4db107f3a4db08b8a1e2541
CollectiveAccess <seth@collectiveaccess.org>
2025-01-19 11:14:17 -0500

    Add new minor version constant

--- app/version.php
+++ app/version.php
@@ -1,6 +1,9 @@
 <?php
- 	# CollectiveAccess version number; don't change
+ 	# CollectiveAccess major version number; don't change
 	define('__CollectiveAccess__', '2.0');
+	
+ 	# CollectiveAccess minor version number; don't change
+	define('__CollectiveAccess_Version__', '2.0.4');
 
 	# Schema revision
 	define('__CollectiveAccess_Schema_Rev__', 199);
