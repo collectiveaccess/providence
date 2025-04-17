c1860a50e3bee4fcb77f96ae6e6fda7bd4662481
CollectiveAccess <seth@collectiveaccess.org>
2025-04-17 00:25:50 -0400

    Add expanded health check info

--- hc/index.php
+++ hc/index.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2019 Whirl-i-Gig
+ * Copyright 2019-2025 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -25,38 +25,69 @@
  *
  * ----------------------------------------------------------------------
  */
-	define("__CA_APP_TYPE__", "PROVIDENCE");
-	define("__CA_MICROTIME_START_OF_REQUEST__", microtime());
-	define("__CA_BASE_MEMORY_USAGE__", memory_get_usage(true));
-	require("../app/helpers/errorHelpers.php");
+define("__CA_APP_TYPE__", "PROVIDENCE");
+define("__CA_MICROTIME_START_OF_REQUEST__", microtime());
+define("__CA_BASE_MEMORY_USAGE__", memory_get_usage(true));
+require("../app/helpers/errorHelpers.php");
+
+$s = explode("/", isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : __FILE__);
+array_pop($s); array_pop($s);
+define("__CA_BASE_DIR__", join("/", $s));
+
+if (!file_exists('../setup.php')) {
+	print "No setup.php found";
+	http_response_code(500);
+	exit; 
+}
+if (!@require('../setup.php')) { 
+	print "Loading setup.php failed";
+	http_response_code(500);
+	exit; 
+}
+if (!@require_once('../app/helpers/post-setup.php')) {
+	print "Loading post-setup.php failed";
+	http_response_code(500);
+	exit; 
+}
+
+require_once(__CA_APP_DIR__.'/lib/ConfigurationCheck.php');
+ConfigurationCheck::performQuick(['skipPathChecks' => true]);
+if(ConfigurationCheck::foundErrors()){
+	print "Configuration check failed";
+	ConfigurationCheck::renderErrorsAsHTMLOutput();
+	http_response_code(500);
+	exit; 
+}
+
+if(isset($_REQUEST['key']) && defined('__CA_HC_INFO_KEY__') && strlen(__CA_HC_INFO_KEY__) && (__CA_HC_INFO_KEY__ === $_REQUEST['key'])) {
+	header("Content-type: application/json");
+	$ret = [
+		'status' => 'happy',
+		'php_version' => caGetPHPVersion(),
+		'ca_version' => __CollectiveAccess_Version__,
+		'schema_revision' => __CollectiveAccess_Schema_Rev__,
+		
+		'server_time' => date('c'),
+		'timezone' => date_default_timezone_get(),
+		'cpu_load' => sys_getloadavg(),
+		'os' => php_uname()
+	];
 	
-	$s = explode("/", isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : __FILE__);
-	array_pop($s); array_pop($s);
-	define("__CA_BASE_DIR__", join("/", $s));
-	
-	if (!file_exists('../setup.php')) {
-		print "No setup.php found";
-		http_response_code(500);
-		exit; 
-	}
-	if (!@require('../setup.php')) { 
-		print "Loading setup.php failed";
-	    http_response_code(500);
-		exit; 
-	}
-	if (!@require_once('../app/helpers/post-setup.php')) {
-		print "Loading post-setup.php failed";
-	    http_response_code(500);
-		exit; 
+	foreach([
+		'CA_PROVIDENCE_BRANCH' => 'providence_branch',
+		'CA_PAWTUCKET_BRANCH' => 'pawtucket_branch',
+		'WORDPRESS_BRANCH' => 'wordpress_branch',
+		'CA_PROVIDENCE_ROOT' => 'providence_path',
+		'CA_PAWTUCKET_ROOT' => 'pawtucket_path',
+		'WORDPRESS_ROOT' => 'wordpress_path'
+	] as $env => $k) {
+		if(strlen($e = getenv($env))) {
+			$ret[$k] = $e;
+		} 
 	}
 	
-	require_once(__CA_APP_DIR__.'/lib/ConfigurationCheck.php');
-	ConfigurationCheck::performQuick(['skipPathChecks' => true]);
-	if(ConfigurationCheck::foundErrors()){
-		print "Configuration check failed";
-		ConfigurationCheck::renderErrorsAsHTMLOutput();
-	    http_response_code(500);
-		exit; 
-	}
-	
+	print caFormatJson(json_encode($ret));
+} else {
+	header("Content-type: text/plain");
 	print "status=happy";
+}
