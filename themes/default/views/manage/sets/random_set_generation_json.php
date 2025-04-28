ebe9074a012e63d8c4da4dad5275bc9612e33d8d
seth <Seth>
2025-04-28 09:21:48 -0400

    correct copyright year

--- themes/default/views/manage/sets/random_set_generation_json.php
+++ themes/default/views/manage/sets/random_set_generation_json.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 022 Whirl-i-Gig
+ * Copyright 2025 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
