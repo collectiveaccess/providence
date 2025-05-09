a522d2511a7b6c4392bd9afe004e7fd4ba426fef
seth <Seth>
2025-05-09 15:23:45 -0400

    Formatting

--- themes/default/views/editor/collections/screen_html.php
+++ themes/default/views/editor/collections/screen_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2008-2023 Whirl-i-Gig
+ * Copyright 2008-2025 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -25,62 +25,62 @@
  *
  * ----------------------------------------------------------------------
  */
- 	$t_collection 		= $this->getVar('t_subject');
-	$vn_collection_id 	= $this->getVar('subject_id');
-	$vn_above_id 		= $this->getVar('above_id');
-	$vn_after_id 		= $this->getVar('after_id');
+$t_collection 		= $this->getVar('t_subject');
+$collection_id 	= $this->getVar('subject_id');
+$above_id 		= $this->getVar('above_id');
+$after_id 		= $this->getVar('after_id');
 
-	$vb_can_edit	 	= $t_collection->isSaveable($this->request);
-	$vb_can_delete		= $t_collection->isDeletable($this->request);
+$can_edit	 	= $t_collection->isSaveable($this->request);
+$can_delete		= $t_collection->isDeletable($this->request);
 
-	$vs_rel_table		= $this->getVar('rel_table');
-	$vn_rel_type_id		= $this->getVar('rel_type_id');
-	$vn_rel_id			= $this->getVar('rel_id');
-	
-	$forced_values 		= $this->getVar('forced_values') ?? [];
+$rel_table		= $this->getVar('rel_table');
+$rel_type_id		= $this->getVar('rel_type_id');
+$rel_id			= $this->getVar('rel_id');
 
-	if ($vb_can_edit) {
-		$va_cancel_parameters = ($vn_collection_id ? array('collection_id' => $vn_collection_id) : array('type_id' => $t_collection->getTypeID()));
-		print $vs_control_box = caFormControlBox(
-			caFormSubmitButton($this->request, __CA_NAV_ICON_SAVE__, _t("Save"), 'CollectionEditorForm').' '.
-			($this->getVar('show_save_and_return') ? caFormSubmitButton($this->request, __CA_NAV_ICON_SAVE__, _t("Save and return"), 'CollectionEditorForm', array('isSaveAndReturn' => true)) : '').' '.
-			caFormNavButton($this->request, __CA_NAV_ICON_CANCEL__, _t("Cancel"), '', 'editor/collections', 'CollectionEditor', 'Edit/'.$this->request->getActionExtra(), $va_cancel_parameters),
-			($this->getVar('show_show_notifications') ? caFormJSButton($this->request, __CA_NAV_ICON_ALERT__, _t("Show editor alerts"), '', ['class' => 'caEditorFormNotifications']) : ''), 
-			((intval($vn_collection_id) > 0) && $vb_can_delete) ? caFormNavButton($this->request, __CA_NAV_ICON_DELETE__, _t("Delete"), '', 'editor/collections', 'CollectionEditor', 'Delete/'.$this->request->getActionExtra(), array('collection_id' => $vn_collection_id)) : ''
-		);
-	}
+$forced_values 		= $this->getVar('forced_values') ?? [];
+
+if ($can_edit) {
+	$cancel_parameters = ($collection_id ? array('collection_id' => $collection_id) : array('type_id' => $t_collection->getTypeID()));
+	print $control_box = caFormControlBox(
+		caFormSubmitButton($this->request, __CA_NAV_ICON_SAVE__, _t("Save"), 'CollectionEditorForm').' '.
+		($this->getVar('show_save_and_return') ? caFormSubmitButton($this->request, __CA_NAV_ICON_SAVE__, _t("Save and return"), 'CollectionEditorForm', array('isSaveAndReturn' => true)) : '').' '.
+		caFormNavButton($this->request, __CA_NAV_ICON_CANCEL__, _t("Cancel"), '', 'editor/collections', 'CollectionEditor', 'Edit/'.$this->request->getActionExtra(), $cancel_parameters),
+		($this->getVar('show_show_notifications') ? caFormJSButton($this->request, __CA_NAV_ICON_ALERT__, _t("Show editor alerts"), '', ['class' => 'caEditorFormNotifications']) : ''), 
+		((intval($collection_id) > 0) && $can_delete) ? caFormNavButton($this->request, __CA_NAV_ICON_DELETE__, _t("Delete"), '', 'editor/collections', 'CollectionEditor', 'Delete/'.$this->request->getActionExtra(), array('collection_id' => $collection_id)) : ''
+	);
+}
 ?>
-	<div class="sectionBox">
+<div class="sectionBox">
 <?php
-			print caFormTag($this->request, 'Save/'.$this->request->getActionExtra().'/collection_id/'.$vn_collection_id, 'CollectionEditorForm', null, 'POST', 'multipart/form-data');
-			
-			$va_bundle_list = [];
-			$va_form_elements = $t_collection->getBundleFormHTMLForScreen($this->request->getActionExtra(), array(
-									'request' => $this->request, 
-									'formName' => 'CollectionEditorForm',
-									'forcedValues' => $forced_values), $va_bundle_list);
-												
-			print join("\n", $va_form_elements);
-			
-			if ($vb_can_edit) { print $vs_control_box; }
+		print caFormTag($this->request, 'Save/'.$this->request->getActionExtra().'/collection_id/'.$collection_id, 'CollectionEditorForm', null, 'POST', 'multipart/form-data');
+		
+		$bundle_list = [];
+		$form_elements = $t_collection->getBundleFormHTMLForScreen($this->request->getActionExtra(), array(
+								'request' => $this->request, 
+								'formName' => 'CollectionEditorForm',
+								'forcedValues' => $forced_values), $bundle_list);
+											
+		print join("\n", $form_elements);
+		
+		if ($can_edit) { print $control_box; }
 ?>
-			<input type='hidden' name='collection_id' value='<?= $vn_collection_id; ?>'/>
-			<input type='hidden' name='above_id' value='<?= $vn_above_id; ?>'/>
-			<input id='isSaveAndReturn' type='hidden' name='is_save_and_return' value='0'/>
-			<input type='hidden' name='rel_table' value='<?= $vs_rel_table; ?>'/>
-			<input type='hidden' name='rel_type_id' value='<?= $vn_rel_type_id; ?>'/>
-			<input type='hidden' name='rel_id' value='<?= $vn_rel_id; ?>'/>
-			<input type='hidden' name='after_id' value='<?= $vn_after_id; ?>'/>
+		<input type='hidden' name='collection_id' value='<?= $collection_id; ?>'/>
+		<input type='hidden' name='above_id' value='<?= $above_id; ?>'/>
+		<input id='isSaveAndReturn' type='hidden' name='is_save_and_return' value='0'/>
+		<input type='hidden' name='rel_table' value='<?= $rel_table; ?>'/>
+		<input type='hidden' name='rel_type_id' value='<?= $rel_type_id; ?>'/>
+		<input type='hidden' name='rel_id' value='<?= $rel_id; ?>'/>
+		<input type='hidden' name='after_id' value='<?= $after_id; ?>'/>
 <?php
-			if($this->request->getParameter('rel', pInteger)) {
+		if($this->request->getParameter('rel', pInteger)) {
 ?>
-				<input type='hidden' name='rel' value='1'/>
+			<input type='hidden' name='rel' value='1'/>
 <?php
-			}
+		}
 ?>
-		</form>
-	</div>
+	</form>
+</div>
 
-	<div class="editorBottomPadding"><!-- empty --></div>
-	
-	<?= caSetupEditorScreenOverlays($this->request, $t_collection, $va_bundle_list); ?>
+<div class="editorBottomPadding"><!-- empty --></div>
+
+<?= caSetupEditorScreenOverlays($this->request, $t_collection, $bundle_list); ?>
