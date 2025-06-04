0df1f982d1081ca6a6d4a61d1d3e14d06493260c
CollectiveAccess <seth@collectiveaccess.org>
2025-06-04 00:24:35 -0400

    Formatting

--- themes/default/views/find/Results/ajax_results_editable_complex_data_form_html.php
+++ themes/default/views/find/Results/ajax_results_editable_complex_data_form_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2016-2021 Whirl-i-Gig
+ * Copyright 2016-2025 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -25,82 +25,78 @@
  *
  * ----------------------------------------------------------------------
  */
- 
- 	$t_subject = 		$this->getVar('t_subject');
- 	$vn_subject_id = 	$t_subject->getPrimaryKey();
- 	
- 	$va_bundles = 		$this->getVar('bundles');
- 	$vs_bundle = 		$this->getVar('bundle');
- 	$vn_row = 			$this->getVar('row');
- 	$vn_col = 			$this->getVar('col');
- 	$placement_id = 	$this->getVar('placement_id');
- 	
- 	$vb_can_edit = true;
-?>
-	
-<?php
+$t_subject = 		$this->getVar('t_subject');
+$subject_id = 		$t_subject->getPrimaryKey();
 
-	$va_form_elements = $t_subject->getBundleFormHTMLForScreen(null, array(
-			'request' => $this->request, 
-			'formName' => 'complex',
-			'bundles' => $va_bundles,
-			'dontAllowBundleShowHide' => true
-	));
-	
-	print caFormTag($this->request, '#', 'caEditableResultsComplexDataForm', null, 'POST', 'multipart/form-data', null, array('noCSRFToken' => true, 'disableUnsavedChangesWarning' => true, 'disableSubmit' => true));
-?>
-		<div class="caResultsComplexDataEditorErrorContainer" id="caEditableResultsComplexDataFormErrors"> </div>
-		
-		<div id="caResultsComplexDataEditorPanelControlButtons">
-<?php
-		if ($vb_can_edit) {
-			print caFormControlBox(caFormJSButton($this->request, __CA_NAV_ICON_SAVE__, _t("Save"), "caEditableResultsComplexDataFormSaveButton", array("onclick" => "caEditableResultsComplexDataFormHandler.save(event);"))
-				.' '.caFormJSButton($this->request, __CA_NAV_ICON_CANCEL__, _t("Cancel"), "caEditableResultsComplexDataFormCancelButton", array("onclick" => "jQuery(\"#caEditableResultsComplexDataForm\").parent().parent().data(\"panel\").hidePanel();")), 
-				'',
-				''
-			);
-		}
-?>
-		</div>
-		<div class="caResultsComplexDataEditorSectionBox">
-			<div style="margin: 5px;"> </div>
-<?php
-		print join("\n", $va_form_elements); 
+$bundles = 			$this->getVar('bundles');
+$bundle = 			$this->getVar('bundle');
+$row = 				$this->getVar('row');
+$col = 				$this->getVar('col');
+$placement_id = 	$this->getVar('placement_id');
 
-		print caHTMLHiddenInput('id', array('value' => $vn_subject_id));
-		print caHTMLHiddenInput('bundle', array('value' => $vs_bundle));
-		print caHTMLHiddenInput('row', array('value' => $vn_row));
-		print caHTMLHiddenInput('col', array('value' => $vn_col));
+$can_edit = true;
+
+$form_elements = $t_subject->getBundleFormHTMLForScreen(null, array(
+		'request' => $this->request, 
+		'formName' => 'complex',
+		'bundles' => $bundles,
+		'dontAllowBundleShowHide' => true
+));
+
+print caFormTag($this->request, '#', 'caEditableResultsComplexDataForm', null, 'POST', 'multipart/form-data', null, array('noCSRFToken' => true, 'disableUnsavedChangesWarning' => true, 'disableSubmit' => true));
+?>
+	<div class="caResultsComplexDataEditorErrorContainer" id="caEditableResultsComplexDataFormErrors"> </div>
+	
+	<div id="caResultsComplexDataEditorPanelControlButtons">
+<?php
+	if ($can_edit) {
+		print caFormControlBox(caFormJSButton($this->request, __CA_NAV_ICON_SAVE__, _t("Save"), "caEditableResultsComplexDataFormSaveButton", array("onclick" => "caEditableResultsComplexDataFormHandler.save(event);"))
+			.' '.caFormJSButton($this->request, __CA_NAV_ICON_CANCEL__, _t("Cancel"), "caEditableResultsComplexDataFormCancelButton", array("onclick" => "jQuery(\"#caEditableResultsComplexDataForm\").parent().parent().data(\"panel\").hidePanel();")), 
+			'',
+			''
+		);
+	}
+?>
+	</div>
+	<div class="caResultsComplexDataEditorSectionBox">
+		<div style="margin: 5px;"> </div>
+<?php
+		print join("\n", $form_elements); 
+
+		print caHTMLHiddenInput('id', array('value' => $subject_id));
+		print caHTMLHiddenInput('bundle', array('value' => $bundle));
+		print caHTMLHiddenInput('row', array('value' => $row));
+		print caHTMLHiddenInput('col', array('value' => $col));
 		print caHTMLHiddenInput('placement_id', array('value' => $placement_id));
 ?>
-		</div>
-	</form>
+	</div>
+</form>
 	
-	<script type="text/javascript">
-		var caEditableResultsComplexDataFormHandler = caUI.initQuickAddFormHandler({
-			formID: 'caEditableResultsComplexDataForm',
-			formErrorsPanelID: 'caEditableResultsComplexDataFormErrors',
-			formTypeSelectID: null, 
-		
-			formUrl: <?= json_encode(caNavUrl($this->request, '*', '*', 'resultsComplexDataEditor')); ?>,
-			fileUploadUrl: <?= json_encode(caNavUrl($this->request, "*", "*", "saveResultsEditorFiles")); ?>,
-			saveUrl: <?= json_encode(caNavUrl($this->request, "*", "*", "saveResultsEditorData")); ?>,
-			csrfToken: <?= json_encode(caGenerateCSRFToken($this->request)); ?>,
-		
-			headerText: <?= json_encode(_t('Edit %1', $t_subject->getTypeName())); ?>,
-			saveText: <?= json_encode(_t('Updated %1 ', $t_subject->getTypeName())." <em>%1</em>"); ?>,
-			busyIndicator: <?= json_encode(caBusyIndicatorIcon($this->request)); ?>,
-			onSave: function(resp) { 
-				if (resp.status == 0) {
-					var ht = jQuery("#caResultsEditorWrapper .caResultsEditorContent").data('handsontable');
-					ht.setDataAtCell(<?= (int)$vn_row; ?>, <?= (int)$vn_col; ?>, resp.display, 'external');
-					if (jQuery("#caEditableResultsComplexDataForm") && jQuery("#caEditableResultsComplexDataForm").parent() && jQuery("#caEditableResultsComplexDataForm").parent().parent() && jQuery("#caEditableResultsComplexDataForm").parent().parent().data("panel")) { jQuery("#caEditableResultsComplexDataForm").parent().parent().data("panel").hidePanel();  }
-					jQuery(".caResultsEditorStatus").html("Saved changes").show();
-					setTimeout(function() { jQuery('.caResultsEditorStatus').fadeOut(500); }, 5000);
-				} else {
-					caEditableResultsComplexDataFormHandler.setErrors(resp.errors);
-					jQuery("#caEditableResultsComplexDataForm input[name=form_timestamp]").val(resp.time);	// update form timestamp
-				}
+<script type="text/javascript">
+	var caEditableResultsComplexDataFormHandler = caUI.initQuickAddFormHandler({
+		formID: 'caEditableResultsComplexDataForm',
+		formErrorsPanelID: 'caEditableResultsComplexDataFormErrors',
+		formTypeSelectID: null, 
+	
+		formUrl: <?= json_encode(caNavUrl($this->request, '*', '*', 'resultsComplexDataEditor')); ?>,
+		fileUploadUrl: <?= json_encode(caNavUrl($this->request, "*", "*", "saveResultsEditorFiles")); ?>,
+		saveUrl: <?= json_encode(caNavUrl($this->request, "*", "*", "saveResultsEditorData")); ?>,
+		csrfToken: <?= json_encode(caGenerateCSRFToken($this->request)); ?>,
+	
+		headerText: <?= json_encode(_t('Edit %1', $t_subject->getTypeName())); ?>,
+		saveText: <?= json_encode(_t('Updated %1 ', $t_subject->getTypeName())." <em>%1</em>"); ?>,
+		busyIndicator: <?= json_encode(caBusyIndicatorIcon($this->request)); ?>,
+		onSave: function(resp) { 
+			if (resp.status == 0) {
+				var ht = jQuery("#caResultsEditorWrapper .caResultsEditorContent").data('handsontable');
+				ht.setDataAtCell(<?= (int)$row; ?>, <?= (int)$col; ?>, resp.display, 'external');
+				if (jQuery("#caEditableResultsComplexDataForm") && jQuery("#caEditableResultsComplexDataForm").parent() && jQuery("#caEditableResultsComplexDataForm").parent().parent() && jQuery("#caEditableResultsComplexDataForm").parent().parent().data("panel")) { jQuery("#caEditableResultsComplexDataForm").parent().parent().data("panel").hidePanel();  }
+				jQuery(".caResultsEditorStatus").html("Saved changes").show();
+				setTimeout(function() { jQuery('.caResultsEditorStatus').fadeOut(500); }, 5000);
+			} else {
+				caEditableResultsComplexDataFormHandler.setErrors(resp.errors);
+				jQuery("#caEditableResultsComplexDataForm input[name=form_timestamp]").val(resp.time);	// update form timestamp
 			}
-		});
-	</script>	
+		}
+	});
+</script>	
