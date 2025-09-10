179c485181e6b73f0b78f5c76f267bdce598302d
CollectiveAccess <seth@collectiveaccess.org>
2025-09-10 11:15:06 -0400

    Honor delete privs in set list

--- themes/default/views/manage/set_list_html.php
+++ themes/default/views/manage/set_list_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2009-2024 Whirl-i-Gig
+ * Copyright 2009-2025 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -25,36 +25,44 @@
  *
  * ----------------------------------------------------------------------
  */
-$t_set 				= $this->getVar('t_set');
-$va_set_list 		= $this->getVar('set_list');
-$vn_type_id 		= $this->getVar('list_set_type_id');
-$vs_current_sort 	= $this->getVar('current_sort');
-$vs_current_sort_direction 	= $this->getVar('current_sort_direction');
-$vs_type_name_singular		= $this->getVar('type_name_singular');
+$t_set 						= $this->getVar('t_set');
+$set_list 					= $this->getVar('set_list');
+$type_id 					= $this->getVar('list_set_type_id');
+$current_sort 				= $this->getVar('current_sort');
+$current_sort_direction 	= $this->getVar('current_sort_direction');
+$type_name_singular			= $this->getVar('type_name_singular');
+$table_list 				= $this->getVar('table_list');
 
-$user_id = $this->request->getUserID();
-	
+$user_id 					= $this->request->getUserID();
+$can_delete					= $this->request->user->canDoAction("can_delete_sets") || $this->request->user->canDoAction("can_delete_own_sets");
+
 if (!$this->request->isAjax()) {
-	$vs_set_type_menu = '<div class="sf-small-menu form-header-button rounded" style="padding: 6px;">'.
+	$set_type_menu = '<div class="sf-small-menu form-header-button rounded" style="padding: 6px;">'.
 							'<div class="caNavHeaderIcon">'.
 								'<a href="#" onclick="_navigateToNewForm(jQuery(\'#typeList\').val(), jQuery(\'#tableList\').val());">'.caNavIcon(__CA_NAV_ICON_ADD__, 2).'</a>'.
 							'</div>'.
 						'<form action="#">'._t('Create new').' ';
-	if(!$vn_type_id){
+	if(!$type_id){
 		$t_list = new ca_lists();
-		$vs_set_type_menu .= $t_list->getListAsHTMLFormElement('set_types', 'set_type', ['id' => 'typeList']).' ';
+		$set_type_menu .= $t_list->getListAsHTMLFormElement('set_types', 'set_type', ['id' => 'typeList']).' ';
 	}else{
-		$vs_set_type_menu .= " <b>".mb_strtolower($vs_type_name_singular)."</b><input type='hidden' id='typeList' name='set_type' value='".$vn_type_id."'> ";
+		$set_type_menu .= " <b>".mb_strtolower($type_name_singular)."</b><input type='hidden' id='typeList' name='set_type' value='".$type_id."'> ";
 	}
-	$vs_set_type_menu .= _t('containing').' '.caHTMLSelect('table_num', $this->getVar('table_list'), ['id' => 'tableList'], ['value' => Datamodel::getTableNum($t_set->getAppConfig()->get('set_editor_default_table'))]).'</form>'.'</div>';
-
+	$set_type_menu .= _t('containing').' ';
+	
+	if(sizeof($table_list) > 1) {
+		$set_type_menu .= caHTMLSelect('table_num', $table_list, ['id' => 'tableList'], ['value' => Datamodel::getTableNum($t_set->getAppConfig()->get('set_editor_default_table'))]).'</form>'.'</div>';
+	} else {
+		$n = array_keys($table_list);
+		$set_type_menu .= $n[0].caHTMLHiddenInput('table_num', ['value' => $table_list[$n[0]], 'id' => 'tableList']);
+	}
 ?>
 <script language="JavaScript" type="text/javascript">
 	jQuery(document).ready(function(){
 		jQuery('#setSearch').autocomplete(
 			{
 				minLength: 3, delay: 800, html: true,
-				source: '<?= caNavUrl($this->request, 'lookup', 'Set', 'Get', array('noInline' => 1, 'type' => $vn_type_id)); ?>',
+				source: '<?= caNavUrl($this->request, 'lookup', 'Set', 'Get', array('noInline' => 1, 'type' => $type_id)); ?>',
 				select: function(event, ui) {
 					if (parseInt(ui.item.id) > 0) {
 						jQuery('#setSearch').val('');
@@ -71,15 +79,15 @@ if (!$this->request->isAjax()) {
 </script>
 <div class="sectionBox">
 	<?php 
-		$vs_type_id_form_element = '';
-		if ($vn_type_id = intval($this->getVar('list_set_type_id'))) {
-			$vs_type_id_form_element = '<input type="hidden" name="type_id" value="'.$vn_type_id.'"/>';
+		$type_id_form_element = '';
+		if ($type_id = intval($this->getVar('list_set_type_id'))) {
+			$type_id_form_element = '<input type="hidden" name="type_id" value="'.$type_id.'"/>';
 		}
 		print caFormTag($this->request, 'ListSets', 'SetSearchForm', null, 'post', 'multipart/form-data', '_top', array('noCSRFToken' => false, 'disableUnsavedChangesWarning' => true));
 		print caFormControlBox(
-			'<div class="simple-search-box">'._t('Search').': <input type="text" id="setSearch" name="search" value="'.htmlspecialchars($this->getVar('search'), ENT_QUOTES, 'UTF-8').'" size="20"/></div>'.$vs_type_id_form_element,
+			'<div class="simple-search-box">'._t('Search').': <input type="text" id="setSearch" name="search" value="'.htmlspecialchars($this->getVar('search'), ENT_QUOTES, 'UTF-8').'" size="20"/></div>'.$type_id_form_element,
 			'',
-			$vs_set_type_menu
+			$set_type_menu
 		); 
 ?>
 		</form>
@@ -102,58 +110,58 @@ if (!$this->request->isAjax()) {
 		<thead>
 			<tr>
 				<th class="list-header-nosort"> </th>
-				<th class="<?= (($vs_current_sort == "name") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-					<?= caNavLink($this->request, _t('Name'), '', 'manage', 'Set', 'ListSets', array('sort' => 'name', 'direction' => ((($vs_current_sort == "name") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+				<th class="<?= (($current_sort == "name") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+					<?= caNavLink($this->request, _t('Name'), '', 'manage', 'Set', 'ListSets', array('sort' => 'name', 'direction' => ((($current_sort == "name") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 				</th>
-				<th class="<?= (($vs_current_sort == "set_content_type") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-					<?= caNavLink($this->request, _t('Content'), '', 'manage', 'Set', 'ListSets', array('sort' => 'set_content_type', 'direction' => ((($vs_current_sort == "set_content_type") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+				<th class="<?= (($current_sort == "set_content_type") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+					<?= caNavLink($this->request, _t('Content'), '', 'manage', 'Set', 'ListSets', array('sort' => 'set_content_type', 'direction' => ((($current_sort == "set_content_type") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 				</th>
 <?php
-				if(!$vn_type_id){
+				if(!$type_id){
 ?>
-					<th class="<?= (($vs_current_sort == "set_type") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-						<?= caNavLink($this->request, _t('Type'), '', 'manage', 'Set', 'ListSets', array('sort' => 'set_type', 'direction' => ((($vs_current_sort == "set_type") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+					<th class="<?= (($current_sort == "set_type") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+						<?= caNavLink($this->request, _t('Type'), '', 'manage', 'Set', 'ListSets', array('sort' => 'set_type', 'direction' => ((($current_sort == "set_type") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 					</th>
 <?php
 				}
 ?>
-				<th class="<?= (($vs_current_sort == "item_count") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-					<?= caNavLink($this->request, _t('# Items'), '', 'manage', 'Set', 'ListSets', array('sort' => 'item_count', 'direction' => ((($vs_current_sort == "item_count") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+				<th class="<?= (($current_sort == "item_count") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+					<?= caNavLink($this->request, _t('# Items'), '', 'manage', 'Set', 'ListSets', array('sort' => 'item_count', 'direction' => ((($current_sort == "item_count") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 				</th>
-				<th class="<?= (($vs_current_sort == "lname") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-					<?= caNavLink($this->request, _t('Owner'), '', 'manage', 'Set', 'ListSets', array('sort' => 'lname', 'direction' => ((($vs_current_sort == "lname") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+				<th class="<?= (($current_sort == "lname") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+					<?= caNavLink($this->request, _t('Owner'), '', 'manage', 'Set', 'ListSets', array('sort' => 'lname', 'direction' => ((($current_sort == "lname") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 				</th>
-				<th class="<?= (($vs_current_sort == "access") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-					<?= caNavLink($this->request, _t('Access'), '', 'manage', 'Set', 'ListSets', array('sort' => 'access', 'direction' => ((($vs_current_sort == "access") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+				<th class="<?= (($current_sort == "access") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+					<?= caNavLink($this->request, _t('Access'), '', 'manage', 'Set', 'ListSets', array('sort' => 'access', 'direction' => ((($current_sort == "access") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 				</th>
-				<th class="<?= (($vs_current_sort == "created") ? "list-header-sorted-".$vs_current_sort_direction : ""); ?> list-header-nolink">
-					<?= caNavLink($this->request, _t('Created'), '', 'manage', 'Set', 'ListSets', array('sort' => 'created', 'direction' => ((($vs_current_sort == "created") && ($vs_current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
+				<th class="<?= (($current_sort == "created") ? "list-header-sorted-".$current_sort_direction : ""); ?> list-header-nolink">
+					<?= caNavLink($this->request, _t('Created'), '', 'manage', 'Set', 'ListSets', array('sort' => 'created', 'direction' => ((($current_sort == "created") && ($current_sort_direction != "desc")) ? "desc" : "asc"))); ?>
 				</th>
 				<th class="{sorter: false} list-header-nosort listtableEdit"> </th>
 			</tr>
 		</thead>
 		<tbody id="setListBody">
 <?php
-	if (sizeof($va_set_list)) {
-		foreach($va_set_list as $va_set) {
+	if (sizeof($set_list)) {
+		foreach($set_list as $set) {
 ?>
 			<tr>
 				<td>
-					<input type="checkbox" class="algebraSetSelector set-table-<?= $va_set["table_num"]; ?>" name="algebra_set_id[]" data-table_num="<?= $va_set["table_num"]; ?>" value="<?= $va_set["set_id"]; ?>">
+					<input type="checkbox" class="algebraSetSelector set-table-<?= $set["table_num"]; ?>" name="algebra_set_id[]" data-table_num="<?= $set["table_num"]; ?>" value="<?= $set["set_id"]; ?>">
 				</td>
 				<td>
-					<div class="caItemListName"><?= $va_set['name'].($va_set['set_code'] ? 
-					((mb_strlen($va_set['set_code']) > 20) ? "<br/>(<span class='abbreviatedPath' title='{$va_set['set_code']}'>".
-					caTruncateStringWithEllipsis($va_set['set_code'], 20, 'start')."</span>)" : '<br/>('.$va_set['set_code'].')') : ""); ?></div>
+					<div class="caItemListName"><?= $set['name'].($set['set_code'] ? 
+					((mb_strlen($set['set_code']) > 20) ? "<br/>(<span class='abbreviatedPath' title='{$set['set_code']}'>".
+					caTruncateStringWithEllipsis($set['set_code'], 20, 'start')."</span>)" : '<br/>('.$set['set_code'].')') : ""); ?></div>
 				</td>
 				<td>
-					<div><?= $va_set['set_content_type']; ?></div>
+					<div><?= $set['set_content_type']; ?></div>
 				</td>
 <?php
-				if(!$vn_type_id){
+				if(!$type_id){
 ?>
 				<td>
-					<div><?= $va_set['set_type']; ?></div>
+					<div><?= $set['set_type']; ?></div>
 				</td>
 <?php
 				}
@@ -161,27 +169,27 @@ if (!$this->request->isAjax()) {
 				<td align="center">
 					<div>
 <?php 	
-					if (($va_set['item_count'] > 0) && ($this->request->user->canDoAction('can_batch_edit_'.Datamodel::getTableName($va_set['table_num'])))) {
-						print caNavButton($this->request, __CA_NAV_ICON_BATCH_EDIT__, _t('Batch edit'), 'batchIcon', 'batch', 'Editor', 'Edit', array('id' => 'ca_sets:'.$va_set['set_id']), array(), array('icon_position' => __CA_NAV_ICON_ICON_POS_LEFT__, 'use_class' => 'list-button', 'no_background' => true, 'dont_show_content' => true));
-						print $va_set['item_count']; 
+					if (($set['item_count'] > 0) && ($this->request->user->canDoAction('can_batch_edit_'.Datamodel::getTableName($set['table_num'])))) {
+						print caNavButton($this->request, __CA_NAV_ICON_BATCH_EDIT__, _t('Batch edit'), 'batchIcon', 'batch', 'Editor', 'Edit', array('id' => 'ca_sets:'.$set['set_id']), array(), array('icon_position' => __CA_NAV_ICON_ICON_POS_LEFT__, 'use_class' => 'list-button', 'no_background' => true, 'dont_show_content' => true));
+						print $set['item_count']; 
 					} else {
-						print $va_set['item_count']; 
+						print $set['item_count']; 
 					}
 ?>
 					</div>
 				</td>
 				<td>
-					<div class="caItemListOwner"><?= $va_set['fname'].' '.$va_set['lname'].($va_set['email'] ? "<br/>(<a href='mailto:".$va_set['email']."'>".$va_set['email']."</a>)" : ""); ?></div>
+					<div class="caItemListOwner"><?= $set['fname'].' '.$set['lname'].($set['email'] ? "<br/>(<a href='mailto:".$set['email']."'>".$set['email']."</a>)" : ""); ?></div>
 				</td>
 				<td>
-					<div><?= $t_set->getChoiceListValue('access', $va_set['access']); ?></div>
+					<div><?= $t_set->getChoiceListValue('access', $set['access']); ?></div>
 				</td>
 				<td>
-					<div><?= caGetLocalizedDate($va_set['created'], ['timeOmit' => true, 'dateFormat' => 'delimited']); ?></div>
+					<div><?= caGetLocalizedDate($set['created'], ['timeOmit' => true, 'dateFormat' => 'delimited']); ?></div>
 				</td>
 				<td class="listtableEditDelete">
-					<?= caNavButton($this->request, __CA_NAV_ICON_EDIT__, _t("Edit"), '', 'manage/sets', 'SetEditor', 'Edit', array('set_id' => $va_set['set_id']), array(), array('icon_position' => __CA_NAV_ICON_ICON_POS_LEFT__, 'use_class' => 'list-button', 'no_background' => true, 'dont_show_content' => true)); ?>
-					<?php ($t_set->haveAccessToSet($user_id, __CA_SET_EDIT_ACCESS__, $va_set['set_id'])) ? print caNavButton($this->request, __CA_NAV_ICON_DELETE__, _t("Delete"), '', 'manage/sets', 'SetEditor', 'Delete', array('set_id' => $va_set['set_id']), array(), array('icon_position' => __CA_NAV_ICON_ICON_POS_LEFT__, 'use_class' => 'list-button', 'no_background' => true, 'dont_show_content' => true)) : ''; ?>
+					<?= caNavButton($this->request, __CA_NAV_ICON_EDIT__, _t("Edit"), '', 'manage/sets', 'SetEditor', 'Edit', array('set_id' => $set['set_id']), array(), array('icon_position' => __CA_NAV_ICON_ICON_POS_LEFT__, 'use_class' => 'list-button', 'no_background' => true, 'dont_show_content' => true)); ?>
+					<?php ($can_delete && $t_set->haveAccessToSet($user_id, __CA_SET_EDIT_ACCESS__, $set['set_id'])) ? print caNavButton($this->request, __CA_NAV_ICON_DELETE__, _t("Delete"), '', 'manage/sets', 'SetEditor', 'Delete', array('set_id' => $set['set_id']), array(), array('icon_position' => __CA_NAV_ICON_ICON_POS_LEFT__, 'use_class' => 'list-button', 'no_background' => true, 'dont_show_content' => true)) : ''; ?>
 				</td>
 			</tr>
 <?php
@@ -189,7 +197,7 @@ if (!$this->request->isAjax()) {
 	} else {
 ?>
 		<tr>
-			<td colspan='<?= $vn_type_id ? 9 : 10; ?>'>
+			<td colspan='<?= $type_id ? 9 : 10; ?>'>
 				<div align="center">
 					<?= _t('No sets have been created'); ?>
 				</div>
