752c477c4488cc312f8706ecdb5ba35e733c2090
CollectiveAccess <seth@collectiveaccess.org>
2025-09-10 11:15:22 -0400

    Don't redirect to single direct idno hit in quicksearch when there are other (even inexact) results

--- app/controllers/find/QuickSearchController.php
+++ app/controllers/find/QuickSearchController.php
@@ -103,8 +103,8 @@ class QuickSearchController extends BaseFindController {
 						}
 						$result->seek(0);
 					}
-				}else{
-					$multiple_results = 1;
+				} else {
+					$multiple_results++;
 				}
 			}
 		}
@@ -123,8 +123,7 @@ class QuickSearchController extends BaseFindController {
 				
 		$this->view->setVar('maxNumberResults', $this->opn_num_results_per_item_type);
 		
-		if($force_to) {
-			$multiple_results = null;
+		if($force_to && !$multiple_results) {
 			$single_results = [$force_to['table'] => $force_to['id']];
 		}
 		
