796bb751914b4e88e38ce93331e16d13693dffb6
Seth <seth@whirl-i-gig.com>
2026-03-08 04:07:04 +0000

    Bump copyright year

--- assets/ca/ca.genericbundle.js
+++ assets/ca/ca.genericbundle.js
@@ -6,7 +6,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2008-2025 Whirl-i-Gig
+ * Copyright 2008-2026 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
