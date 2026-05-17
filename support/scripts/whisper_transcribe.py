d566988459d8218f43d90b40508d0f7ff5655906
seth <Seth>
2026-05-17 01:18:39 -0400

    Add support for word timestamps and JSON output

--- support/scripts/whisper_transcribe.py
+++ support/scripts/whisper_transcribe.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 import os
 import pathlib
 import argparse
+import json
 try:
     import whisper
 except ModuleNotFoundError:
@@ -19,35 +20,48 @@ parser.add_argument('--model', nargs=1, default='base',
                     help='Specify whisper model size, base model set as default')
 parser.add_argument('--tmpdir', nargs=1, default='/tmp',
                                     help='Specify whisper temporary directory')
+parser.add_argument('--format', nargs=1, default='vtt',
+                    help='Output format, vtt set as default')
+                    
+parser.add_argument('--words', nargs=1, default=False,
+                    help='Output word timestamps, false as default')
 
 args = parser.parse_args()
 
-def transcribe_audio(input, output, model, tmpdir):
-    vttFilename = output
-    if pathlib.PurePath(vttFilename).suffix != '.vtt':
-        vttFilename = vttFilename + '.vtt'
+def transcribe_audio(input, output, model, tmpdir, format, words):
+    filename = output
+    if pathlib.PurePath(filename).suffix != ('.' + format):
+        filename = filename + '.' + format
     
     if isinstance(model, list):
         model = model[0]
     model = whisper.load_model(model, None, tmpdir)
     
     try:
-        transcribe = model.transcribe(audio=input)
+        transcribe = model.transcribe(audio=input, word_timestamps=words)
     except RuntimeError as e:
         print("[whisper_transcribe.py] Could not transcribe file: " + str(e))
         quit(2)
-    segments = transcribe['segments']
-    with open(vttFilename, 'w', encoding='utf-8') as vttFile:
-        vttFile.write("WEBVTT\n\n")
-        for segment in segments:
-            startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+'.000'
-            endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+'.000'
-            text = segment['text']
-            segmentId = segment['id']+1
-            segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if (len(text) and text[0] == ' ') else text}\n\n"
+        
+    if format == 'vtt':
+        segments = transcribe['segments']
+        with open(filename, 'w', encoding='utf-8') as vttFile:
+            vttFile.write("WEBVTT\n\n")
+            for segment in segments:
+                startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+'.000'
+                endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+'.000'
+                text = segment['text']
+                segmentId = segment['id']+1
+                segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if (len(text) and text[0] == ' ') else text}\n\n"
+    
+                vttFile.write(segment)
+    elif format == 'json':
+        with open(filename, 'w', encoding='utf-8') as jsonFile:
+            json.dump(transcribe, jsonFile, indent=4, ensure_ascii=False)
+    else:
+        print("Invalid format")
+        quite(3)
+        
+    return filename
 
-            vttFile.write(segment)
-
-    return vttFilename
-
-transcribe_audio(args.input[0], args.output[0], args.model, args.tmpdir[0])
+transcribe_audio(args.input[0], args.output[0], args.model, args.tmpdir[0], args.format[0], args.words)
