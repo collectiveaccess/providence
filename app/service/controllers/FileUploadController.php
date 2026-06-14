fd7c5c8580278ab82fa8acc5db92bbaa02f42f93
CollectiveAccess <seth@collectiveaccess.org>
2026-06-13 23:18:07 -0400

    Add parameters to allow direct linking of uploaded files as representations to a primary record; used for condition checking app

--- app/service/controllers/FileUploadController.php
+++ app/service/controllers/FileUploadController.php
@@ -39,17 +39,20 @@ class FileUploadController extends BaseServiceController {
 	}
 	# -------------------------------------------------------
 	/**
-	 *
+	 * 
 	 */
 	public function upload() {
+		$request = $this->getRequest();
+		$t_instance = null;
 		try {
-			if(intval($this->getRequest()->getParameter('pretty', pInteger))>0) {
+			if(intval($request->getParameter('pretty', pInteger))>0) {
 				$this->getView()->setVar('pretty_print', true);
 			}
 
 			if(!strlen($jwt = \GraphQLServices\GraphQLServiceController::getBearerToken())) {
-				$jwt = $this->request->getParameter('jwt', pString);
+				$jwt = $request->getParameter('jwt', pString);
 			}
+			
 			if(!($u = \GraphQLServices\GraphQLServiceController::authenticate($jwt, ['returnAs' => 'array', 'throw' => true]))) {
 				return null;
 			}
@@ -59,9 +62,33 @@ class FileUploadController extends BaseServiceController {
 				throw new ApplicationException(_t('Upload path does not exist or is not writeable'));
 			}
 			
-			$errors = $copied = [];
+			if($table = $request->getParameter('table', pString)) {
+				if(!($t_instance = Datamodel::getInstance($table))) {
+					throw new ApplicationException(_t('Invalid table "%1"', $table));
+				}
+				$id = $request->getParameter('id', pInteger);
+				if(!$t_instance->load($id)) {
+					throw new ApplicationException(_t('Invalid id "%1"', $id));
+				}
+				if(!method_exists($t_instance, 'addRepresentation')) {
+					throw new ApplicationException(_t('Table does not take representations'));
+				}
+				
+				if(!$t_instance->isSaveable(new ca_users($u['id']), 'ca_object_representations')) {
+					throw new ApplicationException(_t('Access denied'));
+				}
+				$type_id = $request->getParameter('type_id', pString);
+				$locale_id = $request->getParameter('locale_id', pString);
+				$status = $request->getParameter('status', pString);
+				$access = $request->getParameter('access', pString);
+				$idno = $request->getParameter('idno', pString);
+				$label = $request->getParameter('label', pString);
+			}
+			
+			$errors = $notices = $copied = [];
+			$is_primary = true;
 			if(is_array($_FILES) && is_array($_FILES['file'])) {
-				$excluded_extensions = $this->getRequest()->getAppConfig()->getList('media_uploader_exclude_file_extensions');
+				$excluded_extensions = $request->getAppConfig()->getList('media_uploader_exclude_file_extensions');
 				
 				foreach($_FILES['file'] as $k => $v) {
 					if(!is_array($v)) {
@@ -69,8 +96,8 @@ class FileUploadController extends BaseServiceController {
 					}
 				}
 			
-				foreach($_FILES['file']['name'] as $i => $name) {
-					$name = preg_replace("![^A-Z0-9_\-\.]+!i", "_", $name);
+				foreach($_FILES['file']['name'] as $i => $n) {
+					$name = preg_replace("![^A-Z0-9_\-\.]+!i", "_", $n);
 					$ext = pathinfo($name, PATHINFO_EXTENSION);
 					
 					$rpath = preg_replace("!^".__CA_BASE_DIR__."!i", "", "{$path}/{$name}");
@@ -84,12 +111,22 @@ class FileUploadController extends BaseServiceController {
 						continue;
 					}
 					$copied[$rpath] = filesize("{$path}/{$name}");
+					
+					if($t_instance) {
+						if($t_instance->addRepresentation("{$path}/{$name}", $type_id, $locale_id, $status, $access, $is_primary, ['idno' => $idno, 'preferred_labels' => $label], ['original_filename' => $n])) {
+							$is_primary = false;
+							$notices[$rpath] = _t('Added uploaded file as representation for %1 with id %2 (%3)', $table, $id, $t_instance->get('idno'));
+						} else {
+							$errors[$rpath] = _t('Could not create representation for file "%1": %2', $name, join('; ', $t_instance->getErrors()));
+						}
+					}
 				}
 			}
 			if(sizeof($copied) > 0) {
 				$content = [
 					'fileCount' => sizeof($_FILES['file']['name']),
 					'files' => $copied,
+					'notices' => $notices,
 					'errors' => $errors
 				];
 				$this->getView()->setVar('content', $content);
